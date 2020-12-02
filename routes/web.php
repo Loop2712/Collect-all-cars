f8eaff88f7eaccd40ce33aea854217346931a035
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2020-12-02 14:40:07 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 
 Auth::routes();
 
 //Route::get('/home', 'HomeController@index')->name('home');
 
-
+Route::get('/', 'pageController@index')->name('home');
 Route::get('/home', 'pageController@index')->name('home');
 Route::resource('car', 'carController');
 Route::resource('car', 'carController');
