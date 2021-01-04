ecbb4ca8d44d3f7e35de111f4283c22374b7ef8c
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-01-04 14:08:54 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,8 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    
+    <title>Car</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
