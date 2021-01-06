460cc047020c592916548d3b4dc582fa600b05db
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-01-06 11:49:15 +0700

    Update select_get.blade.php

--- resources/views/register_car/select_get.blade.php
+++ resources/views/register_car/select_get.blade.php
@@ -3,7 +3,6 @@
         <div class="col-12">
         	<center>
                 <div><br><br>
-                    <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(100)->generate('Make me into an QrCode!')) !!} ">
                     {!! QrCode::size(300)->generate('{{ $register_car->name }}'); !!}
                 </div>
 	        	<a href="{{ url('/deliver/create') }}"><img width="80%" src="{{ asset('/img/icon/จัดส่ง.png') }}"></a>
