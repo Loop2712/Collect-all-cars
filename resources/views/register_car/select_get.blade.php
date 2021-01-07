e01af2ba505e0fa54391fcc007cea55ed4ace29b
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-01-07 11:18:09 +0700

    Update select_get.blade.php

--- resources/views/register_car/select_get.blade.php
+++ resources/views/register_car/select_get.blade.php
@@ -3,7 +3,7 @@
         <div class="col-12">
         	<center>
                 <div><br><br>
-                    {!! QrCode::size(300)->generate('https://car.viicheck.com/guest'); !!}
+                    {!! QrCode::size(300)->generate('https://car.viicheck.com/guest/create'); !!}
                 </div>
 	        	<a href="{{ url('/deliver/create') }}"><img width="80%" src="{{ asset('/img/icon/จัดส่ง.png') }}"></a>
 	        	<br>
