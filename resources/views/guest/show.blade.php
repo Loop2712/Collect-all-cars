52cfec539d6b641b3a1333a2dbc29e30bde1f613
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-01-07 11:13:51 +0700

    update

--- resources/views/guest/show.blade.php
+++ resources/views/guest/show.blade.php
@@ -3,9 +3,6 @@
 @section('content')
     <div class="container">
         <div class="row">
-            @include('admin.sidebar')
-
-            <div class="col-md-9">
                 <div class="card">
                     <div class="card-header">Guest {{ $guest->id }}</div>
                     <div class="card-body">
