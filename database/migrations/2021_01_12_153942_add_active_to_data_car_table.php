e913ecbb99e535f6e417b9fe11133fd352f6a8b7
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-01-12 23:41:54 +0700

    add active to details , data_cars

---
+++ database/migrations/2021_01_12_153942_add_active_to_data_car_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddActiveToDataCarTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('data_cars', function (Blueprint $table) {
+            $table->string('active')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('data_cars', function (Blueprint $table) {
+            //
+        });
+    }
+}
