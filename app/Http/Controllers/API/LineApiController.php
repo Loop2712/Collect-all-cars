286aeb04061338105d3b67402b3e75e0e7eb9098
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-01-19 10:42:29 +0700

    Update LineApiController.php

--- app/Http/Controllers/API/LineApiController.php
+++ app/Http/Controllers/API/LineApiController.php
@@ -13,5 +13,16 @@ class LineApiController extends Controller
     public function store(Request $request)
 	{
 		http_response_code(200);
+
+		$access_token = 'es9wh5s70CThUrRxbF62bIYGxPCeAbRlQbC2jktBUuMWyQISqyBrtkLiyHh9Wfx8NA9SPWTBr4ooOYPusYcwuZjsy6khvF717wmNnAEBu4peodF+Z4AZ3s6KOexDq3+Fa9oNWiyCLNuf1sHKz4Hc9AdB04t89/1O/w1cDnyilFU=';
+		// Get POST body content
+		// $content = file_get_contents('php://input');
+
+		$content = $request->all();
+        $data = [
+            "title" => "Line",
+            "content" => json_encode($content, JSON_UNESCAPED_UNICODE),
+        ];
+        Mylog::create($data);
 	}
 }
