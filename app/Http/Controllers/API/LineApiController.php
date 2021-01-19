8a1087e9f955b1b30a26e5a7d23c7d9bceb2222a
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-01-19 10:46:32 +0700

    Update LineApiController.php

--- app/Http/Controllers/API/LineApiController.php
+++ app/Http/Controllers/API/LineApiController.php
@@ -14,7 +14,7 @@ class LineApiController extends Controller
 	{
 		http_response_code(200);
 
-		$access_token = 'es9wh5s70CThUrRxbF62bIYGxPCeAbRlQbC2jktBUuMWyQISqyBrtkLiyHh9Wfx8NA9SPWTBr4ooOYPusYcwuZjsy6khvF717wmNnAEBu4peodF+Z4AZ3s6KOexDq3+Fa9oNWiyCLNuf1sHKz4Hc9AdB04t89/1O/w1cDnyilFU=';
+		$access_token = 'YHK/caBpRII4zi9AItTECuE+x9LVrg/F+ZZxtrrZLhij26Vh3V97VnAkYmKeSl9cNA9SPWTBr4ooOYPusYcwuZjsy6khvF717wmNnAEBu4rLJULOe1ul5rzKHJKMQYfsjDCNSE2aPUtiuWbOxrQFvwdB04t89/1O/w1cDnyilFU=';
 		// Get POST body content
 		// $content = file_get_contents('php://input');
 
