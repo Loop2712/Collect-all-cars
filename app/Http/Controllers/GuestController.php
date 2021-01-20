97a580b89a68d9630aa4774f43af92696933c17e
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-01-20 12:15:20 +0700

    Update GuestController.php

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -143,6 +143,10 @@ class GuestController extends Controller
         $masseng = $data['masseng'];
 
         $register_car = DB::select("SELECT * FROM register_cars WHERE registration_number = '$registration' AND province = '$county'");
+        echo "<pre>";
+        print_r($register_car);
+        echo "</pre>";
+        exit();
 
         $strAccessToken = "VsNZQKpv/ojbmRVXqM6v4PdOHGG5MKQblyKr4LuXo0jyGGRkaNBRLmEBQKE1BzLRNA9SPWTBr4ooOYPusYcwuZjsy6khvF717wmNnAEBu4oeppBc/woRCLiPqz3X5xTCMrEwxvrExidXIidR9SWUxAdB04t89/1O/w1cDnyilFU=";
  
