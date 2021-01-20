d6a379e8e29e7c834b29b0bd0820948d90d39f7b
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-01-20 12:51:55 +0700

    ส่งไลน์ได้แล้ว

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -146,30 +146,33 @@ class GuestController extends Controller
         
         foreach($register_car as $item){
 
-            $strAccessToken = "VsNZQKpv/ojbmRVXqM6v4PdOHGG5MKQblyKr4LuXo0jyGGRkaNBRLmEBQKE1BzLRNA9SPWTBr4ooOYPusYcwuZjsy6khvF717wmNnAEBu4oeppBc/woRCLiPqz3X5xTCMrEwxvrExidXIidR9SWUxAdB04t89/1O/w1cDnyilFU=";
+            // if(!empty($item->provider_id)){
+                $strAccessToken = "VsNZQKpv/ojbmRVXqM6v4PdOHGG5MKQblyKr4LuXo0jyGGRkaNBRLmEBQKE1BzLRNA9SPWTBr4ooOYPusYcwuZjsy6khvF717wmNnAEBu4oeppBc/woRCLiPqz3X5xTCMrEwxvrExidXIidR9SWUxAdB04t89/1O/w1cDnyilFU=";
      
-            $strUrl = "https://api.line.me/v2/bot/message/push";
-             
-            $arrHeader = array();
-            $arrHeader[] = "Content-Type: application/json";
-            $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
-             
-            $arrPostData = array();
-            $arrPostData['to'] = $item->provider_id;
-            $arrPostData['messages'][0]['type'] = "text";
-            $arrPostData['messages'][0]['text'] = "รถหมายเลขทะเบียน"." ".$item->registration_number." ".$item->province." ".$masseng;
-             
-             
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL,$strUrl);
-            curl_setopt($ch, CURLOPT_HEADER, false);
-            curl_setopt($ch, CURLOPT_POST, true);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-            $result = curl_exec($ch);
-            curl_close ($ch);
+                $strUrl = "https://api.line.me/v2/bot/message/push";
+                 
+                $arrHeader = array();
+                $arrHeader[] = "Content-Type: application/json";
+                $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+                 
+                $arrPostData = array();
+                $arrPostData['to'] = $item->provider_id;
+                $arrPostData['messages'][0]['type'] = "text";
+                $arrPostData['messages'][0]['text'] = "รถหมายเลขทะเบียน"." ".$item->registration_number." ".$item->province." ".$masseng;
+                 
+                 
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL,$strUrl);
+                curl_setopt($ch, CURLOPT_HEADER, false);
+                curl_setopt($ch, CURLOPT_POST, true);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+                $result = curl_exec($ch);
+                curl_close ($ch);
+            // }
+            
         }
         
     }
