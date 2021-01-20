4dd84d23dc29f7eb9c642332e0ea6e06795f52b4
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-01-20 13:27:48 +0700

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -76,7 +76,7 @@ class LoginController extends Controller
         $this->_registerOrLoginUser($user);
 
         // Return home after login
-        return redirect()->route('home');
+        // return redirect()->route('home');
     }
 
     // Line login
