49661018fc5654e9764654a756d5bf1691024478
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-01-20 22:43:00 +0700

    Update LineApiController.php

--- app/Http/Controllers/API/LineApiController.php
+++ app/Http/Controllers/API/LineApiController.php
@@ -14,7 +14,7 @@ class LineApiController extends Controller
 	{
 		echo "Hello";
 
-		$strAccessToken = "VsNZQKpv/ojbmRVXqM6v4PdOHGG5MKQblyKr4LuXo0jyGGRkaNBRLmEBQKE1BzLRNA9SPWTBr4ooOYPusYcwuZjsy6khvF717wmNnAEBu4oeppBc/woRCLiPqz3X5xTCMrEwxvrExidXIidR9SWUxAdB04t89/1O/w1cDnyilFU=";
+		$access_token = "VsNZQKpv/ojbmRVXqM6v4PdOHGG5MKQblyKr4LuXo0jyGGRkaNBRLmEBQKE1BzLRNA9SPWTBr4ooOYPusYcwuZjsy6khvF717wmNnAEBu4oeppBc/woRCLiPqz3X5xTCMrEwxvrExidXIidR9SWUxAdB04t89/1O/w1cDnyilFU=";
  
 		// $content = file_get_contents('php://input');
 		// $arrJson = json_decode($content, true);
@@ -29,45 +29,42 @@ class LineApiController extends Controller
 		// Parse JSON
 		$events = $content;
 		 
-		$strUrl = "https://api.line.me/v2/bot/message/reply";
-		 
-		$arrHeader = array();
-		$arrHeader[] = "Content-Type: application/json";
-		$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
-		 
-		if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
-		  $arrPostData = array();
-		  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-		  $arrPostData['messages'][0]['type'] = "text";
-		  $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
-		}else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
-		  $arrPostData = array();
-		  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-		  $arrPostData['messages'][0]['type'] = "text";
-		  $arrPostData['messages'][0]['text'] = "ฉันยังไม่มีชื่อนะ";
-		}else if($arrJson['events'][0]['message']['text'] == "ทำอะไรได้บ้าง"){
-		  $arrPostData = array();
-		  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-		  $arrPostData['messages'][0]['type'] = "text";
-		  $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
-		}else{
-		  $arrPostData = array();
-		  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-		  $arrPostData['messages'][0]['type'] = "text";
-		  $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
+		if (!is_null($events['events'])) {
+
+			foreach ($events['events'] as $event) {
+				// หา UserId ของเจ้าของรถ
+				if ($event['type'] == 'source' && $event['source']['type'] == 'user') {
+					$userId = $event['source']['userId'];
+
+					$reply_provider_id = DB::select("SELECT reply_provider_id FROM register_cars WHERE provider_id = '$userId' ");
+				
+					// หาข้อความที่ของเจ้าของรถส่งมา
+					if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+						$text = $event['message']['text'];
+
+
+						// Make a POST Request to Messaging API to reply to sender
+				        $url = 'https://api.line.me/v2/bot/message/reply';
+				        $data = [
+				            'replyToken' => $reply_provider_id,
+				            'messages' => $text
+				        ];
+				        $post = json_encode($data);
+				        $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+				        $ch = curl_init($url);
+				        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+				        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+				        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+				        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+				        $result = curl_exec($ch);
+				        curl_close($ch);
+				        echo $result . "";
+				    }
+				}
+			}
+
 		}
-		 
-		 
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL,$strUrl);
-		curl_setopt($ch, CURLOPT_HEADER, false);
-		curl_setopt($ch, CURLOPT_POST, true);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		$result = curl_exec($ch);
-		curl_close ($ch);
 
 	}
 }
