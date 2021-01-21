912b9ba77a5d54c7e6482fc8bf579dda4afe196b
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-01-21 09:12:54 +0700

    Update LineApiController.php

--- app/Http/Controllers/API/LineApiController.php
+++ app/Http/Controllers/API/LineApiController.php
@@ -44,45 +44,43 @@ class LineApiController extends Controller
 						$reply_provider_id = array();
 						foreach($reply as $item){
 							$reply_p = $item->reply_provider_id;
+						
+							 // ตอบกลับ
+							$arrPostData = array();
+							$arrPostData['to'] = $reply_p;
+			                $arrPostData['messages'][0]['type'] = "text";
+			                $arrPostData['messages'][0]['text'] = $text;
 
-							$reply_provider_id[] = $reply_p
-						}
-						 // ตอบกลับ
-						$arrPostData = array();
-						$arrPostData['to'] = $reply_provider_id;
-		                $arrPostData['messages'][0]['type'] = "text";
-		                $arrPostData['messages'][0]['text'] = $text;
+			                $strUrl = "https://api.line.me/v2/bot/message/push";
+	                 
+			                $arrHeader = array();
+			                $arrHeader[] = "Content-Type: application/json";
+			                $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
 
-		                $strUrl = "https://api.line.me/v2/bot/message/push";
-                 
-		                $arrHeader = array();
-		                $arrHeader[] = "Content-Type: application/json";
-		                $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+							// // Make a POST Request to Messaging API to reply to sender
+					        // $url = 'https://api.line.me/v2/bot/message/push';
+					        // $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+					        // $ch = curl_init($url);
+					        // curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+					        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+					        // curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+					        // curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+					        // curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+					        // $result = curl_exec($ch);
+					        // curl_close($ch);
+					        // echo $result . "";
 
-						// // Make a POST Request to Messaging API to reply to sender
-				        // $url = 'https://api.line.me/v2/bot/message/push';
-				        // $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-				        // $ch = curl_init($url);
-				        // curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-				        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-				        // curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
-				        // curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-				        // curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-				        // $result = curl_exec($ch);
-				        // curl_close($ch);
-				        // echo $result . "";
-
-				        $ch = curl_init();
-			            curl_setopt($ch, CURLOPT_URL,$strUrl);
-			            curl_setopt($ch, CURLOPT_HEADER, false);
-			            curl_setopt($ch, CURLOPT_POST, true);
-			            curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
-			            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
-			            curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-			            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-			            $result = curl_exec($ch);
-			            curl_close ($ch);
-					    
+					        $ch = curl_init();
+				            curl_setopt($ch, CURLOPT_URL,$strUrl);
+				            curl_setopt($ch, CURLOPT_HEADER, false);
+				            curl_setopt($ch, CURLOPT_POST, true);
+				            curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+				            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+				            curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+				            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+				            $result = curl_exec($ch);
+				            curl_close ($ch);
+					    }
 				    }
 				}
 			}
