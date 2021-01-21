17941da26f12ef57751e0e641e8bd6cd79a83ef6
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-01-21 23:01:53 +0700

    Update LineMessagingAPI.php

--- app/Models/LineMessagingAPI.php
+++ app/Models/LineMessagingAPI.php
@@ -24,7 +24,7 @@ class LineMessagingAPI extends Model
 
                 $data = [
 		            "title" => "replyToUser",
-		            "content" => "replyToUser",
+		            "content" => $messages
 		        ];
 		        MyLog::create($data);
                 break;
