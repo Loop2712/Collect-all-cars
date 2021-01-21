fc83979e2028156433a29b7ffb9c15d86ee1683b
Loop2712 <52275890+Loop2712@users.noreply.github.com>
2021-01-21 12:25:04 +0700

    update js css bootstrap

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,10 +13,11 @@
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     
 
     <!-- Css Styles -->
-    <link rel="stylesheet" href="{{ asset('css/car/bootstrap.min.css')}}" type="text/css">
+    <!-- <link rel="stylesheet" href="{{ asset('css/car/bootstrap.min.css')}}" type="text/css"> -->
     <link rel="stylesheet" href="{{ asset('css/car/font-awesome.min.css')}}" type="text/css">
     <link rel="stylesheet" href="{{ asset('css/car/elegant-icons.css')}}" type="text/css">
     <link rel="stylesheet" href="{{ asset('css/car/nice-select.css')}}" type="text/css">
@@ -172,8 +173,10 @@
     <!-- Search End -->
 
     <!-- Js Plugins -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
     <script src="{{ asset('js/car/jquery-3.3.1.min.js')}}"></script>
-    <script src="{{ asset('js/car/bootstrap.min.js')}}"></script>
+    <!-- <script src="{{ asset('js/car/bootstrap.min.js')}}"></script> -->
     <script src="{{ asset('js/car/jquery.nice-select.min.js')}}"></script>
     <script src="{{ asset('js/car/jquery-ui.min.js')}}"></script>
     <script src="{{ asset('js/car/jquery.magnific-popup.min.js')}}"></script>
