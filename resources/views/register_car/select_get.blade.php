62b4d2930945fc158b62fe58d2a37a5ae667e207
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-01-24 14:38:19 +0700

    Update select_get.blade.php

--- resources/views/register_car/select_get.blade.php
+++ resources/views/register_car/select_get.blade.php
@@ -4,9 +4,9 @@
         	<center>
                 <div><br><br>
                     <!-- <img src="https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=https://market.viicheck.com/guest/create/&choe=UTF-8"  /> -->
-                    <img src="{{ asset('/img/more/sticker-VII-v1.png') }}"/>
+                    <img width="250" src="{{ asset('/img/more/sticker-VII-v1.png') }}"/>
                 </div>
-                <a href="{{ asset('/img/more/sticker-VII-v1.png') }}" download ><img width="50%" src="{{ asset('/img/icon/ไdownload_jpg.png') }}"></a>
+                <a href="{{ asset('/img/more/sticker-VII-v1.png') }}" download ><img width="50%" src="{{ asset('/img/icon/download_jpg.png') }}"></a>
         	</center>
         </div>
     </div>
