4492c2eadd2dc925bf827221f5e609c4ea013f60
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-01-24 14:30:23 +0700

    Update select_get.blade.php

--- resources/views/register_car/select_get.blade.php
+++ resources/views/register_car/select_get.blade.php
@@ -3,10 +3,10 @@
         <div class="col-12">
         	<center>
                 <div><br><br>
-                    <img src="https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=https://market.viicheck.com/guest/create/&choe=UTF-8"  />
-                    <!-- <img src="{{ asset('/img/more/qr-code.png') }}"/> -->
+                    <!-- <img src="https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=https://market.viicheck.com/guest/create/&choe=UTF-8"  /> -->
+                    <img src="{{ asset('/img/more/sticker-VII-v1.png') }}"/>
                 </div>
-                <a href="{{ asset('/img/more/qr-code.png') }}" download ><img width="80%" src="{{ asset('/img/icon/ไม่จัดส่ง.png') }}"></a>
+                <a href="{{ asset('/img/more/sticker-VII-v1.png') }}" download ><img width="80%" src="{{ asset('/img/icon/ไม่จัดส่ง.png') }}"></a>
         	</center>
         </div>
     </div>
