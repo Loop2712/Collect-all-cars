6e633e618911447c7adb563ec79c5c9446430160
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-01-26 01:00:45 +0700

    Update thx_guest.blade.php

--- resources/views/guest/thx_guest.blade.php
+++ resources/views/guest/thx_guest.blade.php
@@ -4,10 +4,10 @@
 			<center>
 				<br>
 				<h2>ระบบได้ทำการแจ้งเตือนไปยังเจ้าของรถเรียบร้อยแล้ว กรุณารอเจ้าของรถสักครู่</h2>
-				<span style="color: red;">กรุณาเพิ่ม&nbsp;&nbsp;<a href="https://lin.ee/xnFKMfc"><img width="70" src="{{ asset('/img/more/logo_.png') }}"></a>&nbsp;&nbsp;เป็นเพื่อนเพื่อรับข้อความตอบกลับจากเจ้าของรถ</span>
+				<span style="color: red;font-size: 20px;">กรุณาเพิ่ม&nbsp;&nbsp;<a href="https://lin.ee/xnFKMfc"><img width="70" src="{{ asset('/img/more/logo_.png') }}"></a>&nbsp;&nbsp;เป็นเพื่อนเพื่อรับข้อความตอบกลับจากเจ้าของรถ</span>
 				<br><br>
 				<a href="https://lin.ee/xnFKMfc">
-					<img width="50%" src="https://www.ineedtoknow.org/wp-content/uploads/2016/02/backupLine2.jpg">
+					<img width="80%" src="https://www.ineedtoknow.org/wp-content/uploads/2016/02/backupLine2.jpg">
 				</a>
 				<br><br>
 				<a href="https://lin.ee/xnFKMfc"><img src="https://scdn.line-apps.com/n/line_add_friends/btn/th.png" alt="เพิ่มเพื่อน" height="36" border="0"></a>
