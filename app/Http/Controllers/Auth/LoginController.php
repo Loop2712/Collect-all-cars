56653ee9e00f6556487d57925ddeda8c4b73c9dd
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-02-01 02:15:54 +0700

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -118,10 +118,10 @@ class LoginController extends Controller
 
             if (empty($data->email)) {
                 $user->username = $data->name;
-                $user->email = " ";
+                $user->email = "";
             }
             if (empty($data->avatar)) {
-                $user->avatar = " ";
+                $user->avatar = "";
             }
 
             $user->save();
