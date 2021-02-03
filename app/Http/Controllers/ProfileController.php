06d905e070781faf6ecaa86f6adc1600b312eb00
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-02-03 14:37:06 +0700

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,6 +6,7 @@ use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Register_car;
+use Illuminate\Support\Facades\DB;
 
 class ProfileController extends Controller
 {
