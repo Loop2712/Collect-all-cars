305b5282763dfd2864063f7a17f00ebd838749eb
Loop2712 <52275890+Loop2712@users.noreply.github.com>
2021-02-04 12:23:38 +0700

    แก้เมนู side bar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,18 +1,22 @@
 <div class="col col-md-3">
     <div class="card">
-        <div class="card-body">
-        <div class="row">
-        <a href="{{ url('/profile' ) }}">
-        <i class="fa fa-pencil-square-o" aria-hidden="true"></i> ข้อมูลส่วนบุคคล </a><br><br>
-        </div>
-        <div class="row">
-        <a href="{{ url('/wishlist' ) }}">
-        <i class="fa fa-heart" aria-hidden="true"></i> รายการโปรด </a><br><br>
-        </div>
-        <div class="row">
-        <a href="{{ url('/sell' ) }}">
-        <i class="fa fa-pencil-square-o" aria-hidden="true"></i> ขายรถ </a><br><br>
-        </div>
+        <div class="card-body" style="padding: 2rem;">
+        <div class="call__text">
+            <a href="{{ url('/profile' ) }}">
+            <i class="fas fa-user-edit"></i> &nbsp;&nbsp;   ข้อมูลส่วนบุคคล </a>
+        </div><br>
+        <div class="call__text">
+            <a href="{{ url('/wishlist' ) }}">
+            <i class="fa fa-heart" ></i>&nbsp;&nbsp;    รายการโปรด </a>
+        </div><br>
+        <div class="call__text">
+            <a href="{{ url('/sell' ) }}">
+            <i class="fas fa-car"></i>&nbsp;&nbsp;    ขายรถยนต์ </a>
+        </div><br>
+        <div class="call__text">
+            <a href="{{ url('/motercycles' ) }}">
+            <i class="fas fa-motorcycle"></i>&nbsp;&nbsp;    ขายรถจักรยานยนต์ </a>
+        </div><br>
         
         </div>
     </div>
