903074a4de17ada08ad1fa60a7de0df90aadd42c
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-02-15 11:37:18 +0700

    update model line absolute

--- resources/views/guest/modal.blade.php
+++ resources/views/guest/modal.blade.php
@@ -3,11 +3,17 @@
 @section('content')
 
 <center>
-	<img style="margin-top: -40px;" width="100%" src="{{ asset('/img/more/select.jpg') }}">
-	<hr>
 	<div class="col-12">
-		<div class="row">
+		<div>
+			<img style="margin-top: -40px;" width="100%" src="{{ asset('/img/more/select.jpg') }}">
 			@if(Auth::check())
+				<a style="position: absolute;left: 200px;top: 500px;" href="{{ url('/guest/create') }}"><img width="300" height="400" src="{{ asset('/img/icon/empty.png') }}"></a>
+			@else
+				<a style="position: absolute;left: 200px;top: 500px;" href="{{ route('login.line') }}?redirectTo={{ url('/guest/create') }}"><img width="300" height="400" src="{{ asset('/img/icon/empty.png') }}"></a>
+			@endif
+		</div>
+		<div class="row">
+			<!-- @if(Auth::check())
 	    		<div class="col-6">
 					<a href="{{ url('/guest/create') }}"><img width="160" height="60" src="{{ asset('/img/icon/line.png') }}"></a>
 				</div>
@@ -21,7 +27,7 @@
 				<div class="col-6">
 	    			<a href="{{ route('login.facebook') }}?redirectTo={{ url('/guest/create') }}"><img width="160" height="60" src="{{ asset('/img/icon/fb.png') }}">
 	    		</div>
-    		@endif
+    		@endif -->
 		</div>
 
 		<!-- <a href="{{ url('/guest/create') }}"><button class="btn btn-primary btn-sm"><i class="fas fa-check-circle"></i>&nbsp;&nbsp; ตกลง</button></a> -->
--- resources/views/layouts/news.blade.php
+++ resources/views/layouts/news.blade.php
@@ -10,9 +10,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <!-- <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Krub:wght@200&display=swap" rel="stylesheet"> -->
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=K2D:wght@200&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Krub:wght@200&display=swap" rel="stylesheet"> -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=K2D:wght@200&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
