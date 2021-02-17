124313da1d0df77417e0d6676e0c3e61004576c9
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-02-17 12:09:04 +0700

    Create share.blade.php

---
+++ resources/views/news/share.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+
+<center>
+	<div class="col-12">
+		<div>
+			<img style="margin-top: -40px;" width="100%" src="{{ asset('/img/more/select.jpg') }}">
+		</div>
+	</div>
+</center>
+
+
+@endsection
