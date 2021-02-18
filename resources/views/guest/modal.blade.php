b5861e519af1200f0fad04cfcfd6653bde9fd95a
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-02-18 12:52:38 +0700

    Update modal.blade.php

--- resources/views/guest/modal.blade.php
+++ resources/views/guest/modal.blade.php
@@ -7,9 +7,9 @@
 		<div>
 			<img style="margin-top: -40px;" width="100%" src="{{ asset('/img/more/select.jpg') }}">
 			@if(Auth::check())
-				<a style="position: absolute;right: 52%;top: 49%;" href="{{ url('/guest/create') }}"><img width="25%" src="{{ asset('/img/icon/empty.png') }}"></a>
+				<a style="position: absolute;right: 66%;top: 46%;" href="{{ url('/guest/create') }}"><img width="70" src="{{ asset('/img/icon/empty.png') }}"></a>
 			@else
-				<a style="position: absolute;right: 52%;top: 49%;" href="{{ route('login.line') }}?redirectTo={{ url('/guest/create') }}"><img width="25%" src="{{ asset('/img/icon/empty.png') }}"></a>
+				<a style="position: absolute;right: 66%;top: 46%;" href="{{ route('login.line') }}?redirectTo={{ url('/guest/create') }}"><img width="70" src="{{ asset('/img/icon/empty.png') }}"></a>
 			@endif
 		</div>
 		<div class="row">
