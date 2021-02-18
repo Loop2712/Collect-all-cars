2438d0dd25fcfb82b7423159bf85b60b7243406e
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-02-18 13:01:43 +0700

    modal login

--- resources/views/guest/modal.blade.php
+++ resources/views/guest/modal.blade.php
@@ -6,11 +6,19 @@
 	<div class="col-12">
 		<div>
 			<img style="margin-top: -40px;" width="100%" src="{{ asset('/img/more/select.jpg') }}">
+
+			<!-- LINE -->
 			@if(Auth::check())
 				<a style="position: absolute;right: 66%;top: 46%;" href="{{ url('/guest/create') }}"><img width="70" src="{{ asset('/img/icon/empty.png') }}"></a>
 			@else
 				<a style="position: absolute;right: 66%;top: 46%;" href="{{ route('login.line') }}?redirectTo={{ url('/guest/create') }}"><img width="70" src="{{ asset('/img/icon/empty.png') }}"></a>
 			@endif
+
+			<!-- FACEBOOK -->
+			<a style="position: absolute;right: 39%;top: 46%;" href="#" onclick="not_ready();"><img width="70" src="{{ asset('/img/icon/empty.png') }}"></a>
+
+			<!-- GOOGLE -->
+			<a style="position: absolute;right: 13%;top: 46%;" href="#" onclick="not_ready();"><img width="70" src="{{ asset('/img/icon/empty.png') }}"></a>
 		</div>
 		<div class="row">
 			<!-- @if(Auth::check())
@@ -33,6 +41,14 @@
 		<!-- <a href="{{ url('/guest/create') }}"><button class="btn btn-primary btn-sm"><i class="fas fa-check-circle"></i>&nbsp;&nbsp; ตกลง</button></a> -->
 	</div>
 </center>
+<script>
+
+function not_ready() {
+    alert("ขออภัย ขณะนี้ระบบยังไม่เปิดให้บริการ");
+}
+
+</script>
+
 
 
 @endsection
