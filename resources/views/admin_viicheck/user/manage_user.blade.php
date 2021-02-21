b5db1f6639a0d48fb6de037cc82a40f0c5888e87
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-02-22 02:53:25 +0700

    Update manage_user.blade.php

--- resources/views/admin_viicheck/user/manage_user.blade.php
+++ resources/views/admin_viicheck/user/manage_user.blade.php
@@ -64,8 +64,8 @@
                                             <center><b>{{ $item->id }}</b></center>
                                         </div> -->
                                         <div class="col-4">
-                                            <h5 class="text-success">&nbsp;&nbsp;{{ $item->name }}&nbsp;
-                                            <span style="font-size: 15px;"><a target="break" href="{{ url('/').'/profile/'.$item->id }}"><i class="far fa-eye text-primary"></i></a></span></h5>
+                                            <h5 class="text-success"><span style="font-size: 15px;"><a target="break" href="{{ url('/').'/profile/'.$item->id }}"><i class="far fa-eye text-primary"></i></a></span>&nbsp;&nbsp;{{ $item->name }}
+                                            </h5>
                                         </div>
                                         <div class="col-1">
                                             <center>
