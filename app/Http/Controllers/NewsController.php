5a138a02815ef68938612e60ad6b899d3f6229e4
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-02-21 15:42:51 +0700

    Update NewsController.php

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -74,8 +74,8 @@ class NewsController extends Controller
             $image = Image::make(storage_path("app/public")."/".$requestData['photo']);
             $image_facebook = Image::make(storage_path("app/public")."/".$requestData['photo']);
 
-            // $image->rotate($requestData['rotation']);
-            // $image_facebook->rotate($requestData['rotation']);
+            $image->rotate($requestData['rotation']);
+            $image_facebook->rotate($requestData['rotation']);
 
             //  เช็คเนื้อหาที่รุนแรง
             if ($requestData['severe'] == 'Yes') {
