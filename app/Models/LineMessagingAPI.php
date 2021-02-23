d510835d7ccd2430c25cb9cb5f9a0bfcd0c890f0
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-02-23 23:00:55 +0700

    update

--- app/Models/LineMessagingAPI.php
+++ app/Models/LineMessagingAPI.php
@@ -346,8 +346,8 @@ class LineMessagingAPI extends Model
                 foreach($user as $item){
                     $template_path = storage_path('../public/json/flex-driver_license.json');   
                     $string_json = file_get_contents($template_path);
-                    $string_json = str_replace("car",$item->driver_license,$string_json);
-                    $string_json = str_replace("motorcycle",$item->driver_license2,$string_json);
+                    $string_json = str_replace("ccaarr",$item->driver_license,$string_json);
+                    $string_json = str_replace("mmotorcycle",$item->driver_license2,$string_json);
                 }
 
                 $messages = [ json_decode($string_json, true) ]; 
