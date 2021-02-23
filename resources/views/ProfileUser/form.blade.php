caa3c2a9f1a73b09598dbcbd81d0fe2d0b36d97f
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-02-23 22:25:55 +0700

    update

--- resources/views/ProfileUser/form.blade.php
+++ resources/views/ProfileUser/form.blade.php
@@ -112,7 +112,7 @@
         <div class="row">
             <div class="col-12 col-md-2">
                 <label for="massengbox" class="control-label">
-                    <b>{{ 'ใบอนุญาตขับรถ / Driver license ' }}</b>
+                    <b>{{ 'ใบอนุญาตขับรถ / Driver license ' }}</b><br>
                     <span style="font-size: 13px;" class="text-danger">ใบอนุญาตขับรถจะไม่แสดงให้ผู้อื่นเห็น</span>
                 </label>
             </div>
