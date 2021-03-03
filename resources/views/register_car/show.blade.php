d5421c1cd06fab952c68e058ddd22f4739473199
Loop2712 <52275890+Loop2712@users.noreply.github.com>
2021-03-04 02:33:35 +0700

    Update show.blade.php

--- resources/views/register_car/show.blade.php
+++ resources/views/register_car/show.blade.php
@@ -7,8 +7,7 @@
                 <div class="card">
                     <div class="card-header">รหัสรถหมายเลข {{ $register_car->id }}</div>
                     <div class="card-body"> 
-                        <form method="POST" action="{{ url('/sell') }}" accept-charset="UTF-8" class="form-horizontal" enctype="multipart/form-data">
-                            {{ csrf_field() }}
+                        
 
                          
 
@@ -23,12 +22,15 @@
                         <br/>
                         <br/> -->
                         @if($register_car->car_type == "car")
+                        <form method="POST" action="{{ url('/sell') }}" accept-charset="UTF-8" class="form-horizontal" enctype="multipart/form-data">
+                            {{ csrf_field() }}
                         <div class="table-responsive">
+                        
                             <table class="table">
                                 <tbody>
                                     
                                     <tr>
-                                        <th> ยี่ห้อรถ / Brand  </th>
+                                        <th><label  class="control-label">{{ 'ยี่ห้อรถ / Brand' }}</label></th>
                                         <td> 
                                         <div class="form-group {{ $errors->has('brand') ? 'has-error' : ''}}">
                                             <input class="form-control" name="brand" type="text" id="brand" value="{{ $register_car->brand }}"  readonly/>
@@ -37,7 +39,7 @@
                                          </td>
                                     </tr>
                                     <tr>
-                                        <th> รุ่นรถ / Model  </th>
+                                        <th><label  class="control-label">{{ 'รุ่นรถ / Gear Model' }}</label></th>
                                         <td> 
                                         <div class="form-group {{ $errors->has('model') ? 'has-error' : ''}}">
                                             <input class="form-control" name="model" type="text" id="model" value="{{ $register_car->generation }}"  readonly/>
@@ -46,7 +48,7 @@
                                         </td>
                                     </tr>
                                     <tr>
-                                        <th> ระบบเกียร์ </th>
+                                        <th> <label  class="control-label">{{ 'ระบบเกียร์ / Gear System' }}</label> </th>
                                         <td>
                                             <select name="gear" id="gear" class="form-control" value="{{ isset($sell->gear) ? $sell->gear : ''}}" >
                                                 <option value="" data-display="">- กรุณาเลือกระบบเกียร์ / Please select Gear -</option>
@@ -57,18 +59,20 @@
                                         </td>
                                     </tr>
                                     <tr>
-                                        <th> น้ำมันที่ใช้ </th>
+                                        <th> <label  class="control-label">{{ 'เชิ้อเพลิง / Engine Type' }}</label> </th>
                                         <td>
-                                    <select class="form-control" name="fuel" id="fuel" value="{{ isset($sell->fuel) ? $sell->fuel : ''}}" >
-                                            <option value="" selected> - กรุณาเลือกเชื้อเพลิง / Please select fuel - </option> 
-                                        @foreach (json_decode('{"ดีเซล":"ดีเซล","เบนซิน":"เบนซิน","ไฟฟ้า":"ไฟฟ้า","ไฮบริด":"ไฮบริด","NGV":"NGV"}', true) as $optionKey => $optionValue)
-                                            <option  ption value="{{ $optionKey }}"  {{ (isset($sell->fuel) && $sell->fuel == $optionKey) ? 'selected' : ''}}>{{ $optionValue }}</option>
-                                        @endforeach
-                                    </select>
+                                            <div class="form-group">
+                                                <select class="form-control" name="fuel" id="fuel" value="{{ isset($sell->fuel) ? $sell->fuel : ''}}" >
+                                                        <option value="" selected> - กรุณาเลือกเชื้อเพลิง / Please select fuel - </option> 
+                                                    @foreach (json_decode('{"ดีเซล":"ดีเซล","เบนซิน":"เบนซิน","ไฟฟ้า":"ไฟฟ้า","ไฮบริด":"ไฮบริด","NGV":"NGV"}', true) as $optionKey => $optionValue)
+                                                        <option  ption value="{{ $optionKey }}"  {{ (isset($sell->fuel) && $sell->fuel == $optionKey) ? 'selected' : ''}}>{{ $optionValue }}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
                                         </td>
                                     </tr>
                                     <tr>
-                                        <th> สถานที่ </th>
+                                        <th> <label  class="control-label">{{ 'สถานที่ / Location' }}</label>  </th>
                                         <td> 
                                             <div class="form-group {{ $errors->has('location') ? 'has-error' : ''}}">
                                                 <select name="location" id="location" class="form-control" required>
@@ -81,12 +85,12 @@
                                                         </option>
                                                         @endforeach                                     
                                                 </select>
-                                                {!! $errors->first('province', '<p class="help-block">:message</p>') !!}
+                                                {!! $errors->first('location', '<p class="help-block">:message</p>') !!}
                                             </div> 
                                         </td>
                                     </tr>
                                     <tr>
-                                        <th> จำนวนที่นั่ง </th>
+                                        <th><label  class="control-label">{{ 'จำนวนที่นั่ง / Seat Capacity' }}</label>  </th>
                                         <td>
                                         <div class="form-group {{ $errors->has('seats') ? 'has-error' : ''}}">
                                             <input class="form-control" name="seats" type="number" id="seats" value="{{ isset($sell->seats) ? $sell->seats : ''}}"  >
@@ -95,7 +99,7 @@
                                         </td>
                                     </tr>
                                     <tr>
-                                        <th> ระยะทาง (เลขไมล์)</th>
+                                        <th><label  class="control-label">{{ 'ระยะทาง (เลขไมล์) / Mileage' }}</label> </th>
                                         <td>  
                                         <div class="form-group {{ $errors->has('distance') ? 'has-error' : ''}}">
                                             <input class="form-control" name="distance" type="number" id="distance" value="{{ isset($sell->distance) ? $sell->distance : ''}}"  >
@@ -104,10 +108,18 @@
                                         </td>
                                     </tr>
                                     <tr>
-                                        <th>อัพโหลดรูปเพิ่มเติม</th>
+                                        <th><label  class="control-label">{{ 'ราคา / Price' }}</label>  <span style="color: #FF0033;"> *</span> </th>
+                                        <td>  
+                                        <div class="form-group {{ $errors->has('price') ? 'has-error' : ''}}">
+                                            <input class="form-control" name="price" type="number" id="price" value="{{ isset($sell->price) ? $sell->price : ''}}"  >
+                                            {!! $errors->first('price', '<p class="help-block">:message</p>') !!}
+                                        </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th><label  class="control-label">{{ 'รูปภาพ / Photo' }}</label><span style="color: #FF0033;"> *</span></th>
                                         <td>
                                         <div class="form-group {{ $errors->has('image') ? 'has-error' : ''}}">
-                                            <label for="image" class="control-label">{{ 'รูปภาพ / Photo' }}</label><span style="color: #FF0033;"> *</span>
                                             <input class="form-control" name="image" type="file" id="image" value="{{ isset($sell->image) ? $sell->image : ''}}" required accept="image/*" multiple="multiple">
                                             {!! $errors->first('image', '<p class="help-block">:message</p>') !!}
                                         </div>
@@ -122,32 +134,56 @@
                             {!! $errors->first('active', '<p class="help-block">:message</p>') !!}
                         </div>
 
-                        <form method="POST" action="{{ url('/register_car') }}" accept-charset="UTF-8" class="form-horizontal" enctype="multipart/form-data">
+                        <!-- <form method="POST" action="{{ url('/register_car') }}" accept-charset="UTF-8" class="form-horizontal" enctype="multipart/form-data">
                             {{ csrf_field() }}
                         <div class="form-group {{ $errors->has('active') ? 'has-error' : ''}}">
                             <input class="d-none form-control" name="active" type="text" id="active" value="{{ isset($register_car->active) ? $register_car->active : 'No'}}" >
                             {!! $errors->first('active', '<p class="help-block">:message</p>') !!}
-                        </div>
+                        </div> -->
                         <div class="form-group">
                             <input class="btn btn-primary" type="submit" value="{{ 'บันทึก' }}">
                         </div>
-                        </form>
+                        <!-- </form> -->
                     </form>
                         @elseif($register_car->car_type == "motorcycle")
                         <div class="table-responsive">
                             <table class="table">
                                 <tbody>
+                                    <form method="POST" action="{{ url('/sell') }}" accept-charset="UTF-8" class="form-horizontal" enctype="multipart/form-data">
+                                        {{ csrf_field() }}
+
                                     <tr>
-                                        <th>ID</th><td><img width="40"src="{{ asset('/img/logo_brand/logo-') }}{{ strtolower($register_car->brand) }}.png"></td>
+                                    <th> ยี่ห้อรถ / Brand  </th>
+                                    <td> {{ $register_car->brand }} </td>
                                     </tr>
-                                    <tr><th> ยี่ห้อรถ / Brand  </th><td> {{ $register_car->brand }} </td></tr>
-                                    <tr><th> รุ่นรถ / Model  </th><td> {{ $register_car->generation }} </td></tr>
-                                    <tr><th> ระบบเกียร์ </th><td> {{ $register_car->year }} </td></tr>
-                                    <tr><th> สี </th><td>  </td></tr>
-                                    <tr><th> เครื่องยนต์ (cc) </th><td>  </td></tr>
-                                    <tr><th> สถานที่ </th><td>  </td></tr>
+                                    <tr>
+                                    <th> รุ่นรถ / Model  </th>
+                                    <td> {{ $register_car->generation }} </td>
+                                    </tr>
+                                    <tr>
+                                    <th> ระบบเกียร์ </th>
+                                    <td> {{ $register_car->year }} </td>
+                                    </tr>
+                                    <tr>
+                                    <th> เครื่องยนต์ (cc) </th>
+                                    <td>  </td>
+                                    </tr>
+                                    <tr>
+                                    <th> สถานที่ </th>
+                                    <td>  </td>
+                                    </tr>
+                                    <tr>
+                                    <th> รูปภาพ </th>
+                                    <td>  </td>
+                                    </tr>
+                                   
+                                    
                                 </tbody>
                             </table>
+                                <div class="form-group">
+                                    <input class="btn btn-primary" type="submit" value="{{ 'บันทึก' }}">
+                                </div>
+                            </form>
                         </div>
                         @endif
 
