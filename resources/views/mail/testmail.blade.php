b2a4e267ab9af99603bbae2e006235898342ebf8
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-03-04 00:22:17 +0700

    Update testmail.blade.php

--- resources/views/mail/testmail.blade.php
+++ resources/views/mail/testmail.blade.php
@@ -1,16 +1,15 @@
-@foreach($data as $item)
-	<h1>เรียนคุณ <span style="color: #0066FF ">{{ $item->name }}</span></h1>
 
-	<p>ผู้ใช้รถหมายเลขทะเบียน {{ $item->registration_number }} {{ $item->province }} ที่ท่านได้ติดต่อสักครู่นี้</p>
+<h1>เรียนคุณ <span style="color: #0066FF ">{{ $data["name"] }}</span></h1>
 
-	<p>แจ้งว่า</p><br>
-	<p>
-		@if($item->postback_data == "wait")
-		<b>รอสักครู่ / Wait a moment</b>
-		@endif
+<p>ผู้ใช้รถหมายเลขทะเบียน {{ $data["registration_number"] }} {{ $data["province"] }} ที่ท่านได้ติดต่อสักครู่นี้</p>
 
-		@if($item->postback_data == "thx")
-		<b>ขอบคุณ / Thank you</b>
-		@endif
-	</p>
-@endforeach
+<p>แจ้งว่า</p><br>
+<p>
+	@if($data["postback_data"] == "wait")
+	<b>รอสักครู่ / Wait a moment</b>
+	@endif
+
+	@if($data["postback_data"] == "thx")
+	<b>ขอบคุณ / Thank you</b>
+	@endif
+</p>
