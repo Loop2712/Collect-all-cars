d4ae5cca421853227c49005ecc96e3af5e612359
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-03-03 14:17:48 +0700

    Update LineMessagingAPI.php

--- app/Models/LineMessagingAPI.php
+++ app/Models/LineMessagingAPI.php
@@ -435,96 +435,109 @@ class LineMessagingAPI extends Model
                     ])
 	            ->get();
 
-		// $reply = DB::select("SELECT * FROM register_cars WHERE provider_id = '$provider_id' ");
-
-    	switch($postback_data)
-        {
-        	case "wait": 
-				foreach($reply as $item){
-					$to_user = $item->reply_provider_id;
-                	$template_path = storage_path('../public/json/callback_guest.json');   
-                    $string_json = file_get_contents($template_path);
-                    $string_json = str_replace("ตัวอย่าง","ผู้ใช้แจ้งว่า..",$string_json);
-                    $string_json = str_replace("9กก9999",$item->registration_number,$string_json);
-                    $string_json = str_replace("กรุงเทพมหานคร",$item->province,$string_json);
-                    $string_json = str_replace("ขอบคุณ","รอสักครู่ / Wait a moment",$string_json);
-
-                    $messages = [ json_decode($string_json, true) ];
-	        	}
-                break;
-            case "thx": 
-				foreach($reply as $item){
-					$to_user = $item->reply_provider_id;
-                    $template_path = storage_path('../public/json/callback_guest.json');   
-                    $string_json = file_get_contents($template_path);
-                    $string_json = str_replace("ตัวอย่าง","ผู้ใช้แจ้งว่า..",$string_json);
-                    $string_json = str_replace("9กก9999",$item->registration_number,$string_json);
-                    $string_json = str_replace("กรุงเทพมหานคร",$item->province,$string_json);
-                    $string_json = str_replace("ขอบคุณ","ขอบคุณค่ะ / Thank you",$string_json);
-
-                    $messages = [ json_decode($string_json, true) ];
-	        	}
-                break;
-
+        // type login
+        foreach($reply as $item){
+            
+            $type_login = DB::table('users')
+                        ->select('type')
+                        ->where('provider_id', $item->reply_provider_id)
+                        ->get();
         }
 
-        $body = [
-            "to" => $to_user,
-            "messages" => $messages,
-        ];
+        foreach($type_login as $item){
+            switch ($item->type) {
+                case 'line':
+                    switch($postback_data)
+                    {
+                        case "wait": 
+                            foreach($reply as $item){
+                                $to_user = $item->reply_provider_id;
+                                $template_path = storage_path('../public/json/callback_guest.json');   
+                                $string_json = file_get_contents($template_path);
+                                $string_json = str_replace("ตัวอย่าง","ผู้ใช้แจ้งว่า..",$string_json);
+                                $string_json = str_replace("9กก9999",$item->registration_number,$string_json);
+                                $string_json = str_replace("กรุงเทพมหานคร",$item->province,$string_json);
+                                $string_json = str_replace("ขอบคุณ","รอสักครู่ / Wait a moment",$string_json);
 
-        $opts = [
-            'http' =>[
-                'method'  => 'POST',
-                'header'  => "Content-Type: application/json \r\n".
-                            'Authorization: Bearer '.$this->channel_access_token,
-                'content' => json_encode($body, JSON_UNESCAPED_UNICODE),
-                //'timeout' => 60
-            ]
-        ];
-                            
-        $context  = stream_context_create($opts);
-        $url = "https://api.line.me/v2/bot/message/push";
-        $result = file_get_contents($url, false, $context);
+                                $messages = [ json_decode($string_json, true) ];
+                            }
+                            break;
+                        case "thx": 
+                            foreach($reply as $item){
+                                $to_user = $item->reply_provider_id;
+                                $template_path = storage_path('../public/json/callback_guest.json');   
+                                $string_json = file_get_contents($template_path);
+                                $string_json = str_replace("ตัวอย่าง","ผู้ใช้แจ้งว่า..",$string_json);
+                                $string_json = str_replace("9กก9999",$item->registration_number,$string_json);
+                                $string_json = str_replace("กรุงเทพมหานคร",$item->province,$string_json);
+                                $string_json = str_replace("ขอบคุณ","ขอบคุณค่ะ / Thank you",$string_json);
 
-        //SAVE LOG
-        $data = [
-            "title" => "https://api.line.me/v2/bot/message/push",
-            "content" => json_encode($result, JSON_UNESCAPED_UNICODE),
-        ];
+                                $messages = [ json_decode($string_json, true) ];
+                            }
+                            break;
 
-        DB::table('register_cars')
-                ->where([ ['provider_id', $provider_id],['now', "Yes"] ])
-                ->update(['now' => null]);
+                    }
 
-        MyLog::create($data);
-        return $result;
+                    $body = [
+                        "to" => $to_user,
+                        "messages" => $messages,
+                    ];
 
-  //       $strAccessToken = "VsNZQKpv/ojbmRVXqM6v4PdOHGG5MKQblyKr4LuXo0jyGGRkaNBRLmEBQKE1BzLRNA9SPWTBr4ooOYPusYcwuZjsy6khvF717wmNnAEBu4oeppBc/woRCLiPqz3X5xTCMrEwxvrExidXIidR9SWUxAdB04t89/1O/w1cDnyilFU=";
-     
-  //       $strUrl = "https://api.line.me/v2/bot/message/push";
-         
-  //       $arrHeader = array();
-  //       $arrHeader[] = "Content-Type: application/json";
-  //       $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
-         
-  //       $arrPostData = array();
-  //       $arrPostData['to'] = $to_user;
-    
-  //       $arrPostData['messages'][0]['type'] = "text";
-		// $arrPostData['messages'][0]['text'] = $messages;
+                    $opts = [
+                        'http' =>[
+                            'method'  => 'POST',
+                            'header'  => "Content-Type: application/json \r\n".
+                                        'Authorization: Bearer '.$this->channel_access_token,
+                            'content' => json_encode($body, JSON_UNESCAPED_UNICODE),
+                            //'timeout' => 60
+                        ]
+                    ];
+                                        
+                    $context  = stream_context_create($opts);
+                    $url = "https://api.line.me/v2/bot/message/push";
+                    $result = file_get_contents($url, false, $context);
 
-		// $ch = curl_init();
-  //       curl_setopt($ch, CURLOPT_URL,$strUrl);
-  //       curl_setopt($ch, CURLOPT_HEADER, false);
-  //       curl_setopt($ch, CURLOPT_POST, true);
-  //       curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
-  //       curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
-  //       curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-  //       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-  //       $result = curl_exec($ch);
-  //       curl_close ($ch);
-        
+                    //SAVE LOG
+                    $data = [
+                        "title" => "https://api.line.me/v2/bot/message/push",
+                        "content" => json_encode($result, JSON_UNESCAPED_UNICODE),
+                    ];
+
+                    DB::table('register_cars')
+                            ->where([ ['provider_id', $provider_id],['now', "Yes"] ])
+                            ->update(['now' => null]);
+
+                    MyLog::create($data);
+                    return $result;
+                    break;
+
+                case 'google':
+                    switch($postback_data)
+                    {
+                        case "wait":
+                            //
+                            break;
+                        case "thx":
+                            //
+                            break;
+
+                    }
+                    break;
+
+                case 'facebook':
+                    switch($postback_data)
+                    {
+                        case "wait":
+                            //
+                            break;
+                        case "thx":
+                            //
+                            break;
+
+                    }
+                    break;
+            }
+        }
 
     }
 
