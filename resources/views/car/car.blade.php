d5f2d39ca54949d0a25d004dca7e3c8937f80c8a
Loop2712 <52275890+Loop2712@users.noreply.github.com>
2021-03-11 14:15:54 +0700

    Update car.blade.php

--- resources/views/car/car.blade.php
+++ resources/views/car/car.blade.php
@@ -155,8 +155,7 @@
                     <div class="row">
                     @foreach($data as $item)
                         <div class="col-lg-4 col-md-4">
-                        <form id="my_form" method="POST" action="{{ url('/wishlist') }}" accept-charset="UTF-8" class="form-horizontal" enctype="multipart/form-data" >
-                            {{ csrf_field() }}
+                        
                             <div class="car__item" style="box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.15), 0 4px 10px 0 rgba(0, 0, 0, 0.15);">
                                 <div class="car__item__pic__slider owl-carousel">
                                     @if($item->image == "" )
@@ -203,11 +202,14 @@
                                                 <p class="mb-0 "> <a href="{{ url('/car/'.$item->id ) }}" style="color:#fff;"> <b>&nbsp;ดูข้อมูลเพิ่มเติม</b>  </a></p>
                                             </div>
                                             <div class="whislist">
+                                                <form id="my_form" method="POST" action="{{ url('/wishlist') }}" accept-charset="UTF-8" class="form-horizontal" enctype="multipart/form-data" >
+                                                    {{ csrf_field() }}
                                                     <input class="d-none" name="product_id" type="number" id="product_id" value="{{ $item->id}}" >
                                                     <input class="d-none" name="user_id" type="number" id="user_id" value="" >
                                                     <input class="d-none" name="car_type" type="text" id="car_type" value="car" >
                                                         
                                                     <a href="javascript:{}" onclick="document.getElementById('my_form').submit();"><b>&emsp;ถูกใจ</b></a>    
+                                                </form>
                                             </div>
                                         </div>
                                     </div>
@@ -216,7 +218,7 @@
                                 
                             </div>
                             
-                        </form>
+                        
                         </div>
                         @endforeach 
                     </div>
