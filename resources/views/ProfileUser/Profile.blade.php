3a99ed43e0fd6b327cb831b6dea71ba66197959f
Loop2712 <52275890+Loop2712@users.noreply.github.com>
2021-03-11 15:02:12 +0700

    Update Profile.blade.php

--- resources/views/ProfileUser/Profile.blade.php
+++ resources/views/ProfileUser/Profile.blade.php
@@ -146,12 +146,12 @@
                         <td class="text-primary"> 
                                     <!-- <div class="row"> -->
                                           <div class="col-12 col-md-6">
-                                              <label for="massengbox" class="control-label">&nbsp;&nbsp;&nbsp;รถยนต์</label>
+                                              <label for="massengbox" class="control-label">&nbsp;รถยนต์</label>
                                               <br>
                                               <img src="{{ url('storage')}}/{{ $data->driver_license }}" style="width:200px" /><br/><br/> 
                                           </div>
                                         <div class="col-12 col-md-6">
-                                              <label for="massengbox" class="control-label">&nbsp;&nbsp;&nbsp;รถจักรยานยนต์</label>
+                                              <label for="massengbox" class="control-label">&nbsp;รถจักรยานยนต์</label>
                                               <br>
                                               <img src="{{ url('storage')}}/{{ $data->driver_license2 }}" style="width:200px" /><br/><br/> 
                                         </div>
