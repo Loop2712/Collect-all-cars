c29fee7d99b4284b3bed27c4df9d86b1624ee44a
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-03-11 12:29:46 +0700

    Update GuestController.php

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -415,12 +415,11 @@ class GuestController extends Controller
     public function welcome_line_guest()
     {
         if(Auth::check()){
-            return redirect('guest/create?openExternalBrowser=1');
+            return redirect('guest/create');
             // echo Auth::User()->name;
         }else{
-            return redirect('/login/line?redirectTo=guest/create&openExternalBrowser=1');
+            return redirect('/login/line?redirectTo=guest/create');
         }
-        // return redirect('/login/line?redirectTo=guest/create&openExternalBrowser=1');
     }
 
 }
