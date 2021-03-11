6af98242b932e0f0912c130e75611420ff9df142
Loop2712 <52275890+Loop2712@users.noreply.github.com>
2021-03-11 13:35:18 +0700

    Update index.blade.php

--- resources/views/wishlist/index.blade.php
+++ resources/views/wishlist/index.blade.php
@@ -49,7 +49,7 @@
                         <!-- <ul class="event-time margin-10px-bottom md-margin-5px-bottom">
                             <li><br><i class="fas fa-user margin-5px-right"></i> ชื่อผู้ขาย : John Sminth</li>
                         </ul> -->
-                        <p> สถานที่ {{$item->products->location}}</p>
+                        <p style="font-size:12px"> สถานที่ {{$item->products->location}}</p>
                         <a class="butn small margin-10px-top md-no-margin-top" href="{{ url('/car/'.$item->products->id ) }}" style="color:#000">ดูข้อมูลเพิ่มเติม<i class="fas fa-long-arrow-alt-right margin-10px-left"></i><br><br> </a>
                             <form method="POST" action="{{ url('/wishlist' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
                                     {{ method_field('DELETE') }}
@@ -91,8 +91,8 @@
                         <!-- <ul class="event-time margin-10px-bottom md-margin-5px-bottom">
                             <li><br><i class="fas fa-user margin-5px-right"></i> ชื่อผู้ขาย : John Sminth</li>
                         </ul> -->
-                        <p>สถานที่ {{$item->productM->location}}</p>
-                        <a class="butn small margin-10px-top md-no-margin-top" href="{{ url('/motercycle/'.$item->productM->id ) }}">ดูข้อมูลเพิ่มเติม <i class="fas fa-long-arrow-alt-right margin-10px-left"></i><br><br></a>
+                        <p style="font-size:12px">สถานที่ {{$item->productM->location}}</p>
+                        <a class="butn small margin-10px-top md-no-margin-top" href="{{ url('/motercycle/'.$item->productM->id ) }}" style="color:#000">ดูข้อมูลเพิ่มเติม <i class="fas fa-long-arrow-alt-right margin-10px-left"></i><br><br></a>
                             <form method="POST" action="{{ url('/wishlist' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
                                     {{ method_field('DELETE') }}
                                     {{ csrf_field() }}
