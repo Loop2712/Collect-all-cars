546fb0de0655177c6d9e2f4a10b3d5d8c0df66fd
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-03-20 22:52:57 +0700

    update

--- resources/views/not_comfor/form.blade.php
+++ resources/views/not_comfor/form.blade.php
@@ -1,4 +1,4 @@
-<span style="font-size: 22px;" class="control-label">{{ 'ทางเราขอทราบเหตุผลของคุณเพื่อแจ้งถึงผู้เรียก / We would like to know your reason to inform the caller.'}}</span>
+<span style="font-size: 22px;" class="control-label">{{ 'เหตุผลของท่าน / Please give reasons'}}</span>
 <div class="form-group {{ $errors->has('provider_id') ? 'has-error' : ''}}">
     <!-- <label for="provider_id" class="control-label">{{ 'Provider Id' }}</label> -->
     <input class="form-control" name="provider_id" type="hidden" id="provider_id" value="{{ isset($not_comfor->provider_id) ? $not_comfor->provider_id : Auth::user()->provider_id}}" readonly>
@@ -10,24 +10,30 @@
     {!! $errors->first('reply_provider_id', '<p class="help-block">:message</p>') !!}
 </div>
 <div class="form-group {{ $errors->has('content') ? 'has-error' : ''}}">
-    <label for="content" class="control-label">{{ 'เหตุผล / Because' }}</label><span style="color: #FF0033;"> *</span>
+    <!-- <label for="content" class="control-label">{{ 'เหตุผล / Because' }}</label><span style="color: #FF0033;"> *</span> -->
     <input class="form-control" name="content" type="text" id="content" value="{{ isset($not_comfor->content) ? $not_comfor->content : ''}}" required>
     {!! $errors->first('content', '<p class="help-block">:message</p>') !!}
 </div>
 <div class="form-group {{ $errors->has('want_phone') ? 'has-error' : ''}}">
-    <label for="want_phone" class="control-label">{{ 'คุณต้องการที่จะแสดงเบอร์ของคุณหรือไม่ / Would you like to show your number?' }}</label><span style="color: #FF0033;"> *</span>
+    <label for="want_phone" class="control-label">{{ 'เบอร์โทรศัพท์ของท่าน / Your contact number' }}</label><span style="color: #FF0033;"> *</span>
     <br>
-    <input name="want_phone" type="radio" id="want_phone" value="{{ isset($not_comfor->want_phone) ? $not_comfor->want_phone : 'Yes'}}" 
+    <div class="form-group {{ $errors->has('phone') ? 'has-error' : ''}}">
+        <!-- <label for="phone" class="control-label">{{ 'เบอร์ของคุณ / Your phone number' }}</label> -->
+        <input class="form-control" name="phone" type="text" id="phone" value="{{ isset($not_comfor->phone) ? $not_comfor->phone : Auth::user()->phone }}" required>
+        {!! $errors->first('phone', '<p class="help-block">:message</p>') !!}
+    </div>
+
+    <!-- <input name="want_phone" type="radio" id="want_phone" value="{{ isset($not_comfor->want_phone) ? $not_comfor->want_phone : 'Yes'}}" 
     onclick="document.querySelector('#phone').classList.remove('d-none');"> 
-        &nbsp;&nbsp;&nbsp;แสดง / Show 
-    <br>
-    <input name="want_phone" type="radio" id="want_phone" checked value="{{ isset($not_comfor->want_phone) ? $not_comfor->want_phone : 'No'}}" onclick="document.querySelector('#phone').classList.add('d-none');"> &nbsp;&nbsp;&nbsp;ไม่แสดง / Not showing
+        &nbsp;&nbsp;&nbsp;แสดง / Show  -->
+    <input name="want_phone" type="checkbox" id="want_phone" value="{{ isset($not_comfor->want_phone) ? $not_comfor->want_phone : 'No'}}" onchange="if(this.checked){
+        document.querySelector('#phone').classList.add('d-none'),
+        document.querySelector('#phone').remove('required');
+    }else{
+        document.querySelector('#phone').classList.remove('d-none'),
+        document.querySelector('#phone').add('required');
+    }"> &nbsp;&nbsp;&nbsp;ไม่แสดง / Do not show
     {!! $errors->first('want_phone', '<p class="help-block">:message</p>') !!}
-</div>
-<div class="form-group {{ $errors->has('phone') ? 'has-error' : ''}}">
-    <!-- <label for="phone" class="control-label">{{ 'เบอร์ของคุณ / Your phone number' }}</label> -->
-    <input class="form-control d-none" name="phone" type="text" id="phone" value="{{ isset($not_comfor->phone) ? $not_comfor->phone : Auth::user()->phone }}">
-    {!! $errors->first('phone', '<p class="help-block">:message</p>') !!}
 </div>
 
 
