0d4dff882ca98258fead4e4593952e37dfb5faef
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-03-21 23:55:25 +0700

    Update MailToGuest.blade.php

--- resources/views/mail/MailToGuest.blade.php
+++ resources/views/mail/MailToGuest.blade.php
@@ -1,26 +1,23 @@
 <div class="container" style="box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.15), 0 4px 10px 0 rgba(0, 0, 0, 0.15);">
     <div class="row">
-        <div class="col-12" style="border-style: solid;border-color: #ff0101">
+        <div class="col-12" style="border-style: solid;border-color: #ff0101;border-radius: 10px;background-color: #07375D">
         	<center>
-        		<h1>เรียนคุณ</h1>
-				<h2><span style="color: #0066FF ">{{ $data["name"] }}</span></h2>
-
-				<h4>
-					ผู้ใช้รถหมายเลขทะเบียน <br>
-					<span style="color: #FF0000">{{ $data["registration_number"] }} {{ $data["province"] }}</span><br>
-					ที่ท่านได้ติดต่อเมื่อสักครู่นี้
-				</h4>
-				<h3>แจ้งว่า</h3>
-				<h2>
-					@if($data["postback_data"] == "wait")
-						กรุณารอสักครู่ / Wait a moment
-					@endif
-
-					@if($data["postback_data"] == "thx")
-						ขอบคุณ / Thank you
-					@endif
-				</h2>
+        		<img width="80%" src="{{ asset('/img/logo/logo-flex-line.png') }}">
+        		<hr>
         	</center>
+			<h3 style="color: #FFFFFF">ผู้ใช้รถหมายเลขทะเบียน</h3>
+			<h5 style="color: #FFFFFF">{{ $data["registration_number"] }} {{ $data["province"] }}</h5>
+			<hr>
+			<h3>แจ้งว่า</h3>
+			<h5>
+				@if($data["postback_data"] == "wait")
+					กรุณารอสักครู่ / Wait a moment
+				@endif
+
+				@if($data["postback_data"] == "thx")
+					ขอบคุณ / Thank you
+				@endif
+			</h5>
         </div>
     </div>
 </div>
