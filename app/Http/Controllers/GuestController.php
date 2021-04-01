232b04e799390a88adcd15fae96f3e9d5a9d54a0
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-04-01 22:43:53 +0700

    Update GuestController.php

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -91,7 +91,7 @@ class GuestController extends Controller
             $watermark = Image::make(public_path('watermark.png'));
             $image->insert($watermark , 'bottom-right', 15, 15)->save();
 
-            $image->fit(900, 1200);
+            $image->fit(1200, 900);
             $size = $image->filesize();  
 
             if($size > 112000 ){
