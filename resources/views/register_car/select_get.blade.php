48385def9a1f46da77e12ecd621381902ebc0758
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-04-08 08:11:10 +0700

    Update select_get.blade.php

--- resources/views/register_car/select_get.blade.php
+++ resources/views/register_car/select_get.blade.php
@@ -62,11 +62,6 @@
                                     <hr>
                                     <p>Print & put on the windscreen of your car.</p>
                                 </div>
-                                <div class="col-12">
-                                    <button style="padding-left: 95px;padding-right: 95px; border-radius: 20px; padding-top: 10px; padding-bottom: 10px; font-size: 14px; background-color: #db2d2e; border: none;"  class="btn btn-danger main-shadow"> Download
-                                    </button>
-                                    <br><br>
-                                </div>
                             </div>
                         </div>
                         <div class="col-12">
@@ -76,6 +71,8 @@
                                         <!-- <img src="https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=https://market.viicheck.com/guest/create/&choe=UTF-8"  /> -->
                                         <img width="100%" src="{{ asset('/img/more/sticker-VII-v1.png') }}"/>
                                     </div>
+                                    <button style="padding-left: 95px;padding-right: 95px; border-radius: 20px; padding-top: 10px; padding-bottom: 10px; font-size: 14px; background-color: #db2d2e; border: none;"  class="btn btn-danger main-shadow"> Download
+                                    </button>
                                 </center>
                             </div>
                         </div>
