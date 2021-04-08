1088d2beeeda1db323aebe9ad75423071161f7be
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-04-08 08:13:05 +0700

    Update select_get.blade.php

--- resources/views/register_car/select_get.blade.php
+++ resources/views/register_car/select_get.blade.php
@@ -64,7 +64,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-6">
+                        <div class="col-12">
                             <div class="col-12">
                                 <center>
                                     <div>
@@ -76,13 +76,15 @@
                                 </center>
                             </div>
                         </div>
-                        <div class="col-6">
+                        <div class="col-12">
                             <div class="col-12">
                                 <center>
                                     <div>
                                         <!-- <img src="https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=https://market.viicheck.com/guest/create/&choe=UTF-8"  /> -->
                                         <img width="100%" src="{{ asset('/img/more/sticker-VII-v2-9x9-10.png') }}"/>
                                     </div>
+                                    <button style="padding-left: 95px;padding-right: 95px; border-radius: 20px; padding-top: 10px; padding-bottom: 10px; font-size: 14px; background-color: #db2d2e; border: none;"  class="btn btn-danger main-shadow"> Download
+                                    </button>
                                 </center>
                             </div>
                         </div>
