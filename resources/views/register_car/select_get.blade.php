ce5efbcac3a5d0ab9a6bd5633fe2b779d277a6b7
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-04-08 08:28:13 +0700

    Update select_get.blade.php

--- resources/views/register_car/select_get.blade.php
+++ resources/views/register_car/select_get.blade.php
@@ -58,7 +58,7 @@
                                 </div>
                                 <div class="col-9">
                                     <br>
-                                    <h5 style="line-height: 2;">ปริ้นและนำไปแปะไว้หน้ารถได้เลยค่ะ</h5>
+                                    <h4 style="line-height: 2;">ปริ้นและนำไปแปะไว้หน้ารถได้เลยค่ะ</h4>
                                     <hr>
                                     <p>Print & put on the windscreen of your car.</p>
                                 </div>
