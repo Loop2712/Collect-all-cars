dd216d4611a1e3a59c2c961d9a873e4a0be5e8e6
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-04-08 08:32:19 +0700

    Update select_get.blade.php

--- resources/views/register_car/select_get.blade.php
+++ resources/views/register_car/select_get.blade.php
@@ -5,10 +5,10 @@
 <div class="container d-none d-lg-block">
     <center>
         <div class="row">
-            <div class="col-6">
-                <div class="col-12">
-                    <div class="col-8 offset-2 main-shadow">
-                        <div class="col-8">
+            <div class="col-8 offset-2 main-shadow">
+                <div class="col-6">
+                    <div class="col-12">
+                        <div class="col-6">
                             <center>
                                 <div><br><br>
                                     <!-- <img src="https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=https://market.viicheck.com/guest/create/&choe=UTF-8"  /> -->
@@ -16,28 +16,28 @@
                                 </div>
                             </center>
                         </div>
-                        <div class="col-12">
-                            <a href="{{ asset('/img/more/sticker-VII-v1.png') }}" download >
-                                <div class="row">
-                                    <div class="col-3">
-                                        <br><br><br>
-                                            <img  width="110%" src="{{ asset('/img/stickerline/PNG/25.png') }}">
-                                        <br><br><br>
-                                    </div>
-                                    <div class="col-9">
-                                        <br><br><br>
-                                        <h4 style="line-height: 2;">ปริ้นและนำไปแปะไว้หน้ารถได้เลยค่ะ</h4>
-                                            <hr>
-                                            <p>Print & put on the windscreen of your car.</p>
-                                        <br>
-                                        <button style="padding-left: 95px;padding-right: 95px; border-radius: 20px; padding-top: 10px; padding-bottom: 10px; font-size: 14px; background-color: #db2d2e; border: none;"  class="btn btn-danger main-shadow"> Download
-                                        </button>
-                                        <br><br>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
                     </div>
+                </div>
+                <div class="col-12">
+                    <a href="{{ asset('/img/more/sticker-VII-v1.png') }}" download >
+                        <div class="row">
+                            <div class="col-3">
+                                <br><br><br>
+                                    <img  width="110%" src="{{ asset('/img/stickerline/PNG/25.png') }}">
+                                <br><br><br>
+                            </div>
+                            <div class="col-9">
+                                <br><br><br>
+                                <h4 style="line-height: 2;">ปริ้นและนำไปแปะไว้หน้ารถได้เลยค่ะ</h4>
+                                    <hr>
+                                    <p>Print & put on the windscreen of your car.</p>
+                                <br>
+                                <button style="padding-left: 95px;padding-right: 95px; border-radius: 20px; padding-top: 10px; padding-bottom: 10px; font-size: 14px; background-color: #db2d2e; border: none;"  class="btn btn-danger main-shadow"> Download
+                                </button>
+                                <br><br>
+                            </div>
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
