6d6a0d0ecedb021735c06d492da67bb93feb9f56
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-04-08 08:17:42 +0700

    Update select_get.blade.php

--- resources/views/register_car/select_get.blade.php
+++ resources/views/register_car/select_get.blade.php
@@ -72,7 +72,8 @@
                                         <!-- <img src="https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=https://market.viicheck.com/guest/create/&choe=UTF-8"  /> -->
                                         <img width="65%" src="{{ asset('/img/more/sticker-VII-v1.png') }}"/>
                                     </div>
-                                    <button style="padding-left: 95px;padding-right: 95px; border-radius: 20px; padding-top: 10px; padding-bottom: 10px; font-size: 14px; background-color: #db2d2e; border: none;"  class="btn btn-danger main-shadow"> Download
+                                    <br>
+                                    <button style="padding-left: 50px;padding-right: 50px; border-radius: 20px; padding-top: 10px; padding-bottom: 10px; font-size: 14px; background-color: #db2d2e; border: none;"  class="btn btn-danger main-shadow"> Download
                                     </button>
                                     <br><br>
                                 </center>
@@ -86,7 +87,8 @@
                                         <!-- <img src="https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=https://market.viicheck.com/guest/create/&choe=UTF-8"  /> -->
                                         <img width="65%" src="{{ asset('/img/more/sticker-VII-v2-9x9-10.png') }}"/>
                                     </div>
-                                    <button style="padding-left: 95px;padding-right: 95px; border-radius: 20px; padding-top: 10px; padding-bottom: 10px; font-size: 14px; background-color: #db2d2e; border: none;"  class="btn btn-danger main-shadow"> Download
+                                    <br>
+                                    <button style="padding-left: 50px;padding-right: 50px; border-radius: 20px; padding-top: 10px; padding-bottom: 10px; font-size: 14px; background-color: #db2d2e; border: none;"  class="btn btn-danger main-shadow"> Download
                                     </button>
                                     <br><br>
                                 </center>
