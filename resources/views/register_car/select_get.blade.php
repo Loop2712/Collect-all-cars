22ac58ccaa69fe15e0cdfd00142b8c8b54f2ce3f
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-04-08 08:36:09 +0700

    Update select_get.blade.php

--- resources/views/register_car/select_get.blade.php
+++ resources/views/register_car/select_get.blade.php
@@ -12,8 +12,11 @@
                             <center>
                                 <div><br><br>
                                     <!-- <img src="https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=https://market.viicheck.com/guest/create/&choe=UTF-8"  /> -->
-                                    <img width="100%" src="{{ asset('/img/more/sticker-VII-v2-9x9-10.png') }}"/>
+                                    <img width="100%" src="{{ asset('/img/more/sticker-VII-v1.png') }}"/>
                                 </div>
+                                <br>
+                                <button style="padding-left: 95px;padding-right: 95px; border-radius: 20px; padding-top: 10px; padding-bottom: 10px; font-size: 14px; background-color: #db2d2e; border: none;"  class="btn btn-danger main-shadow" onclick="document.getElementById('sticker_v1').click(); "> Download
+                                </button>
                             </center>
                         </div>
                     </div>
@@ -24,6 +27,9 @@
                                     <!-- <img src="https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=https://market.viicheck.com/guest/create/&choe=UTF-8"  /> -->
                                     <img width="100%" src="{{ asset('/img/more/sticker-VII-v2-9x9-10.png') }}"/>
                                 </div>
+                                <br>
+                                <button style="padding-left: 95px;padding-right: 95px; border-radius: 20px; padding-top: 10px; padding-bottom: 10px; font-size: 14px; background-color: #db2d2e; border: none;"  class="btn btn-danger main-shadow" onclick="document.getElementById('sticker_v2').click(); "> ดาวน์โหลด
+                                </button>
                             </center>
                         </div>
                     </div>
@@ -41,9 +47,6 @@
                                 <h4 style="line-height: 2;">ปริ้นและนำไปแปะไว้หน้ารถได้เลยค่ะ</h4>
                                     <hr>
                                     <p>Print & put on the windscreen of your car.</p>
-                                <br>
-                                <button style="padding-left: 95px;padding-right: 95px; border-radius: 20px; padding-top: 10px; padding-bottom: 10px; font-size: 14px; background-color: #db2d2e; border: none;"  class="btn btn-danger main-shadow"> Download
-                                </button>
                                 <br><br>
                             </div>
                         </div>
@@ -53,7 +56,8 @@
         </div>
     </center>
 </div>
-
+<a class="d-none" id="sticker_v1" href="{{ asset('/img/more/sticker-VII-v1.png') }}" download ></a>
+<a class="d-none" id="sticker_v2" href="{{ asset('/img/more/sticker-VII-v2-9x9-10.png') }}" download ></a>
 <!-- แสดงเฉพาะมือถือ -->
 <div style="margin-left: 2px;" class="container d-block d-md-none">
     <center>
@@ -62,8 +66,6 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-12">
-                            <a class="d-none" id="sticker_v1" href="{{ asset('/img/more/sticker-VII-v1.png') }}" download ></a>
-                            <a class="d-none" id="sticker_v2" href="{{ asset('/img/more/sticker-VII-v2-9x9-10.png') }}" download ></a>
                             <div class="row">
                                 <div class="col-3">
                                     <br>
