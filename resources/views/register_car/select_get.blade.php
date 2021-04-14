9fd8ef31549cd2a5715be33aeb3e8e6b886b308c
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-04-15 06:39:10 +0700

    Update select_get.blade.php

--- resources/views/register_car/select_get.blade.php
+++ resources/views/register_car/select_get.blade.php
@@ -69,16 +69,18 @@
                     <div class="row">
                         <div class="col-12">
                             <div class="row">
-                                <div class="col-3">
-                                    <br>
-                                        <img style="margin-left: -20px;" width="200%" src="{{ asset('/img/stickerline/PNG/25.png') }}">
-                                    <br>
+                                <div class="col-8 offset-2">
+                                    <br><br>
+                                    <center>
+                                        <img  width="75%" src="{{ asset('/img/stickerline/PNG/25.png') }}">
+                                    </center>
                                 </div>
-                                <div class="col-9">
+                                <div class="col-12">
                                     <br>
-                                    <h4 style="line-height: 2;">ปริ้นและนำไปแปะไว้หน้ารถได้เลยค่ะ</h4>
-                                    <hr>
-                                    <p>Print & put on the windscreen of your car.</p>
+                                    <center>
+                                        <h4 style="line-height: 2;">ปริ้นและนำไปแปะรถของท่านได้เลยค่ะ</h4>
+                                        <p>Print & put on the windscreen of your car.</p>
+                                    </center>
                                 </div>
                             </div>
                         </div>
@@ -93,6 +95,7 @@
                                     <img width="120%" src="{{ asset('/img/more/sticker-VII-v1.png') }}"/>
                                 </div>
                                 <br>
+                                <p>English</p>
                                 <button style="padding-left: 40px;padding-right: 40px; border-radius: 20px; padding-top: 10px; padding-bottom: 10px; font-size: 14px; background-color: #db2d2e; border: none;"  class="btn btn-danger main-shadow" onclick="document.getElementById('sticker_v1').click(); "> Download
                                 </button>
                                 <br><br>
@@ -108,6 +111,7 @@
                                     <img width="120%" src="{{ asset('/img/more/sticker-VII-v2-9x9-10.png') }}"/>
                                 </div>
                                 <br>
+                                <p>ภาษาไทย</p>
                                 <button style="padding-left: 40px;padding-right: 40px; border-radius: 20px; padding-top: 10px; padding-bottom: 10px; font-size: 14px; background-color: #db2d2e; border: none;"  class="btn btn-danger main-shadow" onclick="document.getElementById('sticker_v2').click(); "> ดาวน์โหลด
                                 </button>
                                 <br><br>
