bcbfc193f5ad8a70fd3692ca71b76eea596f46c6
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-04-15 06:25:29 +0700

    Update select_get.blade.php

--- resources/views/register_car/select_get.blade.php
+++ resources/views/register_car/select_get.blade.php
@@ -7,6 +7,21 @@
         <div class="row">
             <div class="col-8 offset-2 main-shadow">
                 <div class="row">
+                    <div class="col-12">
+                        <div class="row">
+                            <div class="col-3">
+                                <br><br><br>
+                                    <img  width="110%" src="{{ asset('/img/stickerline/PNG/25.png') }}">
+                                <br><br><br>
+                            </div>
+                            <div class="col-9">
+                                <br><br><br><br>
+                                <h4 style="line-height: 2;">ปริ้นและนำไปแปะไว้หน้ารถได้เลยค่ะ</h4>
+                                    <hr>
+                                    <p>Print & put on the windscreen of your car.</p>
+                            </div>
+                        </div>
+                    </div>
                     <div class="col-6">
                         <div class="col-12">
                             <center>
@@ -31,21 +46,6 @@
                                 <button style="padding-left: 95px;padding-right: 95px; border-radius: 20px; padding-top: 10px; padding-bottom: 10px; font-size: 14px; background-color: #db2d2e; border: none;"  class="btn btn-danger main-shadow" onclick="document.getElementById('sticker_v2').click(); "> ดาวน์โหลด
                                 </button>
                             </center>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12">
-                    <div class="row">
-                        <div class="col-3">
-                            <br><br><br>
-                                <img  width="110%" src="{{ asset('/img/stickerline/PNG/25.png') }}">
-                            <br><br><br>
-                        </div>
-                        <div class="col-9">
-                            <br><br><br><br>
-                            <h4 style="line-height: 2;">ปริ้นและนำไปแปะไว้หน้ารถได้เลยค่ะ</h4>
-                                <hr>
-                                <p>Print & put on the windscreen of your car.</p>
                         </div>
                     </div>
                 </div>
