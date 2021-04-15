2cc25d84a9aa3e54a018da74e4caa6d3db4c61f3
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-04-15 07:00:18 +0700

    Update thx_guest_google.blade.php

--- resources/views/guest/thx_guest_google.blade.php
+++ resources/views/guest/thx_guest_google.blade.php
@@ -4,7 +4,7 @@
 <center>
     <div class="row">
         <div class="col-12">
-			<img style="margin-top: -40px;" width="100%" src="{{ asset('/img/more/ทางเราได้รับข้อมูลของท่านแล้ว-01.jpg') }}">
+			<img style="margin-top: -40px;" width="100%" src="{{ asset('/img/more/ทางเราได้รับข้อมูลของท่านแล้ว-02.jpg') }}">
 			<br><br>
 			
 		</div>
