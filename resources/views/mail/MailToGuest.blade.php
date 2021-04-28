2cdda1c7b8e8fd086d6a663072444fd82a98c513
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-04-29 00:30:57 +0700

    Update MailToGuest.blade.php

--- resources/views/mail/MailToGuest.blade.php
+++ resources/views/mail/MailToGuest.blade.php
@@ -3,24 +3,39 @@
     <div class="row">
         <div class="col-12" style="border-style: solid;border-color: #ff0101;border-radius: 10px;background-color: #07375D;border-width: 5px;margin: 20px;padding: 20px;">
         	<div  class="col-12">
-        		<center>
-	        		<img width="70%" src="{{ asset('/img/logo/logo-flex-line.png') }}">
-	        		<div class="col-11" style="border-style: solid;border-color: #ff0101;border-radius: 10px;background-color: #FAEBD7;border-width: 5px;padding: 20px;margin-top: -50px">
-	        			<h6 style="color: #07375D;"><b>โปรดรอสักครู่</b></h6>
-	        			<br>
-	        			<span style="color: #07375D;line-height: 2;">Please wait a moment</span>
-	        			<br><br>
-	        			<img width="65%" src="{{ asset('/img/stickerline/PNG/8.png') }}">
-	        		</div>
-	        		<hr style="border-style: solid;border-color: #FFFFFF;margin-top: 20px;margin-bottom: 20px;">
-	        	</center>
+        		@if($data["postback_data"] == "wait")
+	        		<center>
+		        		<img width="70%" src="{{ asset('/img/logo/logo-flex-line.png') }}">
+		        		<div class="col-11" style="border-style: solid;border-color: #ff0101;border-radius: 10px;background-color: #FAEBD7;border-width: 5px;padding: 20px;margin-top: -50px">
+		        			<span style="color: #07375D;"><b>โปรดรอสักครู่</b></span>
+		        			<br>
+		        			<span style="color: #07375D;line-height: 2;">Please wait a moment</span>
+		        			<br><br>
+		        			<img width="65%" src="{{ asset('/img/stickerline/PNG/8.png') }}">
+		        		</div>
+		        		<hr style="border-style: solid;border-color: #FFFFFF;margin-top: 20px;margin-bottom: 20px;">
+		        	</center>
+		        	@endif
+				@if($data["postback_data"] == "thx")
+					<center>
+		        		<img width="70%" src="{{ asset('/img/logo/logo-flex-line.png') }}">
+		        		<div class="col-11" style="border-style: solid;border-color: #ff0101;border-radius: 10px;background-color: #FAEBD7;border-width: 5px;padding: 20px;margin-top: -50px">
+		        			<span style="color: #07375D;"><b>ขอบคุณ</b></span>
+		        			<br>
+		        			<span style="color: #07375D;line-height: 2;">Thank you</span>
+		        			<br><br>
+		        			<img width="65%" src="{{ asset('/img/stickerline/PNG/14.png') }}">
+		        		</div>
+		        		<hr style="border-style: solid;border-color: #FFFFFF;margin-top: 20px;margin-bottom: 20px;">
+		        	</center>
+				@endif
 	        	<h3 style="color: #FFFFFF">เวลาที่ตอบกลับ / Time</h3>
-				<h4 style="color: #FFFFFF;line-height: 2;">datetime</h4>
+				<h4 style="color: #FFFFFF;line-height: 2;">{{ $data["datetime"] }}</h4>
 
 				<hr style="border-style: solid;border-color: #FFFFFF;margin-top: 20px;margin-bottom: 20px;">
 
 				<h3 style="color: #FFFFFF">เลขทะเบียน / Plate No.</h3>
-				<h4 style="color: #FFFFFF;line-height: 2;">กก9999 กรุงเทพมหานคร</h4>
+				<h4 style="color: #FFFFFF;line-height: 2;">{{ $data["registration_number"] }} {{ $data["province"] }}</h4>
         	</div>
         </div>
     </div>
