a7de095bd0b0cbc95b5b704c0e47d093b59d982f
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-04-29 00:23:58 +0700

    Update MailToGuest.blade.php

--- resources/views/mail/MailToGuest.blade.php
+++ resources/views/mail/MailToGuest.blade.php
@@ -6,18 +6,18 @@
         		<center>
 	        		<img width="70%" src="{{ asset('/img/logo/logo-flex-line.png') }}">
 	        		<div class="col-11" style="border-style: solid;border-color: #ff0101;border-radius: 10px;background-color: #FAEBD7;border-width: 5px;padding: 20px;margin-top: -50px">
-	        			<span style="color: #07375D;"><b>โปรดรอสักครู่</b></span>
+	        			<h6 style="color: #07375D;"><b>โปรดรอสักครู่</b></h6>
 	        			<br>
 	        			<span style="color: #07375D;line-height: 2;">Please wait a moment</span>
 	        			<br><br>
 	        			<img width="65%" src="{{ asset('/img/stickerline/PNG/8.png') }}">
 	        		</div>
-	        		<hr style="border-style: solid;border-color: #FFFFFF;margin-top: 40px;margin-bottom: 40px;">
+	        		<hr style="border-style: solid;border-color: #FFFFFF;margin-top: 20px;margin-bottom: 20px;">
 	        	</center>
 	        	<h3 style="color: #FFFFFF">เวลาที่ตอบกลับ / Time</h3>
 				<h4 style="color: #FFFFFF;line-height: 2;">datetime</h4>
 
-				<hr style="border-style: solid;border-color: #FFFFFF;margin-top: 40px;margin-bottom: 40px;">
+				<hr style="border-style: solid;border-color: #FFFFFF;margin-top: 20px;margin-bottom: 20px;">
 
 				<h3 style="color: #FFFFFF">เลขทะเบียน / Plate No.</h3>
 				<h4 style="color: #FFFFFF;line-height: 2;">กก9999 กรุงเทพมหานคร</h4>
