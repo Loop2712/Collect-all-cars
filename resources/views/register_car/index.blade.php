3f9bf2de18ae092e17d87204410a6c729a120b0a
luxlnw171041 <54930672+luxlnw171041@users.noreply.github.com>
2021-05-10 16:51:25 +0700

    Update index.blade.php

--- resources/views/register_car/index.blade.php
+++ resources/views/register_car/index.blade.php
@@ -213,7 +213,8 @@
                                                     </div>
                                                 </div>
                                                 <br>
-                                                <hr style="margin-top: -25px;">
+                                                <hr class="d-none d-lg-block" style="margin-top: -5px;">
+                                                <hr class="row d-block d-md-none" style="margin-top: -25px;">
                                             </div>
                                         </div>
 
@@ -353,7 +354,7 @@
                                                             <br>
                                                             <h5 style="position: relative; z-index: 5">{{ $item->registration_number }}</h5>
                                                             <p style="position: relative; color: #000000; z-index: 5">{{ $item->province }} </p>
-                                                            <img style="position: absolute;right: 14%;top: 16%;z-index: 2" width="200"src="{{ asset('/img/icon/ป้ายทะเบียน.png') }}">
+                                                            <img style="margin-top: -110px; z-index: 2" width="200"src="{{ asset('/img/icon/ป้ายทะเบียน.png') }}">
                                                         </center>
                                                     </div>
                                                 </div>
@@ -369,7 +370,8 @@
                                                     </div>
                                                 </div>
                                                 <br>
-                                                <hr style="margin-top: -5px;">
+                                                <hr class="d-none d-lg-block" style="margin-top: -5px;">
+                                                <hr class="row d-block d-md-none" style="margin-top: -25px;">
                                             </div>
                                         </div>
 
