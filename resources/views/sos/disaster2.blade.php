b4bc02391008b297dd8c70639d33f928d56bf256
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-05-25 12:00:13 +0700

    Update disaster2.blade.php

--- resources/views/sos/disaster2.blade.php
+++ resources/views/sos/disaster2.blade.php
@@ -1,5 +1,6 @@
 <a id="tel191" class="d-none" href="tel:191"></a>
-<a id="btn_add_line" class="d-none" href="https://lin.ee/xnFKMfc">
+<a id="btn_add_line" class="" href="https://lin.ee/xnFKMfc" onclick="btn_add_line90();">
+	<img style="margin-top: -40px;" width="100%" src="{{ asset('/img/more/ทางเราได้รับข้อมูลของท่านแล้ว-02.jpg') }}">
 </a>
 <script>
     document.addEventListener('DOMContentLoaded', (event) => {
@@ -7,9 +8,9 @@
         setTimeout(function(){ 
           document.getElementById("tel191").click(); 
         }, 100);
-        setTimeout(function(){ 
-          document.getElementById("btn_add_line").click(); 
-        }, 2000);
 
     });
+    function btn_add_line(){
+        document.getElementById("btn_add_line").click();
+    }
 </script>
