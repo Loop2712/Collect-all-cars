a99104f42d0a77b123be7de570acede1fd63b507
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-05-25 13:11:12 +0700

    update

--- database/migrations/2021_05_25_055846_change_columns_org_id_to_register_cars_table.php
+++ database/migrations/2021_05_25_055846_change_columns_org_id_to_register_cars_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddOrganizationIdToRegisterCarsTable extends Migration
+class ChangeColumnsOrgIdToRegisterCarsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,8 +14,7 @@ class AddOrganizationIdToRegisterCarsTable extends Migration
     public function up()
     {
         Schema::table('register_cars', function (Blueprint $table) {
-            $table->string('organization_id')->nullable();
-            $table->string('organization_mail')->nullable();
+            $table->renameColumn('organization_id', 'juristicID');
         });
     }
 
