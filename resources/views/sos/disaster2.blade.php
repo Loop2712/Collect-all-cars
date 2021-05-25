26f248445e26067faad971a24b54c5d082e63dd2
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-05-25 11:52:34 +0700

    update

--- resources/views/sos/disaster2.blade.php
+++ resources/views/sos/disaster2.blade.php
@@ -1,9 +1,15 @@
 <a id="tel191" class="d-none" href="tel:191"></a>
+<a id="btn_add_line" class="d-none" href="https://lin.ee/xnFKMfc">
+</a>
 <script>
     document.addEventListener('DOMContentLoaded', (event) => {
         // console.log("START"); 
         setTimeout(function(){ 
           document.getElementById("tel191").click(); 
         }, 100);
+        setTimeout(function(){ 
+          document.getElementById("btn_add_line").click(); 
+        }, 2000);
+
     });
 </script>
