6e182dc1ef85b3c219324edadb379bd565fdd3d4
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-06-01 12:17:22 +0700

    Update MailToCompany.php

--- app/Mail/MailToCompany.php
+++ app/Mail/MailToCompany.php
@@ -2,7 +2,7 @@
 
 namespace App\Mail;
 use App\Models\LineMessagingAPI;
-
+use App\Models\Guest;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
