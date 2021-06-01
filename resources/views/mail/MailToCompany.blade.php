22be9ee201784ea9f54c78bcc4213bac616b176f
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-06-01 13:18:21 +0700

    Update MailToCompany.blade.php

--- resources/views/mail/MailToCompany.blade.php
+++ resources/views/mail/MailToCompany.blade.php
@@ -4,13 +4,15 @@
 			<div>
 				<h1>เรียน บริษัท <b>{{ $data["juristicNameTH"] }}</b></h1>
 				<br>
-				ทางเราได้รับการแจ้งปัญหาการขับขี่ของยานพาหนะภายในองค์กรของท่าน <b>{{ $data["juristicNameTH"] }} {{ $data["branch"] }} {{ $data["branch_district"] }} {{ $data["branch_province"] }}</b>
+				ทางเราได้รับการแจ้งปัญหาการขับขี่ยานพาหนะภายในองค์กรของท่าน
 				<br>
-				เมื่อเวลา $data["datetime"]
+				สาขา <b>{{ $data["branch"] }}</b> อำเภอ/เขต <b>{{ $data["branch_district"] }}</b> จังหวัด <b>{{ $data["branch_province"] }}</b>
+				<br>
+				เมื่อเวลา {{ $data["datetime"] }}
 				<br>
 				หมายเลขทะเบียน <b>{{ $data["registration_number"] }} {{ $data["province"] }} </b>
 				<br>
-				ปัญหาการขับขี่ที่ได้รับแข้งคือ <b>{{ $data["masseng"] }}</b>
+				ปัญหาการขับขี่ที่ได้รับแจ้งคือ <b>{{ $data["masseng"] }}</b>
 			</div>
 		</div>
 	</div>
