ba28cd391e31b20b4f269951d42cd53c5d43942c
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-06-07 23:31:41 +0700

    Update MailToCompany.blade.php

--- resources/views/mail/MailToCompany.blade.php
+++ resources/views/mail/MailToCompany.blade.php
@@ -4,9 +4,9 @@
 	<div class="container">
 		<div class="row">
 			<!-- คอม -->
-			<div class="col-md-12 d-none d-lg-block">
+			<div class="col-md-12">
 				<div class="row">
-					<div class="col-md-8" style="font-size:18px;">
+					<div class="col-md-8" style="font-size:16px;">
 						<h3>เรียน บริษัท <b>{{ $data["juristicNameTH"] }}</b></h3>
 						
 						ทางเราได้รับการแจ้งปัญหาการขับขี่ยานพาหนะภายในองค์กรของท่าน
@@ -16,9 +16,9 @@
 						<br><hr><br>
 						เมื่อเวลา {{ $data["datetime"] }}
 						<br>
-						หมายเลขทะเบียน <b style="font-size:20px;">{{ $data["registration_number"] }} {{ $data["province"] }} </b>
+						หมายเลขทะเบียน <b style="font-size:18px;">{{ $data["registration_number"] }} {{ $data["province"] }} </b>
 						<br><br>
-						ปัญหาการขับขี่ที่ได้รับแจ้งคือ <b style="font-size:20px;color: red;">{{ $data["masseng"] }}</b>
+						ปัญหาการขับขี่ที่ได้รับแจ้งคือ <b style="font-size:18px;color: red;">{{ $data["masseng"] }}</b>
 						<br><br>
 					</div>
 					<!-- <div class="col-md-4">
@@ -28,54 +28,11 @@
 				</div>
 				@if(!empty($data["phone"]))
 				<div class="row">
-					<div class="col-md-12" style="font-size:20px;">
+					<div class="col-md-12" style="font-size:18px;">
 						เบอร์โทรศัพท์ติดต่อผู้แจ้ง <b> {{ $data["phone"] }}</b>
 						<br>
 					</div>
 					<div class="col-md-2">
-						<br>
-						<button type="button" style="background-color: #69b528; color:#ffffff;text-decoration: none;padding: 10px;border-radius: 10px;">
-							<a type="button" style="background-color: #69b528; color:#ffffff;text-decoration: none;" href="tel:{{ $data['phone'] }}">
-								📞 โทรออก
-							</a>
-						</button>
-					</div>
-				</div>
-				@endif
-			</div>
-
-			<!-- มือถือ -->
-			<div class="col-12 d-block d-none">
-				<div class="col-12" style="font-size:14px;">
-					<img width="200" src="{{ asset('/img/stickerline/PNG/37.png') }}">
-					<br><br>
-					<h5>เรียน บริษัท <b>{{ $data["juristicNameTH"] }}</b></h5>
-					
-					ทางเราได้รับการแจ้งปัญหาการขับขี่ยานพาหนะภายในองค์กรของท่าน
-					<br>
-					<b>สาขา</b> {{ $data["branch"] }}
-					<br>
-					<b>อำเภอ/เขต</b> {{ $data["branch_district"] }}
-					<br>
-					<b>จังหวัด</b> {{ $data["branch_province"] }}
-					<br>
-					<br><hr><br>
-					เมื่อเวลา {{ $data["datetime"] }}
-					<br>
-					หมายเลขทะเบียน <b style="font-size:17px;"><br>
-					{{ $data["registration_number"] }} {{ $data["province"] }} </b>
-					<br><br>
-					ปัญหาการขับขี่ที่ได้รับแจ้งคือ <b style="font-size:17px;color: red;"><br>
-					{{ $data["masseng"] }}</b>
-					<br><br>
-				</div>
-				@if(!empty($data["phone"]))
-				<div class="row">
-					<div class="col-12" style="font-size:14px;">
-						เบอร์โทรศัพท์ติดต่อผู้แจ้ง <b> {{ $data["phone"] }}</b>
-						<br>
-					</div>
-					<div class="col-2">
 						<br>
 						<button type="button" style="background-color: #69b528; color:#ffffff;text-decoration: none;padding: 10px;border-radius: 10px;">
 							<a type="button" style="background-color: #69b528; color:#ffffff;text-decoration: none;" href="tel:{{ $data['phone'] }}">
