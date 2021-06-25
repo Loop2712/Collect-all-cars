577428a8cd3c6b0bb8beef27dd9246b0ebf73f3b
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-06-25 15:16:57 +0700

    update env('CHANNEL_ACCESS_TOKEN');

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -216,7 +216,7 @@ class GuestController extends Controller
         return redirect('guest')->with('flash_message', 'Guest deleted!');
     }
 
-    public $channel_access_token = "VsNZQKpv/ojbmRVXqM6v4PdOHGG5MKQblyKr4LuXo0jyGGRkaNBRLmEBQKE1BzLRNA9SPWTBr4ooOYPusYcwuZjsy6khvF717wmNnAEBu4oeppBc/woRCLiPqz3X5xTCMrEwxvrExidXIidR9SWUxAdB04t89/1O/w1cDnyilFU=";
+    public $channel_access_token = env('CHANNEL_ACCESS_TOKEN');
 
     protected function _pushLine($data)
     {   
