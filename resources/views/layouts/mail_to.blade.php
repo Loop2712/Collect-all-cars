6f8a1123a5f43e83ba758f5172306bdf1c173b64
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-06-28 20:53:51 +0700

    Update mail_to.blade.php

--- resources/views/layouts/mail_to.blade.php
+++ resources/views/layouts/mail_to.blade.php
@@ -17,6 +17,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="{{ asset('Medilab/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
 
 
     <!-- Styles -->
