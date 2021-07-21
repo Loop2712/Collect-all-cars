4ee36f4567156cd7273fc6bcd1a05962b90e96bd
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-07-21 15:49:51 +0700

    Update Sos_mapController.php

--- app/Http/Controllers/Sos_mapController.php
+++ app/Http/Controllers/Sos_mapController.php
@@ -73,7 +73,9 @@ class Sos_mapController extends Controller
         switch ($requestData['content']) {
             case 'help_area':
                 // ตรวจสอบ area แล้วส่งข้อมูลผ่านไลน์ 
-                // $this->_pushLine($requestData);
+
+                $this->_pushLine($requestData);
+
                 return redirect('/sos_thank_area')->with('flash_message', 'Sos_map added!');
                 break;
             case 'police':
@@ -166,6 +168,49 @@ class Sos_mapController extends Controller
         }else{
             return redirect('login/line?redirectTo=sos_map/create');
         }
+    }
+
+    public $channel_access_token = "VsNZQKpv/ojbmRVXqM6v4PdOHGG5MKQblyKr4LuXo0jyGGRkaNBRLmEBQKE1BzLRNA9SPWTBr4ooOYPusYcwuZjsy6khvF717wmNnAEBu4oeppBc/woRCLiPqz3X5xTCMrEwxvrExidXIidR9SWUxAdB04t89/1O/w1cDnyilFU=";
+
+    protected function _pushLine($data)
+    {   
+        $template_path = storage_path('../public/json/flex-accident.json');   
+        $string_json = file_get_contents($template_path);
+        $string_json = str_replace("ตัวอย่าง",$masseng,$string_json);
+
+        $messages = [ json_decode($string_json, true) ];
+
+        switch ($data['area']) {
+            case 'ทดสอบ':
+                $body = [
+                    "to" => "U912994894c449f2237f73f18b5703e89",
+                    "messages" => $messages,
+                ];
+                break;
+        }
+
+        $opts = [
+            'http' =>[
+                'method'  => 'POST',
+                'header'  => "Content-Type: application/json \r\n".
+                            'Authorization: Bearer '.$this->channel_access_token,
+                'content' => json_encode($body, JSON_UNESCAPED_UNICODE),
+                //'timeout' => 60
+            ]
+        ];
+                            
+        $context  = stream_context_create($opts);
+        $url = "https://api.line.me/v2/bot/message/push";
+        $result = file_get_contents($url, false, $context);
+
+        //SAVE LOG
+        $data = [
+            "title" => "https://api.line.me/v2/bot/message/push",
+            "content" => json_encode($result, JSON_UNESCAPED_UNICODE),
+        ];
+        MyLog::create($data);
+        return $result;
+        
     }
 
 
