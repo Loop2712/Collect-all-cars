2535e5caaf3d82ef496ca94ef1162ad85a9148e7
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-07-21 14:22:15 +0700

    update

--- public/js/sos_map.js
+++ public/js/sos_map.js
@@ -31,40 +31,99 @@ document.addEventListener('DOMContentLoaded', (event) => {
         let location_user = document.querySelector("#location_user");
             location_user.innerHTML = '<a class="btn-block shadow-box text-white btn btn-primary" id="submit"><i class="fas fa-search-location"></i> ตำแหน่งของฉัน</a>';
 
-        // // พื้นที่ ทดสอบ
-        //     const test_a = { lat: 14.1150621, lng: 100.6013697 };
-        //     const test_b = { lat: 14.1150621, lng: 100.6074465 };
-
-        //     const test_c = { lat: 14.1127626, lng: 100.6013697 };
-        //     const test_d = { lat: 14.1127626, lng: 100.6074465 };
-        // // END พื้นที่ ทดสอบ
-
-        // // พื้นที่ VRU 
-        //     const vru_a = { lat: 14.1357294, lng: 100.6054468 };
-        //     const vru_b = { lat: 14.1357294, lng: 100.6179993 };
-
-        //     const vru_c = { lat: 14.1319187, lng: 100.6054468 };
-        //     const vru_d = { lat: 14.1319187, lng: 100.6179993 };
-        // // END พื้นที่ VRU
-
         // ตรวจสอบว่าอยู่ในพื้นที่บริการไหน
 
-        if (lat < 14.1150621 && lat > 14.1127626 && lng > 100.6013697 && lng < 100.6074465) {
+        // // พื้นที่ ทดสอบ
+        const area_test = [
+                [ 14.1150621, 100.6013697 ],
+                [ 14.1150621, 100.6074465 ],
+                [ 14.1127626, 100.6074465 ],
+                [ 14.1127626, 100.6013697 ],
+            ];
+
+        // // พื้นที่ VRU
+        const area_vru = [
+                [ 14.1357294, 100.6054468 ],
+                [ 14.1357294, 100.6179993 ],
+                [ 14.1319187, 100.6179993 ],
+                [ 14.1319187, 100.6054468 ],
+                
+              ];
+
+        // // พื้นที่ มธ
+            const area_tu = [
+                [ 14.077896, 100.593465 ],
+                [ 14.080425, 100.600798 ],
+                [ 14.076168, 100.600594 ],
+                [ 14.076193, 100.617410 ],
+                [ 14.066614, 100.616926 ],
+                [ 14.066762, 100.605290 ],
+                [ 14.065550, 100.605443 ],
+                [ 14.066020, 100.597200 ],
+                [ 14.075945, 100.596154 ],
+                [ 14.076391, 100.594189 ],
+            ];
+
+        // พื้นที่ บ้านลัค
+            const area_luck = [
+                [ 14.187077, 101.162882 ],
+                [ 14.187651, 101.162502 ],
+                [ 14.188048, 101.163155 ],
+                [ 14.188600, 101.163246 ],
+                [ 14.188644, 101.164103 ],
+                [ 14.187386, 101.164194 ],
+                [ 14.187077, 101.164133 ],
+            ];
+
+        if ( inside([ lat, lng ], area_test) ) {
             // พื้นที่ทดสอบ
             document.querySelector('#a_help').classList.remove('d-none');
             let area_help = document.querySelector("#area_help");
                 area_help.innerHTML = "ทดสอบ"
-        } else if (lat < 14.1357294 && lat > 14.1319187 && lng > 100.6054468 && lng < 100.6179993) {
+
+        } else if ( inside([ lat, lng ], area_vru) ) {
             // VRU
             document.querySelector('#a_help').classList.remove('d-none');
             let area_help = document.querySelector("#area_help");
                 area_help.innerHTML = "VRU"
-        }else{
+
+        } else if ( inside([ lat, lng ], area_tu) ) {
+            // TU
+            document.querySelector('#a_help').classList.remove('d-none');
+            let area_help = document.querySelector("#area_help");
+                area_help.innerHTML = "TU"
+
+        } else if ( inside([ lat, lng ], area_luck) ) {
+            // luck
+            document.querySelector('#a_help').classList.remove('d-none');
+            let area_help = document.querySelector("#area_help");
+                area_help.innerHTML = "ควยลัค"
+
+        } else{
             document.querySelector('#btn_quick_help').classList.remove('d-none');
         }
         
         // END ตรวจสอบว่าอยู่ในพื้นที่บริการไหน
     }
+
+    function inside(point, vs) {
+
+        var x = point[0], y = point[1];
+        
+        var inside = false;
+
+        for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+            var xi = vs[i][0], yi = vs[i][1];
+            var xj = vs[j][0], yj = vs[j][1];
+            
+            var intersect = ((yi > y) != (yj > y))
+                && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+            if (intersect) inside = !inside;
+        }
+        // console.log(inside);
+        return inside;
+
+    };
 
     function initMap(position) {
         let lat_text = document.querySelector("#lat");
@@ -86,14 +145,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
             const user = { lat: lat, lng: lng };
             const marker_user = new google.maps.Marker({ map, position: user });
 
+        // DRAW AREA
+
         // พื้นที่ทดสอบ
             const area_test = [
                 { lat: 14.1150621, lng: 100.6013697 },
                 { lat: 14.1150621, lng: 100.6074465 },
                 { lat: 14.1127626, lng: 100.6074465 },
                 { lat: 14.1127626, lng: 100.6013697 },
-                
               ];
+
               // Construct the polygon.
               const draw_area_test = new google.maps.Polygon({
                 paths: area_test,
@@ -141,7 +202,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
             const area_tu = [tu_a,tu_b,tu_c,tu_d,tu_e,tu_f,tu_g,tu_h,tu_i,tu_j,];
 
               // Construct the polygon.
-              const draw_area_tu = new google.maps.Polygon({
+            const draw_area_tu = new google.maps.Polygon({
                 paths: area_tu,
                 strokeColor: "#FF0000",
                 strokeOpacity: 0.8,
@@ -150,7 +211,30 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 fillOpacity: 0.35,
               });
               draw_area_tu.setMap(map);
-        // // END พื้นที่ TU 
+        // // END พื้นที่ TU
+
+        // พื้นที่ บ้านลัค
+            const luck_a = { lat: 14.187077, lng: 101.162882 };
+            const luck_b = { lat: 14.187651, lng: 101.162502 };
+            const luck_c = { lat: 14.188048, lng: 101.163155 };
+            const luck_d = { lat: 14.188600, lng: 101.163246 };
+            const luck_e = { lat: 14.188644, lng: 101.164103 };
+            const luck_f = { lat: 14.187386, lng: 101.164194 };
+            const luck_h = { lat: 14.187077, lng: 101.164133 };
+
+            const area_luck = [luck_a,luck_b,luck_c,luck_d,luck_e,luck_f,luck_h,];
+
+              // Construct the polygon.
+            const draw_area_luck = new google.maps.Polygon({
+                paths: area_luck,
+                strokeColor: "#FF0000",
+                strokeOpacity: 0.8,
+                strokeWeight: 1,
+                fillColor: "#FF0000",
+                fillOpacity: 0.35,
+              });
+              draw_area_luck.setMap(map);
+        // // END พื้นที่ บ้านลัค
 
 
         const geocoder = new google.maps.Geocoder();
