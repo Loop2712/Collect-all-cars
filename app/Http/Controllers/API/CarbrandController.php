59f120016b6547f720353722bbfedb58d19232de
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-07-30 18:30:58 +0700

    update

--- app/Http/Controllers/API/CarbrandController.php
+++ app/Http/Controllers/API/CarbrandController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\CarModel;
 use DB;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class CarbrandController extends Controller
 {
@@ -141,4 +142,27 @@ class CarbrandController extends Controller
         return $registration;
     }
 
+    public function download_sticker($text_1 , $text_2)
+    {
+        $image_sticker = Image::make(public_path('img/more/sticker-VII-v-nonetext.png'));
+        $image_sticker->backup();
+
+
+        $image_sticker->text($text_1, 170, 320, function($font) {
+                $font->size(50);
+                $font->color('#FFFFFF');
+            });
+
+        $image_sticker->save();
+
+        // echo $image_sticker;
+        echo $text_1 ;
+        echo "<br>";
+        echo $text_2 ;
+
+
+
+        return redirect('/');
+
+    }
 }
