06df2a482a41ca27a49ec40d531de080a3456bbe
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-08-05 15:53:22 +0700

    Update form.blade.php

--- resources/views/ProfileUser/form.blade.php
+++ resources/views/ProfileUser/form.blade.php
@@ -215,7 +215,7 @@
         var context = canvas.getContext('2d');
 
         if (navigator.mediaDevices.getUserMedia) {
-          navigator.mediaDevices.getUserMedia({ video: true })
+          navigator.mediaDevices.getUserMedia({ video: { facingMode: { exact: "environment" } } })
             .then(function (stream) {
               if (typeof video.srcObject == "object") {
                   video.srcObject = stream;
@@ -267,65 +267,5 @@
         //     dl_img.setAttributeNode(att);
 
     }
-
-    const videoElement = document.querySelector('video');
-    const videoSelect = document.querySelector('select#videoSource');
-    const selectors = [videoSelect];
-
-    function gotDevices(deviceInfos) {
-      // Handles being called several times to update labels. Preserve values.
-      const values = selectors.map(select => select.value);
-      selectors.forEach(select => {
-        while (select.firstChild) {
-          select.removeChild(select.firstChild);
-        }
-      });
-      for (let i = 0; i !== deviceInfos.length; ++i) {
-        const deviceInfo = deviceInfos[i];
-        const option = document.createElement('option');
-        option.value = deviceInfo.deviceId;
-        if (deviceInfo.kind === 'videoinput') {
-          option.text = deviceInfo.label || `camera ${videoSelect.length + 1}`;
-          videoSelect.appendChild(option);
-        } else {
-          console.log('Some other kind of source/device: ', deviceInfo);
-        }
-      }
-      selectors.forEach((select, selectorIndex) => {
-        if (Array.prototype.slice.call(select.childNodes).some(n => n.value === values[selectorIndex])) {
-          select.value = values[selectorIndex];
-        }
-      });
-    }
-
-    navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
-
-
-    function gotStream(stream) {
-      window.stream = stream; // make stream available to console
-      videoElement.srcObject = stream;
-      // Refresh button list in case labels have become available
-      return navigator.mediaDevices.enumerateDevices();
-    }
-
-    function handleError(error) {
-      console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
-    }
-
-    function start() {
-      if (window.stream) {
-        window.stream.getTracks().forEach(track => {
-          track.stop();
-        });
-      }
-      const videoSource = videoSelect.value;
-      const constraints = {
-        video: {deviceId: videoSource ? {exact: videoSource} : undefined}
-      };
-      navigator.mediaDevices.getUserMedia(constraints).then(gotStream).then(gotDevices).catch(handleError);
-    }
-
-
-    videoSelect.onchange = start;
-
+    
 </script>
