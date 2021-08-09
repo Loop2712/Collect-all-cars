ba554cfe3cddbb38b5479ea34ce1ca44350013a0
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-08-09 12:08:02 +0700

    Update form.blade.php

--- resources/views/guest/form.blade.php
+++ resources/views/guest/form.blade.php
@@ -395,7 +395,7 @@
         var text_img = document.querySelector("#text_img");
         var context = canvas.getContext('2d');
 
-        context.drawImage(video, 10, 100, 500, 255, 0, 0, 250, 100);
+        context.drawImage(video, 10, 100, 450, 255, 0, 0, 250, 100);
         photo2.setAttribute('src',canvas.toDataURL('image/png'));
         text_img.value = canvas.toDataURL('image/png');
 
