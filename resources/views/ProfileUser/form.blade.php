9de097a127c4760b6a631ac6a931ae82d6c02ffe
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-08-16 16:59:01 +0700

    Update form.blade.php

--- resources/views/ProfileUser/form.blade.php
+++ resources/views/ProfileUser/form.blade.php
@@ -12,12 +12,12 @@
                     <div class="col-12 col-md-6">
                         <label  class="control-label"><b>{{ 'รูปภาพโปรไฟล์' }}</b></label>
                         <div class="form-group {{ $errors->has('photo') ? 'has-error' : ''}}">
-                            <input class="form-control" name="photo" type="file" id="photo" value="{{ isset($data->photo) ? $data->photo : ''}}" accept="image/*" multiple="multiple" onchange="document.querySelector('#img_old').classList.add('d-none');">
+                            <input class="form-control" name="photo" type="file" id="photo" value="{{ isset($data->photo) ? $data->photo : ''}}" accept="image/*" multiple="multiple" onchange="document.querySelector('#img_profile_old').classList.add('d-none');">
                                 {!! $errors->first('photo', '<p class="help-block">:message</p>') !!}
                         </div>
                         <br>
                         <center>
-                            <div id="img_old" class="">
+                            <div id="img_profile_old" class="">
                                 @if(!empty($data->avatar) and empty($data->photo))
                                     <img style="border-radius: 50%;" width="300" src="{{ $data->avatar }}" class="img-circle img-thumbnail isTooltip">
                                 @endif
@@ -29,7 +29,7 @@
                                 @endif
                             </div>
                             
-                            <div id="dvPreview"></div>
+                            <div id="img_profile_new"></div>
                         </center>
                             
                     </div>
@@ -93,24 +93,32 @@
                     <div class="col-12 col-md-6">
                         <label class="control-label"><b>{{ 'รถยนต์' }}</b></label>
                         <div class="form-group {{ $errors->has('driver_license') ? 'has-error' : ''}}">
-                            <input class="form-control" name="driver_license" type="file" id="driver_license" value="{{ isset($data->driver_license) ? $data->driver_license : ''}}" >
+                            <input class="form-control" name="driver_license" type="file" id="driver_license" value="{{ isset($data->driver_license) ? $data->driver_license : ''}}" onchange="document.querySelector('#driver_license_old').classList.add('d-none');">
                             {!! $errors->first('driver_license', '<p class="help-block">:message</p>') !!}
                         </div>
                         <br>
                         <center>
-                            <img width="250" src="{{ url('storage')}}/{{ $data->driver_license }}">
+                            <div id="driver_license_old" class="">
+                                <img width="250" src="{{ url('storage')}}/{{ $data->driver_license }}">
+                            </div>
+                            <div id="driver_license_new"></div>
                         </center>
                         <br>
                     </div>
                     <div class="col-12 col-md-6">
                         <label class="control-label"><b>{{ 'รถจักยานยนต์' }}</b></label>
                         <div class="form-group {{ $errors->has('driver_license2') ? 'has-error' : ''}}">
-                            <input class="form-control" name="driver_license2" type="file" id="driver_license2" value="{{ isset($data->driver_license2) ? $data->driver_license2 : ''}}" >
+                            <input class="form-control" name="driver_license2" type="file" id="driver_license2" value="{{ isset($data->driver_license2) ? $data->driver_license2 : ''}}" onchange="document.querySelector('#driver_license2_old').classList.add('d-none');">
                             {!! $errors->first('driver_license2', '<p class="help-block">:message</p>') !!}
                         </div>
                         <br>
                         <center>
-                            <img width="250" src="{{ url('storage')}}/{{ $data->driver_license2 }}">
+                            <div id="driver_license2_old" class="">
+                                <img width="250" src="{{ url('storage')}}/{{ $data->driver_license2 }}">
+                            </div>
+                            <div id="driver_license2_new"></div>
+                        </center>
+                        <br>
                     </div>
                 </div>
             </div>
@@ -128,23 +136,136 @@
                         <label class="control-label"><b>{{ 'รถยนต์' }}</b></label>
                         <div class="form-group {{ $errors->has('driver_license') ? 'has-error' : ''}}">
                             <center>
-                                <button type="button" class="btn btn-sm btn-outline-info main-shadow main-radius" onclick="document.querySelector('#driver_license_capture').classList.remove('d-none');">
+                                <button id="btn_click_capture" type="button" class="btn btn-sm btn-outline-info main-shadow main-radius" onclick="
+                                document.querySelector('#driver_license_capture').classList.remove('d-none'),
+                                document.querySelector('#btn_click_capture').classList.add('d-none'),
+                                capture_driver_license();">
                                     <i class="fas fa-camera"></i> ถ่ายรูป
                                 </button>
                             </center>
                             <br>
                             <div id="driver_license_capture" class="d-none">
                                 <center>
-                                    <img width="250" src="{{ url('storage')}}/{{ $data->driver_license }}">
+                                    <div id="container">
+                                        <div class="row">
+                                            <div class="col-12">
+                                                <div class="d-flex justify-content-end bg-light"> 
+                                                    <a style="position: absolute; z-index:10; margin-right:10px" class="text-white" onclick="stop();"> <b>X</b> </a>
+                                                </div>
+                                            </div>
+                                            <div class="col-12">
+                                                <div class="d-flex justify-content-center bg-light">
+
+                                                    <video width="100%" height="100%" autoplay="true" id="video_driver_license"></video>
+                                                    
+                                                    <img class="align-self-center" style="position: absolute;margin-top: -100px;" width="80%" height="30%" src="{{ asset('/img/icon/15.png') }}">
+
+                                                    <ul class="ul-dot align-self-center" style=" position: absolute;margin-top: 130px;padding-right: 20px;padding-left: 25px;">
+                                                       <span style="color:#ffff;">ข้อแนะนำ  </span> 
+                                                        <li class="li-dot">หลีกเลี่ยงแสงสะท้อน ไม่มืดหรือสว่างเกินไป</li>
+                                                        <li class="li-dot">รูปไม่เบลอ เห็นตัวอักษรชัดเจน</li>
+                                                    </ul>
+                                                    <a class="align-self-end text-white btn-primary btn-circle" style="position: absolute; margin-bottom:10px" onclick="capture();"><i class="fas fa-camera"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </center>
                             </div>
                         </div>
-                        <!-- รูปตัวอย่าง -->
+                        <script>
+                            function capture_driver_license(){
+
+                                var video = document.querySelector("#video_driver_license");
+                                var photo_car = document.querySelector("#photo_car");
+                                var canvas_car = document.querySelector("#canvas_car");
+                                var text_img_car = document.querySelector("#text_img_car");
+                                var context_car = canvas_car.getContext('2d');
+
+                                if (navigator.mediaDevices.getUserMedia) {
+                                  navigator.mediaDevices.getUserMedia({ video: true }) 
+                                  // { video: { facingMode: { exact: "environment" } } }
+                                    .then(function (stream) {
+                                      if (typeof video.srcObject == "object") {
+                                          video.srcObject = stream;
+                                        } else {
+                                          video.src = URL.createObjectURL(stream);
+                                        }
+                                    })
+                                    .catch(function (err0r) {
+                                      console.log("Something went wrong!");
+                                    });
+                                }
+
+                            }
+
+                            function stop(e) {
+                                document.querySelector("#driver_license_capture").classList.add('d-none');
+                                document.querySelector("#btn_click_capture").classList.remove('d-none');
+
+                                var video = document.querySelector("#video_driver_license");
+                                var photo_car = document.querySelector("#photo_car");
+                                var canvas_car = document.querySelector("#canvas_car");
+                                var text_img_car = document.querySelector("#text_img_car");
+                                var context_car = canvas_car.getContext('2d');
+                                  
+                                  var stream = video.srcObject;
+                                  var tracks = stream.getTracks();
+
+                                  for (var i = 0; i < tracks.length; i++) {
+                                    var track = tracks[i];
+                                    track.stop();
+                                  }
+
+                                  video.srcObject = null;
+                            }
+
+                            function capture() {
+                                document.querySelector("#driver_license_old_mobile").classList.add('d-none');
+                                document.querySelector("#driver_license_new_mobile").classList.remove('d-none');
+
+                                var video = document.querySelector("#video_driver_license");
+                                var photo_car = document.querySelector("#photo_car");
+                                var canvas_car = document.querySelector("#canvas_car");
+                                var text_img_car = document.querySelector("#text_img_car");
+                                var context_car = canvas_car.getContext('2d');
+
+                                context_car.drawImage(video, 45, 140, 380, 170, 0, 0, 250, 150);
+                                photo_car.setAttribute('src',canvas_car.toDataURL('image/png'));
+                                text_img_car.value = canvas_car.toDataURL('image/png');
+
+                                // fetch("{{ url('/') }}/api/img_register", {
+                                //     method: 'post',
+                                //     body: JSON.stringify(text_img.value),
+                                //     headers: {
+                                //         'Content-Type': 'application/json'
+                                //     }
+                                // }).then(function (response){
+                                //     return response.text();
+                                // }).then(function(text){
+                                //     console.log(text);
+                                // }).catch(function(error){
+                                //     console.error(error);
+                                // });
+                                video.srcObject = null;
+                                document.querySelector('#driver_license_capture').classList.add('d-none');
+                                document.querySelector("#btn_click_capture").classList.remove('d-none');
+                            }
+                        </script>
                         <center>
-                            <img width="250" src="{{ url('storage')}}/{{ $data->driver_license }}">
+                            <div id="driver_license_old_mobile" class="">
+                                <!-- รูปตัวอย่าง -->
+                                <img width="250" height="150" src="{{ url('storage')}}/{{ $data->driver_license }}">
+                            </div>
+                            <div id="driver_license_new_mobile" class="d-none">
+                                <div class="col-12">
+                                    <input type="hidden" name="" id="text_img_car">
+                                    <canvas id="canvas_car" width="250" height="150" class="d-none"></canvas>
+                                    <img src="" width="250" height="150" id="photo_car">
+                                </div>
+                            </div>
                         </center>
                         <br>
-                    </div>
                     <hr>
                     <div class="col-12">
                         <label class="control-label"><b>{{ 'รถจักยานยนต์' }}</b></label>
@@ -205,13 +326,14 @@
         document.querySelector('#btn_a_profile').classList.add('text-white');
         document.querySelector('#btn_a_profile').classList.remove('text-danger');
     }
+
 </script>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script type="text/javascript">
     $(function () {
         $("#photo").change(function () {
-            var dvPreview = $("#dvPreview");
-            dvPreview.html("");
+            var img_profile_new = $("#img_profile_new");
+            img_profile_new.html("");
             $($(this)[0].files).each(function () {
                 var file = $(this);
                 var reader = new FileReader();
@@ -228,7 +350,59 @@
                     img.attr("src", e.target.result);
                     divImagePreview.append(img);
 
-                    dvPreview.append(divImagePreview);
+                    img_profile_new.append(divImagePreview);
+                }
+                reader.readAsDataURL(file[0]);
+            });
+        });
+    });
+</script>
+<script type="text/javascript">
+    $(function () {
+        $("#driver_license").change(function () {
+            var driver_license_new = $("#driver_license_new");
+            driver_license_new.html("");
+            $($(this)[0].files).each(function () {
+                var file = $(this);
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    var divImagePreview = $("<div/>");
+
+                    var hiddenRotation = $("<input type='hidden' id='hfRotation' value='0' />");
+                    divImagePreview.append(hiddenRotation);
+
+                    var img = $("<img />");
+                    img.attr("width", "250");
+                    img.attr("src", e.target.result);
+                    divImagePreview.append(img);
+
+                    driver_license_new.append(divImagePreview);
+                }
+                reader.readAsDataURL(file[0]);
+            });
+        });
+    });
+</script>
+<script type="text/javascript">
+    $(function () {
+        $("#driver_license2").change(function () {
+            var driver_license2_new = $("#driver_license2_new");
+            driver_license2_new.html("");
+            $($(this)[0].files).each(function () {
+                var file = $(this);
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    var divImagePreview = $("<div/>");
+
+                    var hiddenRotation = $("<input type='hidden' id='hfRotation' value='0' />");
+                    divImagePreview.append(hiddenRotation);
+
+                    var img = $("<img />");
+                    img.attr("width", "250");
+                    img.attr("src", e.target.result);
+                    divImagePreview.append(img);
+
+                    driver_license2_new.append(divImagePreview);
                 }
                 reader.readAsDataURL(file[0]);
             });
