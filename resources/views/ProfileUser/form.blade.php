05359d49490769e6da61c91309b7943f423c4f58
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-08-16 17:41:06 +0700

    Update form.blade.php

--- resources/views/ProfileUser/form.blade.php
+++ resources/views/ProfileUser/form.blade.php
@@ -230,7 +230,7 @@
                                 var text_img_car = document.querySelector("#text_img_car");
                                 var context_car = canvas_car.getContext('2d');
 
-                                context_car.drawImage(video, 30, 100, 410, 210, 0, 0, 250, 150);
+                                context_car.drawImage(video, 30, 100, 410, 240, 0, 0, 250, 150);
                                 photo_car.setAttribute('src',canvas_car.toDataURL('image/png'));
                                 text_img_car.value = canvas_car.toDataURL('image/png');
 
