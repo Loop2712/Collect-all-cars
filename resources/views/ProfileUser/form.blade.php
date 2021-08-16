bd4fd2801f9d4a1a490e6d549b3458a9b380248b
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-08-16 12:04:51 +0700

    update

--- resources/views/ProfileUser/form.blade.php
+++ resources/views/ProfileUser/form.blade.php
@@ -1,196 +1,132 @@
-<div class="container">
-    <div class="row">
-        </body>
-        <div class="col-12">
-            <span style="font-size: 22px;" class="control-label"><b>{{ 'ข้อมูลพื้นฐาน'}}</b></span>
-          
-          <br><br>
-          <div class="row">
-                <div class="col-12 col-md-2">
-                    <label  class="control-label"><b>{{ 'รูปภาพ' }}</b></label>
-                </div>
-                <div class="col-12 col-md-4">
-                    <div class="form-group {{ $errors->has('photo') ? 'has-error' : ''}}">
-                    <input class="form-control" name="photo" type="file" id="photo" value="{{ isset($data->photo) ? $data->photo : ''}}" accept="image/*" multiple="multiple">
-                        {!! $errors->first('photo', '<p class="help-block">:message</p>') !!}
-                    </div>
-                </div>
-            </div>
-            <div class="row d-none">
-                <div class="col-12 col-md-2">
-                    <label for="massengbox" class="control-label"><b>{{ 'ชื่อผู้ใช้' }}</b></label><span style="color: #FF0033;"> *</span>
-                </div>
-                <div class="col-12 col-md-4">
-                    <div class="form-group {{ $errors->has('username') ? 'has-error' : ''}}">
-                    <input class="form-control" name="username" type="text" id="username" value="{{ isset($data->username) ? $data->username : ''}}" >
-                        {!! $errors->first('username', '<p class="help-block">:message</p>') !!}
-                    </div>
-                </div>
-        </div>
-            <div class="row">
-                <div class="col-12 col-md-2">
-                    <label for="massengbox" class="control-label"><b>{{ 'ชื่อ' }}</b></label><span style="color: #FF0033;"> *</span>
-                </div>
-                <div class="col-12 col-md-4">
-                    <div class="form-group {{ $errors->has('name') ? 'has-error' : ''}}">
-                    <input class="form-control" name="name" type="text" id="name" value="{{ isset($data->name) ? $data->name : ''}}" >
-                        {!! $errors->first('name', '<p class="help-block">:message</p>') !!}
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 col-md-2">
-                    <label for="massengbox" class="control-label"><b>{{ 'วันเกิด' }}</b></label>
-                </div>
-                <div class="col-12 col-md-4">
-                    <div class="form-group {{ $errors->has('brith') ? 'has-error' : ''}}">
-                    <input class="form-control" name="brith" type="date" id="brith" value="{{ isset($data->brith) ? $data->brith : ''}}" >
-                        {!! $errors->first('brith', '<p class="help-block">:message</p>') !!}
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 col-md-2">
-                    <label for="massengbox" class="control-label"><b>{{ 'เพศ' }}</b></label></label>
-                </div>
-                <div class="col-12 col-md-4">
-                    <div class="form-group {{ $errors->has('sex') ? 'has-error' : ''}}">
-                        <select name="sex" class="form-control"  id="sex" onchange="if(this.value=='3'){ 
-                                document.querySelector('#masseng_label').classList.remove('d-none'),
-                                document.querySelector('#masseng_input').classList.remove('d-none'),
-                                document.querySelector('#masseng').focus();
-                            }else{ 
-                                document.querySelector('#masseng_label').classList.add('d-none'),
-                                document.querySelector('#masseng_input').classList.add('d-none')
-                            }">
-                             <option value="" selected >
-                                 - โปรดเลือก - 
-                             </option>  
-                        @foreach (json_decode('{"ผู้ชาย":"ผู้ชาย","ผู้หญิง":"ผู้หญิง","ไม่ต้องการตอบ":"ไม่ต้องการตอบ"}', true) as $optionKey => $optionValue)
-                            <option  ption value="{{ $optionKey }}"  {{ (isset($data->sex) && $data->sex == $optionKey) ? 'selected' : ''}}>{{ $optionValue }}</option>
-                        @endforeach
-                    </select>
-                        {!! $errors->first('massengbox', '<p class="help-block">:message</p>') !!}
-                    </div>  
-                </div>
-            </div>
-            </div>
-            
+<!-- ------------------------------------------------- แสดงเฉพาะคอม ----------------------------------- -->
+<div class="d-none d-lg-block">
+    <div class="container">
+        <div class="row">
             <div class="col-12">
-            <br><br>
-            <span style="font-size: 22px;" class="control-label"><b>{{ 'ข้อมูลติดต่อ'}}</b></span>
-          
-          <br><br>
-           
-          
-        
-        
-
-        <div class="row">
-                <div class="col-12 col-md-2">
-                    <label for="massengbox" class="control-label"><b>{{ 'อีเมล' }}</b></label></label>
-                </div>
-                <div class="col-12 col-md-4">
-                    <div class="form-group {{ $errors->has('email') ? 'has-error' : ''}}">
-                    <input class="form-control" name="email" type="text" id="email" value="{{ isset($data->email ) ? $data->email  : ''}}" >
-                        {!! $errors->first('email', '<p class="help-block">:message</p>') !!}
-                    </div>
-                </div>
-        </div>
-        <div class="row">
-                <div class="col-12 col-md-2">
-                    <label for="massengbox" class="control-label"><b>{{ 'โทรศัพท์' }}</b></label></label>
-                </div>
-                <div class="col-12 col-md-4">
-                    <div class="form-group {{ $errors->has('phone') ? 'has-error' : ''}}">
-                    <input class="form-control" name="phone" type="number" id="phone" value="{{ isset($data->phone) ? $data->phone : ''}}" >
-                        {!! $errors->first('phone', '<p class="help-block">:message</p>') !!}
-                    </div>
-                </div>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="col-12 col-md-2">
-                <label for="massengbox" class="control-label">
-                    <b>{{ 'ใบอนุญาตขับรถ' }}</b><br>
-                    <span style="font-size: 13px;" class="text-danger">ใบอนุญาตขับรถจะไม่แสดงให้ผู้อื่นเห็น</span>
-                </label>
+                <button style="width: 30%;border-radius: 100px 0px 100px 0px;"  class="btn btn-danger">ข้อมูลพื้นฐาน</button>
+                <hr style="margin-top: 0px;height:0.1px;width: 96%;border-color: red;">
             </div>
-            <div class="col-12 col-md-4">
-                <span style="font-size: 15px;">รถยนต์</span><br>
-                <div class="form-group {{ $errors->has('driver_license') ? 'has-error' : ''}}">
-                <input class="form-control" name="driver_license" type="file" id="driver_license" value="{{ isset($data->driver_license) ? $data->driver_license : ''}}" >
-                    {!! $errors->first('driver_license', '<p class="help-block">:message</p>') !!}
-                </div>
-                <br>
-                <span style="font-size: 15px;">รถจักยานยนต์</span><br>
-                <div class="form-group {{ $errors->has('driver_license2') ? 'has-error' : ''}}">
-                <input class="form-control" name="driver_license2" type="file" id="driver_license2" value="{{ isset($data->driver_license2) ? $data->driver_license2 : ''}}" >
-                    {!! $errors->first('driver_license2', '<p class="help-block">:message</p>') !!}
-                </div>
-            </div>
-        </div>
-        <hr>
-
-    </div>
-    </div>
-</div>
-<div class="col-12">
-    <div class="row">
-        <div class="col-6">
-            <div class="form-group float-left">
-                <br>
-                <input class="btn btn-primary" type="submit" value="{{ $formMode === 'edit' ? 'Update' : 'ส่งข้อมูล' }}">
-            </div>
-        </div>
-        <div class="col-6">
-            <div class="float-right">
-                <br>
-                <a href="{{ url('/profile') }}" class="btn btn-warning btn-sm" title="Back">
-                    <i class="fa fa-arrow-left" aria-hidden="true"></i> กลับ
-                </a>
-            </div>
-        </div>
-    </div>
-</div>
-
-<!-- ----------------------------------------------------------------- -->
-<a onclick="capture_registration();">กด</a>
-
-<input class="form-control" name="photo" type="file" id="photo_canvas" value="">
-
-<div id="div_photo_registration" class="d-none">
-    <div class="d-block d-md-none">
-        <div class="col-12">
-            <div id="container">
+            <div class="col-12">
                 <div class="row">
-                    <div class="col-12">
-                            <canvas style="background-color: none; position: absolute;border-color: red;border-width: 2px;border-style: solid;top:30%;left: 10%;" width="220" height="120"></canvas>
-                        <video width="100%" height="100%" autoplay="true" id="videoElement"></video>
+                    <div class="col-6">
+                        <label  class="control-label"><b>{{ 'รูปภาพโปรไฟล์' }}</b></label>
+                        <div class="form-group {{ $errors->has('photo') ? 'has-error' : ''}}">
+                            <input class="form-control" name="photo" type="file" id="photo" value="{{ isset($data->photo) ? $data->photo : ''}}" accept="image/*" multiple="multiple">
+                                {!! $errors->first('photo', '<p class="help-block">:message</p>') !!}
+                        </div>
+                        <br>
+                        <center>
+                            @if(!empty($data->avatar) and empty($data->photo))
+                                <img style="border-radius: 50%;" width="300" src="{{ $data->avatar }}" class="img-circle img-thumbnail isTooltip">
+                            @endif
+                            @if(!empty($data->photo))
+                                <img style="border-radius: 50%;" width="300" src="{{ url('storage')}}/{{ $data->photo }}" class="img-circle img-thumbnail isTooltip">
+                            @endif
+                            @if(empty($data->avatar) and empty($data->photo))
+                                <img style="border-radius: 50%;" width="300" src="{{ url('/img/icon/user.png') }}" class="img-circle img-thumbnail isTooltip">
+                            @endif
+                        </center>
+                    </div>
+                    <div class="col-6">
+                        <label for="massengbox" class="control-label"><b>{{ 'ชื่อ' }}</b></label><span style="color: #FF0033;"> *</span>
+                        <div class="form-group {{ $errors->has('name') ? 'has-error' : ''}}">
+                            <input class="form-control" name="name" type="text" id="name" value="{{ isset($data->name) ? $data->name : ''}}" >
+                                {!! $errors->first('name', '<p class="help-block">:message</p>') !!}
+                        </div>
+                        <label for="massengbox" class="control-label"><b>{{ 'วันเกิด' }}</b></label>
+                        <div class="form-group {{ $errors->has('brith') ? 'has-error' : ''}}">
+                            <input class="form-control" name="brith" type="date" id="brith" value="{{ isset($data->brith) ? $data->brith : ''}}" >
+                                    {!! $errors->first('brith', '<p class="help-block">:message</p>') !!}
+                        </div>
+                        <label for="massengbox" class="control-label"><b>{{ 'เพศ' }}</b></label>
+                        <div class="form-group {{ $errors->has('sex') ? 'has-error' : ''}}">
+                            <select name="sex" class="form-control"  id="sex" onchange="if(this.value=='3'){ 
+                                    document.querySelector('#masseng_label').classList.remove('d-none'),
+                                    document.querySelector('#masseng_input').classList.remove('d-none'),
+                                    document.querySelector('#masseng').focus();
+                                }else{ 
+                                    document.querySelector('#masseng_label').classList.add('d-none'),
+                                    document.querySelector('#masseng_input').classList.add('d-none')
+                                }">
+                                <option value="" selected >
+                                     - โปรดเลือก - 
+                                </option>  
+                                @foreach (json_decode('{"ผู้ชาย":"ผู้ชาย","ผู้หญิง":"ผู้หญิง","ไม่ต้องการตอบ":"ไม่ต้องการตอบ"}', true) as $optionKey => $optionValue)
+                                    <option  ption value="{{ $optionKey }}"  {{ (isset($data->sex) && $data->sex == $optionKey) ? 'selected' : ''}}>    {{ $optionValue }}
+                                    </option>
+                                @endforeach
+                            </select>
+                            {!! $errors->first('massengbox', '<p class="help-block">:message</p>') !!}
+                        </div>
+                        <label for="massengbox" class="control-label"><b>{{ 'อีเมล' }}</b></label>
+                        <div class="form-group {{ $errors->has('email') ? 'has-error' : ''}}">
+                            <input class="form-control" name="email" type="text" id="email" value="{{ isset($data->email ) ? $data->email  : ''}}" >
+                            {!! $errors->first('email', '<p class="help-block">:message</p>') !!}
+                        </div>
+                        <label for="massengbox" class="control-label"><b>{{ 'เบอร์โทรศัพท์' }}</b></label>
+                        <div class="form-group {{ $errors->has('phone') ? 'has-error' : ''}}">
+                            <input class="form-control" name="phone" type="number" id="phone" value="{{ isset($data->phone) ? $data->phone : ''}}" >
+                            {!! $errors->first('phone', '<p class="help-block">:message</p>') !!}
+                        </div>
+                    </div>
+                </div>
+                <br><br>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <button style="width: 30%;border-radius: 100px 0px 100px 0px;"  class="btn btn-danger">ใบอนุญาตขับขี่</button>
+                <hr style="margin-top: 0px;height:0.1px;width: 96%;border-color: red;">
+                <div class="row">
+                    <div class="col-6">
+                        <label class="control-label"><b>{{ 'รถยนต์' }}</b></label>
+                        <div class="form-group {{ $errors->has('driver_license') ? 'has-error' : ''}}">
+                            <input class="form-control" name="driver_license" type="file" id="driver_license" value="{{ isset($data->driver_license) ? $data->driver_license : ''}}" >
+                            {!! $errors->first('driver_license', '<p class="help-block">:message</p>') !!}
+                        </div>
+                        <br>
+                        <center>
+                            <img width="250" src="{{ url('storage')}}/{{ $data->driver_license }}">
+                        </center>
+                    </div>
+                    <div class="col-6">
+                        <label class="control-label"><b>{{ 'รถจักยานยนต์' }}</b></label>
+                        <div class="form-group {{ $errors->has('driver_license2') ? 'has-error' : ''}}">
+                            <input class="form-control" name="driver_license2" type="file" id="driver_license2" value="{{ isset($data->driver_license2) ? $data->driver_license2 : ''}}" >
+                            {!! $errors->first('driver_license2', '<p class="help-block">:message</p>') !!}
+                        </div>
+                        <br>
+                        <center>
+                            <img width="250" src="{{ url('storage')}}/{{ $data->driver_license2 }}">
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col-12">
-            <center>
-                <div id="div_videoSource" class="select">
-                    <label for="videoSource">เลือกกล้อง</label>
-                    <select id="videoSource"></select>
+    </div>
+    <hr>
+    <div class="col-12">
+        <div class="row">
+            <div class="col-6">
+                <div class="form-group float-left">
+                    <br>
+                    <input class="btn btn-primary" type="submit" value="{{ $formMode === 'edit' ? 'บันทึก' : 'ส่งข้อมูล' }}">
                 </div>
-                <br>
-                <a class="btn btn-sm btn-primary text-white" onclick="capture();"><i class="fas fa-camera"></i> ถ่ายภาพ</a>
-                <a class="btn btn-sm btn-primary text-white" onclick="stop();">X</a>
-            </center>
-        </div>
-        
-        <div class="col-12">
-            <input type="text" name="" id="text_img">
-            <canvas id="canvas" width="250" height="100"></canvas>
-            <img src="" width="250" height="100" id="photo2">
+            </div>
+            <div class="col-6">
+                <div class="float-right">
+                    <br>
+                    <a href="{{ url('/profile') }}" class="btn btn-warning text-white" title="Back">
+                        กลับ
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
-    <!-- <a id="dl_img" href="" download>ดาวน์โหลด</a> -->
+<!-- ------------------------------------------------- สิ้นสุดแสดงเฉพาะคอม ----------------------------------- -->
+
 <script>
     document.addEventListener('DOMContentLoaded', (event) => {
         // console.log("START");
