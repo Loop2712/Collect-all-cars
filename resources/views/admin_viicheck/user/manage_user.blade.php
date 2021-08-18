2516086375baa9e3ba4478a275283f5011ffe0e7
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-08-18 10:07:46 +0700

    Update manage_user.blade.php

--- resources/views/admin_viicheck/user/manage_user.blade.php
+++ resources/views/admin_viicheck/user/manage_user.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
 <br>
-    <div class="container-fluid">
+<!-- --------------------------------- แสดงเฉพาะคอม ------------------------------- -->
+    <div class="container-fluid d-none d-lg-block">
         <div class="row">
             <div class="col-md-12">
                 <div class="card">
@@ -154,4 +155,125 @@
             </div>
         </div>
     </div>
+<!-- --------------------------------- สิ้นสุดแสดงเฉพาะคอม ------------------------------- -->
+
+
+<!-- --------------------------------- แสดงเฉพาะมือถือ ------------------------------- -->
+    <div class="container-fluid d-block d-md-none">
+        <div class="row">
+            <div class="col-12">
+                <a href="{{ url('/view_new_user') }}" class="btn btn-primary float-right">
+                    <i class="fas fa-user-plus"></i> สร้างบัญชีผู้ใช้
+                </a>
+                <br><br>
+            </div>
+            <div class="col-12">
+                <div class="card">
+                    <h3 class="card-header">จัดการผู้ใช้</h3>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-5">
+                                <div class="btn-group" style="margin-top: 5px;">
+                                    <button type="button" class="btn btn-sm btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        เลือกประเภท
+                                    </button>
+                                    <div class="dropdown-menu">
+                                        <a href="{{ url('/manage_user') }}?search=admin" class="btn btn-outline-danger dropdown-item">
+                                            Admin
+                                        </a>
+                                        <a href="{{ url('/manage_user') }}?search=JS100" class="btn btn-outline-success dropdown-item">
+                                            JS100
+                                        </a>
+                                        <a href="{{ url('/manage_user') }}?search=2BGreen" class="btn btn-outline-success dropdown-item">
+                                            2BGreen
+                                        </a>
+                                        <a href="{{ url('/manage_user') }}?search=" class="btn btn-outline-info dropdown-item">
+                                            ทั้งหมด
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-7">
+                                <form method="GET" action="{{ url('/manage_user') }}" accept-charset="UTF-8" class="form-inline float-right" role="search">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control" name="search" placeholder="Search..." value="{{ request('search') }}">
+                                        <span class="input-group-append">
+                                            <button class="btn btn-secondary" type="submit">
+                                                <i class="fa fa-search"></i>
+                                            </button>
+                                        </span>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="container">
+                        <div class="row">
+                            @foreach($all_user as $item)
+                            <div class="col-12">
+                                <div class="row">
+                                    <div class="col-9">
+                                        <h5 class="text-success">
+                                            <span style="font-size: 15px;">
+                                                <a target="break" href="{{ url('/').'/profile/'.$item->id }}">
+                                                    <i class="far fa-eye text-primary"></i>
+                                                </a>
+                                            </span>
+                                            &nbsp;&nbsp;{{ $item->name }}
+                                        </h5>
+                                    </div>
+                                    <div class="col-3">
+                                        <div class="float-right">
+                                            @switch($item->type)
+                                                @case('line')
+                                                    <a class="btn btn-sm btn-light"><i class="fab fa-line text-success"></i></a>
+                                                @break
+                                                @case('facebook')
+                                                    <a class="btn btn-sm btn-light"><i class="fab fa-facebook-square text-primary"></i></a>
+                                                @break
+                                                @case('google')
+                                                    <a class="btn btn-sm btn-light"><i class="fab fa-google text-danger"></i></a>
+                                                @break
+                                                @case(null)
+                                                    <a class="btn btn-sm btn-light"><i class="fas fa-globe" style="color: #5F9EA0"></i></a>
+                                                @break
+                                            @endswitch
+                                        </div>
+                                    </div>
+                                    <br><br>
+                                    <div class="col-5">
+                                        <center>{{ $item->phone }}</center>
+                                    </div>
+                                    <div class="col-4">
+                                        <center>{{ $item->role }}</center>
+                                    </div>
+                                    <div class="col-3">
+                                        <div class="float-right">
+                                            @switch($item->ranking)
+                                                @case('Gold')
+                                                    <p class="btn btn-sm btn-light " href=""><img width="20" src="{{ url('/img/ranking/gold.png') }}"></p>
+                                                @break
+                                                @case('Silver')
+                                                    <p class="btn btn-sm btn-light " href=""><img width="20" src="{{ url('/img/ranking/silver.png') }}"></p>
+                                                @break
+                                                @case('Bronze')
+                                                    <p class="btn btn-sm btn-light " href=""><img width="20" src="{{ url('/img/ranking/bronze.png') }}"></p>
+                                                @break
+                                            @endswitch
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr>
+                            </div>
+                            @endforeach
+                            <div class="pagination-wrapper"> {!! $all_user->appends(['search' => Request::get('search')])->render() !!} </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- --------------------------------- สิ้นสุดแสดงเฉพาะมือถือ ------------------------------- -->
 @endsection
