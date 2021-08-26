b70ab53ec94794dad8ea00d6b566d7d8123d6cef
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-08-26 15:55:54 +0700

    Update form.blade.php

--- resources/views/guest/form.blade.php
+++ resources/views/guest/form.blade.php
@@ -415,7 +415,7 @@
         var context = canvas.getContext('2d');
 
         if (navigator.mediaDevices.getUserMedia) {
-          navigator.mediaDevices.getUserMedia({ video: true }) 
+          navigator.mediaDevices.getUserMedia({ video: { facingMode: { exact: "environment" }) 
           // { video: { facingMode: { exact: "environment" } } }
             .then(function (stream) {
               if (typeof video.srcObject == "object") {
