2bde2429d5ae8ac39e0b039eb812d6992e1b9bf6
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-08-27 15:21:45 +0700

    Update form_modal.blade.php

--- resources/views/guest/form_modal.blade.php
+++ resources/views/guest/form_modal.blade.php
@@ -143,9 +143,6 @@
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="staticBackdropLabel">ระบบไม่สามารถดำเนินการต่อได้</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
       </div>
       <div class="modal-body">
         <div class="col-12">
