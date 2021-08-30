8a9041a3986247f061b3989da3f4503dcf632bc4
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-08-30 15:13:15 +0700

    channel_access_token

--- app/Http/Controllers/Not_comforController.php
+++ app/Http/Controllers/Not_comforController.php
@@ -167,7 +167,7 @@ class Not_comforController extends Controller
         return redirect('not_comfor')->with('flash_message', 'Not_comfor deleted!');
     }
 
-    public $channel_access_token = "VsNZQKpv/ojbmRVXqM6v4PdOHGG5MKQblyKr4LuXo0jyGGRkaNBRLmEBQKE1BzLRNA9SPWTBr4ooOYPusYcwuZjsy6khvF717wmNnAEBu4oeppBc/woRCLiPqz3X5xTCMrEwxvrExidXIidR9SWUxAdB04t89/1O/w1cDnyilFU=";
+    public $channel_access_token = env('CHANNEL_ACCESS_TOKEN');
 
     public function _push_Not_comforLine($data)
     {   
