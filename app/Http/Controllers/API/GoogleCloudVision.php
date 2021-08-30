268d17552a287bf8aaa2553b17b7d01869fa578a
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-08-30 10:06:07 +0700

    replace_A_to_Z

--- app/Http/Controllers/API/GoogleCloudVision.php
+++ app/Http/Controllers/API/GoogleCloudVision.php
@@ -17,14 +17,15 @@ class GoogleCloudVision
         $json = file_get_contents("php://input");
         $data = json_decode($json, true);
 
-        $num_ecplode = explode(" ",$data['text_result_0']) ;
+        $text = $this->replace_A_to_Z($data['text_result_0']);
+        
+        $num_ecplode = explode(" ",$text) ;
 
         if (!empty($num_ecplode[1])){
             $num_of_registration =  preg_replace('/\D/', '', $num_ecplode[1]);
         }else{
-            $num_of_registration =  preg_replace('/\D/', '', $data['text_result_0']);
+            $num_of_registration =  preg_replace('/\D/', '', $text);
         }
-        
 
         if (!empty($num_of_registration)) {
             $register_car = Register_car::where('registration_number', 'LIKE', "%$num_of_registration%")
@@ -36,6 +37,50 @@ class GoogleCloudVision
         }
 
         return $register_car ;
+    }
+
+    // Convert a string to an array with multibyte string
+    function getMBStrSplit($string, $split_length = 1){
+        mb_internal_encoding('UTF-8');
+        mb_regex_encoding('UTF-8'); 
+        
+        $split_length = ($split_length <= 0) ? 1 : $split_length;
+        $mb_strlen = mb_strlen($string, 'utf-8');
+        $array = array();
+        $i = 0; 
+        
+        while($i < $mb_strlen)
+        {
+            $array[] = mb_substr($string, $i, $split_length);
+            $i = $i+$split_length;
+        }
+        
+        return $array;
+    }
+
+    function replace_A_to_Z($data){
+        
+        $str = $data ;
+        $arr1 = $this->getMBStrSplit($str);
+
+        $azRange_1 = range('A', 'Z');
+        $azRange_2 = range('a', 'z');
+
+        foreach ($azRange_1 as $letter_1){
+            $x = str_replace($azRange_1, "" ,$arr1) ;
+        }
+        foreach ($azRange_2 as $letter_2){
+            $z = str_replace($azRange_2, "" ,$x) ;
+        }
+
+        $text = "" ;
+
+        for ($i=0; $i < count($z); $i++) { 
+            
+            $text = $text . $z[$i] ;
+        }
+
+        return $text ;
     }
 
     
