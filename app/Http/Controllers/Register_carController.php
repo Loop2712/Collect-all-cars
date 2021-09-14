dc88c71394e4abe77b9f9b7e7dd03f3adc17471f
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-09-14 13:30:23 +0700

    Update Register_carController.php

--- app/Http/Controllers/Register_carController.php
+++ app/Http/Controllers/Register_carController.php
@@ -470,10 +470,10 @@ class Register_carController extends Controller
         
         $requestData = $request->all();
         
-        echo "<pre>";
-        print_r($requestData);
-        echo "<pre>";
-        exit();
+        // echo "<pre>";
+        // print_r($requestData);
+        // echo "<pre>";
+        // exit();
         
         $register_car = Register_car::findOrFail($id);
         $register_car->update($requestData);
