b588b5c790c6e93c01d143cbead3f1f7c7e135ae
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-09-22 10:02:22 +0700

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -358,7 +358,7 @@ class ProfileController extends Controller
             return redirect('profile/'.$id.'/edit?openExternalBrowser=1');
             // echo Auth::User()->name;
         }else{
-            return redirect('login/line?redirectTo=edit_profile2');
+            return redirect('login/line?redirectTo=edit_profile2?openExternalBrowser=1');
         }
     }
 
@@ -366,7 +366,7 @@ class ProfileController extends Controller
     {
         $id = Auth::id();
 
-        return redirect('profile/'.$id.'/edit?openExternalBrowser=1');
+        return redirect('profile/'.$id.'/edit');
         
     }
 
