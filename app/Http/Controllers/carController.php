2a9f413c82aeb9c277f68e3464fae58a647f64da
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-09-22 10:34:22 +0700

    update

--- app/Http/Controllers/carController.php
+++ app/Http/Controllers/carController.php
@@ -22,48 +22,6 @@ class CarController extends Controller
      */
     public function index(Request $request)
     {
-        $datetime =  date("d-m-Y  h:i:sa");
-
-        // $name_time_zone = "Asia/Bangkok" ; //+7
-        $name_time_zone = "Asia/Ust-Nera" ; //+10
-        // $name_time_zone = "America/Antigua" ; //-4
-
-        $data_time_zone = DB::table('time_zones')
-                        ->where('TimeZone', $name_time_zone)
-                        ->get();
-
-        foreach ($data_time_zone as $key_time_zone) {
-            $UTC_time_zone = $key_time_zone->UTC ;
-        }
-
-        if ($UTC_time_zone < 7) {
-
-            $distance_time = (7) - ($UTC_time_zone) ;
-            $time_2 = date("d-m-Y  h:i:sa", strtotime("-" . $distance_time ." hour"));
-
-        }elseif ($UTC_time_zone > 7) {
-            $UTC_time_zone = "+".$UTC_time_zone;
-            $distance_time = $UTC_time_zone - 7 ;
-            $time_2 = date("d-m-Y  h:i:sa", strtotime("+" . $distance_time ." hour"));
-        }elseif ($UTC_time_zone == 7) {
-            $UTC_time_zone = "+".$UTC_time_zone;
-            $time_2 = $datetime ;
-        }
-
-        echo $name_time_zone ;
-        echo "<br>";
-
-        echo $UTC_time_zone ;
-        echo "<br>";
-
-        $time_zone = $time_2 . ", UTC " . $UTC_time_zone ;
-
-        echo $time_zone ;
-        echo "<br>";
-
-
-        exit();
-
         $brand     = $request->get('brand');
         $typecar   = $request->get('typecar');
         $year      = $request->get('year');
