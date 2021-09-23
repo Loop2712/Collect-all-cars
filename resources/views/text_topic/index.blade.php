83aaa4fcce13211c441696cb37d8251564e902b7
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-09-23 10:59:57 +0700

    update

--- resources/views/text_topic/index.blade.php
+++ resources/views/text_topic/index.blade.php
@@ -74,8 +74,8 @@
                                         <td>{{ $item->ko }}</td>
                                         <td>{{ $item->es }}</td>
                                         <td>
-                                            <!-- <a href="{{ url('/text_topic/' . $item->id) }}" title="View Text_topic"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
-                                            <a href="{{ url('/text_topic/' . $item->id . '/edit') }}" title="Edit Text_topic"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a> -->
+                                            <!-- <a href="{{ url('/text_topic/' . $item->id) }}" title="View Text_topic"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a> -->
+                                            <a href="{{ url('/text_topic/' . $item->id . '/edit') }}" title="Edit Text_topic"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
 
                                             <form method="POST" action="{{ url('/text_topic' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
                                                 {{ method_field('DELETE') }}
