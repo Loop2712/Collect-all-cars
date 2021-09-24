57d5beee33110ee81c48425149858a5c56e5f096
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-09-24 15:05:11 +0700

    Update API_language.php

--- app/Http/Controllers/API/API_language.php
+++ app/Http/Controllers/API/API_language.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Text_topic;
 use Auth;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\API\LineApiController;
 
 class API_language extends Controller
 {
@@ -17,6 +18,39 @@ class API_language extends Controller
               ->update([
                 'language' => $language,
         ]);
+
+        $data_users = DB::table('users')
+                ->where('id', $user_id)
+                ->where('status', "active")
+                ->get();
+
+        foreach ($data_users as $data_user) {
+            $provider_id = $data_user->provider_id ;
+        }
+
+        switch ($language) {
+          case 'th':
+              $richMenuId = "richmenu-c97702fad335082aad0b8a069d4a8e8f" ;
+              break;
+          case 'en':
+              $richMenuId = "richmenu-e335c46ce1d4bfe6688cc4bdb19f8769" ;
+              break;
+          case 'zh-TW':
+              $richMenuId = "zh-TW" ;
+              break;
+          case 'ja':
+              $richMenuId = "ja" ;
+              break;
+          case 'ko':
+              $richMenuId = "ko" ;
+              break;
+          case 'es':
+              $richMenuId = "es" ;
+              break;
+      }
+
+      $lineAPI = new LineApiController();
+      $lineAPI->set_richmanu_language($provider_id , $richMenuId , $language);
 
         // return $language;
     }
