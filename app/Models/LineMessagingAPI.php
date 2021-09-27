3e42a492e7f8bd1d7042afea82cd6a9312ef1bf4
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-09-27 15:26:54 +0700

    Update LineMessagingAPI.php

--- app/Models/LineMessagingAPI.php
+++ app/Models/LineMessagingAPI.php
@@ -1032,7 +1032,7 @@ class LineMessagingAPI extends Model
 
                 $template_path = storage_path('../public/json/flex-promotion.json');   
                 $string_json = file_get_contents($template_path);
-                
+
                 $string_json = str_replace("โปรโมชั่นรถยนต์",$data_topic[1],$string_json);
                 $string_json = str_replace("โปรโมชั่นรถจักรยานยนต์",$data_topic[2],$string_json);
                 $string_json = str_replace("โปรโมชั่น",$data_topic[0],$string_json);
@@ -1043,8 +1043,29 @@ class LineMessagingAPI extends Model
 
             case "promotion_car": 
 
+                $data_topic = [
+                    "โปรโมชั่น",
+                    "รายละเอียด",
+                ];
+
+                for ($i=0; $i < count($data_topic); $i++) { 
+
+                    $text_topic = DB::table('text_topics')
+                            ->select($user_language)
+                            ->where('th', $data_topic[$i])
+                            ->where('en', "!=", null)
+                            ->get();
+
+                    foreach ($text_topic as $item_of_text_topic) {
+                        $data_topic[$i] = $item_of_text_topic->$user_language ;
+                    }
+                }
+
                 $template_path = storage_path('../public/json/flex-promotion_car.json');   
                 $string_json = file_get_contents($template_path);
+
+                $string_json = str_replace("โปรโมชั่น",$data_topic[1],$string_json);
+                $string_json = str_replace("รายละเอียด",$data_topic[2],$string_json);
 
                 $randomPromotion = DB::table('promotions')
                     ->where('type', "car")
@@ -1090,8 +1111,29 @@ class LineMessagingAPI extends Model
 
             case "promotion_motorcycle": 
 
+                $data_topic = [
+                    "โปรโมชั่น",
+                    "รายละเอียด",
+                ];
+
+                for ($i=0; $i < count($data_topic); $i++) { 
+
+                    $text_topic = DB::table('text_topics')
+                            ->select($user_language)
+                            ->where('th', $data_topic[$i])
+                            ->where('en', "!=", null)
+                            ->get();
+
+                    foreach ($text_topic as $item_of_text_topic) {
+                        $data_topic[$i] = $item_of_text_topic->$user_language ;
+                    }
+                }
+
                 $template_path = storage_path('../public/json/flex-promotion_car.json');   
                 $string_json = file_get_contents($template_path);
+
+                $string_json = str_replace("โปรโมชั่น",$data_topic[1],$string_json);
+                $string_json = str_replace("รายละเอียด",$data_topic[2],$string_json);
 
                 $randomPromotion = DB::table('promotions')
                     ->where('type', "motorcycle")
