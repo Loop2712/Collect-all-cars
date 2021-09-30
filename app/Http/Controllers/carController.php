ebf74271a2b74177b71306e16ef948fa45d4e862
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-09-30 16:38:08 +0700

    Update carController.php

--- app/Http/Controllers/carController.php
+++ app/Http/Controllers/carController.php
@@ -30,7 +30,11 @@ class CarController extends Controller
                 ->get();
 
             foreach ($data_users as $data_user) {
-                $user_language = $data_user->language ;
+                if (!empty($data_user->language)) {
+                    $user_language = $data_user->language ;
+                }else{
+                    $user_language = 'th' ;
+                }
                 echo $user_language ;
             }
             
