c710eafb24e6f82d600b44304d62d352b674f9ae
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-09-30 08:44:22 +0700

    Update P_2begreen_service_area_adjustment.blade.php

--- resources/views/Partners_2bgreen/P_2begreen_service_area_adjustment.blade.php
+++ resources/views/Partners_2bgreen/P_2begreen_service_area_adjustment.blade.php
@@ -46,7 +46,7 @@
     
 </style>
 
-<script>
+<!-- <script>
     document.addEventListener('DOMContentLoaded', (event) => {
         // console.log("START");
         initMap();
@@ -83,6 +83,39 @@
 
     }
 
-</script>
+</script> -->
 
+<script>
+    function initMap() {
+  const myLatlng = { lat: -25.363, lng: 131.044 };
+
+  const map = new google.maps.Map(document.getElementById("map")!, {
+    zoom: 4,
+    center: myLatlng,
+  });
+
+  // Create the initial InfoWindow.
+  let infoWindow = new google.maps.InfoWindow({
+    content: "Click the map to get Lat/Lng!",
+    position: myLatlng,
+  });
+
+  infoWindow.open(map);
+
+  // Configure the click listener.
+  map.addListener("click", (mapsMouseEvent) => {
+    // Close the current InfoWindow.
+    infoWindow.close();
+
+    // Create a new InfoWindow.
+    infoWindow = new google.maps.InfoWindow({
+      position: mapsMouseEvent.latLng,
+    });
+    infoWindow.setContent(
+      JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+    );
+    infoWindow.open(map);
+  });
+}
+</script>
 @endsection
