e75c367646c2b4121e99211edc33c55452ce39e8
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-09-30 09:24:18 +0700

    Update P_2begreen_service_area_adjustment.blade.php

--- resources/views/Partners_2bgreen/P_2begreen_service_area_adjustment.blade.php
+++ resources/views/Partners_2bgreen/P_2begreen_service_area_adjustment.blade.php
@@ -54,7 +54,7 @@
 
     function initMap() {
         // 13.7248936,100.4930264 lat lng ประเทศไทย
-        const myLatlng = { lat: -25.363, lng: 131.044 };
+        const myLatlng = { lat: 13.7248936, lng: 100.4930264 };
         const map = new google.maps.Map(document.getElementById("map"), {
             zoom: 4,
             center: myLatlng,
