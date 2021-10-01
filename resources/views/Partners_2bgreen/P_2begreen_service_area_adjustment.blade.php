c6b9a6cff53c7783f90a59169bea2f8ad3b28280
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-10-01 12:48:04 +0700

    Update P_2begreen_service_area_adjustment.blade.php

--- resources/views/Partners_2bgreen/P_2begreen_service_area_adjustment.blade.php
+++ resources/views/Partners_2bgreen/P_2begreen_service_area_adjustment.blade.php
@@ -139,7 +139,7 @@
     function add_location(text_content , count_position , map , marker_lat , marker_lng) {
 
         let input_areaArr = document.querySelector('#areaArr');
-            input_areaArr.value = { lat: parseFloat(marker_lat), lng: parseFloat(marker_lng) };
+            input_areaArr.value = input_areaArr.value + '{lat: ' + parseFloat(marker_lat) +  ', lng: ' + parseFloat(marker_lng)+ ' } ';
 
         let co_position = document.querySelector('#count_position');
 
