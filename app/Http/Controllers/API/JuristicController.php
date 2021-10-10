a349707bbeb8eac7def69517adc66eaa36ac3f81
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-10-10 14:19:17 +0700

    Update JuristicController.php

--- app/Http/Controllers/API/JuristicController.php
+++ app/Http/Controllers/API/JuristicController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Organization;
-use App\Models\partner;
+use App\Models\Partner;
 
 class JuristicController extends Controller
 {
