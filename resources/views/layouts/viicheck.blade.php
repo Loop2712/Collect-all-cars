c54b38f9f73358208236aaaaeac812292b6f66bb
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-10-12 13:24:44 +0700

    Update viicheck.blade.php

--- resources/views/layouts/viicheck.blade.php
+++ resources/views/layouts/viicheck.blade.php
@@ -260,7 +260,7 @@
           <li><a class="nav-link scrollto" href="{{ url('/promotion') }}"><b>โปรโมชั่น</b></a></li>
           <li><a class="nav-link scrollto" href="{{ url('/register_car/create') }}"><b>ลงทะเบียนรถ</b></a></li>
           <li class="dropdown">
-              <input type="hidden" name="name_user" id="name_user" value="{{ Auth::user()->name }}">
+              <input class="notranslate" type="hidden" name="name_user" id="name_user" value="{{ Auth::user()->name }}">
               <a href="#" style="font-size: 18px;">
                 <span class="btn btn-primary main-shadow main-radius">
                   <b><span class="notranslate" id="input_name"></span></b>
