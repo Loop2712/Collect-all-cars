c4847fdc9de70b66ce81d53231e9455632c986cb
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-10-21 16:51:32 +0700

    Update carController.php

--- app/Http/Controllers/carController.php
+++ app/Http/Controllers/carController.php
@@ -30,8 +30,34 @@ class CarController extends Controller
                 ->where('language', 'th')
                 ->get();
 
-        $lineAPI = new LineApiController();
-        $lineAPI->check_language_user($data_users);
+
+        echo count($data_users);
+
+        foreach ($data_users as $key) {
+            echo $key->provider_id;
+            echo "<br>";
+            echo $key->language;
+            echo "<br>";
+            echo "<br>";
+
+            $richMenuId = "richmenu-454c598f6cc2cfa01d9e61dd08c90f1a" ;
+
+            $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(env('CHANNEL_ACCESS_TOKEN'));
+            $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => env('LINE_CLIENT_SECRET')]);
+            $response = $bot->linkRichMenu($key->provider_id, $richMenuId);
+
+            $data = [
+                "title" => "set_richmanu_th",
+                "content" => $key->provider_id,
+            ];
+            MyLog::create($data);
+        }
+        // $lineAPI = new LineApiController();
+        // $lineAPI->check_language_user($data_users);
+
+        
+
+        
 
         exit();
 
