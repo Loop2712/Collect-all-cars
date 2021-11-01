de3392dc8f20318151cbca08fa40f8d2317c4994
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-11-01 13:24:59 +0700

    update

--- app/Models/LineMessagingAPI.php
+++ app/Models/LineMessagingAPI.php
@@ -139,13 +139,6 @@ class LineMessagingAPI extends Model
 
                 $messages = [ json_decode($string_json, true) ]; 
                 break;
-            case "language": 
-
-                $template_path = storage_path('../public/json/flex-language.json');   
-                $string_json = file_get_contents($template_path);
-
-                $messages = [ json_decode($string_json, true) ]; 
-                break;
         	case "other": 
 
                 $data_Text_topic = [
