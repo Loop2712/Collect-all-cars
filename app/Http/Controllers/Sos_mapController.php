77d9b2ac664dab32d71528ae8fd021bbe64dd661
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-11-05 13:15:18 +0700

    Update Sos_mapController.php

--- app/Http/Controllers/Sos_mapController.php
+++ app/Http/Controllers/Sos_mapController.php
@@ -71,11 +71,6 @@ class Sos_mapController extends Controller
         
         $requestData = $request->all();
 
-        echo "<pre>";
-        print_r($requestData);
-        echo "<pre>";
-        exit();
-
         if (!empty($requestData['text_img'])) {
 
             $name_file_img = uniqid('photo_sos-', true);
