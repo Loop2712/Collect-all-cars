e02b9c6ab270f49d1f96f6004a8b7186e84c1f94
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-11-05 12:06:06 +0700

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -90,7 +90,7 @@ class LoginController extends Controller
     {
         $user = Socialite::driver('google')->user();
 
-        $this->_registerOrLoginUser($user, "google",null);
+        $this->_registerOrLoginUser($user, "google",null,null);
 
         $value = $request->session()->get('redirectTo');
         $request->session()->forget('redirectTo');
@@ -110,7 +110,7 @@ class LoginController extends Controller
     {
         $user = Socialite::driver('facebook')->user();
         // print_r($user);
-        $this->_registerOrLoginUser($user,"facebook",null);
+        $this->_registerOrLoginUser($user,"facebook",null,null);
 
         // Return home after login
         return redirect()->intended();
