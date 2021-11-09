72cda7cd973457feda1df5866374f31f077d47e2
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-11-09 13:42:05 +0700

    Update sos.blade.php

--- resources/views/admin_viicheck/sos.blade.php
+++ resources/views/admin_viicheck/sos.blade.php
@@ -291,6 +291,10 @@
 
     });
 
+    var draw_area ;
+    var map ;
+    var marker ;
+
     function initMap() {
         let text_zoom = document.getElementById("va_zoom").value;
         let num_zoom = parseFloat(text_zoom);
@@ -308,7 +312,6 @@
         // 13.7248936,100.4930264 lat lng ประเทศไทย
 
         //วาดพื้นที่รับผิดชอบ
-        draw_area(map);
 
         //ปักหมุด
         @foreach($view_maps_all as $item)
@@ -332,179 +335,38 @@
         let text_center_lat = document.getElementById("center_lat");
         let text_center_lng = document.getElementById("center_lng");
 
-        switch(text_area) {
-            case "ViiCHECK":
-                text_zoom.value = "6" ;
-                text_center_lat.value = "13.7248936" ;
-                text_center_lng.value = "100.4930264" ;
-                break;
-            case "VRU":
-                text_zoom.value = "15.4" ;
-                text_center_lat.value = "14.1337902" ;
-                text_center_lng.value = "100.6124206" ;
-                break;
-            case "TU":
-                text_zoom.value = "14.5" ;
-                text_center_lat.value = "14.0731804" ;
-                text_center_lng.value = "100.6064698" ;
-                break;
-            case "KMUTNB":
-                text_zoom.value = "16.5" ;
-                text_center_lat.value = "13.8211903" ;
-                text_center_lng.value = "100.5138453" ;
-                break;
-        }
-        initMap();
-    }
+        fetch("{{ url('/') }}/api/area_current/"+text_area)
+            .then(response => response.json())
+            .then(result => {
+                // console.log(result);
 
-    function draw_area(map) {
-        // พื้นที่ทดสอบ
-        const area_test = [
-            { lat: 14.1150621, lng: 100.6013697 },
-            { lat: 14.1150621, lng: 100.6074465 },
-            { lat: 14.1127626, lng: 100.6074465 },
-            { lat: 14.1127626, lng: 100.6013697 },
-          ];
+                var bounds = new google.maps.LatLngBounds();
 
-          // Construct the polygon.
-          const draw_area_test = new google.maps.Polygon({
-            paths: area_test,
-            strokeColor: "#008450",
-            strokeOpacity: 0.8,
-            strokeWeight: 1,
-            fillColor: "#008450",
-            fillOpacity: 0.25,
-          });
-          draw_area_test.setMap(map);
-        // // END พื้นที่ ทดสอบ
+                for (let ix = 0; ix < result.length; ix++) {
+                    bounds.extend(result[ix]);
+                }
 
-        // // พื้นที่ VRU 
-        const area_vru = [
-            { lat: 14.1357294, lng: 100.6054468 },
-            { lat: 14.1357294, lng: 100.6179993 },
-            { lat: 14.1319187, lng: 100.6179993 },
-            { lat: 14.1319187, lng: 100.6054468 },
-            
-          ];
-          // Construct the polygon.
-          const draw_area_vru = new google.maps.Polygon({
-            paths: area_vru,
-            strokeColor: "#008450",
-            strokeOpacity: 0.8,
-            strokeWeight: 1,
-            fillColor: "#008450",
-            fillOpacity: 0.25,
-          });
-          draw_area_vru.setMap(map);
-        // // END พื้นที่ VRU 
+                map = new google.maps.Map(document.getElementById("map"), {
+                    // zoom: num_zoom,
+                    center: bounds.getCenter(),
+                });
+                map.fitBounds(bounds);
 
-        // พื้นที่ TU
-        const tu_a = { lat: 14.077896, lng: 100.593465 };
-        const tu_b = { lat: 14.080425, lng: 100.600798 };
-        const tu_c = { lat: 14.076168, lng: 100.600594 };
-        const tu_d = { lat: 14.076193, lng: 100.617410 };
-        const tu_e = { lat: 14.066614, lng: 100.616926 };
-        const tu_f = { lat: 14.066762, lng: 100.605290 };
-        const tu_g = { lat: 14.065550, lng: 100.605443 };
-        const tu_h = { lat: 14.066020, lng: 100.597200 };
-        const tu_i = { lat: 14.075945, lng: 100.596154 };
-        const tu_j = { lat: 14.076391, lng: 100.594189 };
+                // Construct the polygon.
+                draw_area = new google.maps.Polygon({
+                    paths: result,
+                    strokeColor: "#008450",
+                    strokeOpacity: 0.8,
+                    strokeWeight: 1,
+                    fillColor: "#008450",
+                    fillOpacity: 0.25,
+                });
+                draw_area.setMap(map);
+                
+            });
 
-        const area_tu = [tu_a,tu_b,tu_c,tu_d,tu_e,tu_f,tu_g,tu_h,tu_i,tu_j,];
+        //ปักหมุดภายในพื้นที่รับผิดชอบ
 
-          // Construct the polygon.
-        const draw_area_tu = new google.maps.Polygon({
-            paths: area_tu,
-            strokeColor: "#008450",
-            strokeOpacity: 0.8,
-            strokeWeight: 1,
-            fillColor: "#008450",
-            fillOpacity: 0.25,
-          });
-          draw_area_tu.setMap(map);
-        // // END พื้นที่ TU
-
-        // พื้นที่ บ้านลัค
-        const luck_a = { lat: 14.187077, lng: 101.162882 };
-        const luck_b = { lat: 14.187651, lng: 101.162502 };
-        const luck_c = { lat: 14.188048, lng: 101.163155 };
-        const luck_d = { lat: 14.188600, lng: 101.163246 };
-        const luck_e = { lat: 14.188644, lng: 101.164103 };
-        const luck_f = { lat: 14.187386, lng: 101.164194 };
-        const luck_h = { lat: 14.187077, lng: 101.164133 };
-
-        const area_luck = [luck_a,luck_b,luck_c,luck_d,luck_e,luck_f,luck_h,];
-
-        // Construct the polygon.
-        const draw_area_luck = new google.maps.Polygon({
-            paths: area_luck,
-            strokeColor: "#008450",
-            strokeOpacity: 0.8,
-            strokeWeight: 1,
-            fillColor: "#008450",
-            fillOpacity: 0.25,
-          });
-          draw_area_luck.setMap(map);
-        // END พื้นที่ บ้านลัค
-
-        // พื้นที่ พระจอมเกล้าพระนครเหนือ
-        const kmutnb_a = { lat: 13.819200, lng: 100.512753 };
-        const kmutnb_b = { lat: 13.818744, lng: 100.514181 };
-        const kmutnb_c = { lat: 13.819357, lng: 100.514901 };
-        const kmutnb_d = { lat: 13.819157, lng: 100.515523 };
-        const kmutnb_e = { lat: 13.819333, lng: 100.515561 };
-        const kmutnb_f = { lat: 13.819222, lng: 100.516228 };
-        const kmutnb_g = { lat: 13.821000, lng: 100.517113 };
-        const kmutnb_i = { lat: 13.822192, lng: 100.514845 };
-        const kmutnb_j = { lat: 13.824593, lng: 100.512288 };
-        const kmutnb_k = { lat: 13.824234, lng: 100.511526 };
-        const kmutnb_l = { lat: 13.823348, lng: 100.512004 };
-        const kmutnb_m = { lat: 13.822952, lng: 100.511392 };
-        const kmutnb_n = { lat: 13.821650, lng: 100.512422 };
-        const kmutnb_o = { lat: 13.820254, lng: 100.514284 };
-
-        const area_kmutnb = [kmutnb_a,kmutnb_b,kmutnb_c,kmutnb_d,kmutnb_e,
-            kmutnb_f,kmutnb_g,kmutnb_i,kmutnb_j,kmutnb_k,kmutnb_l,kmutnb_m,kmutnb_n,kmutnb_o,];
-
-        // Construct the polygon.
-        const draw_area_kmutnb = new google.maps.Polygon({
-            paths: area_kmutnb,
-            strokeColor: "#008450",
-            strokeOpacity: 0.8,
-            strokeWeight: 1,
-            fillColor: "#008450",
-            fillOpacity: 0.25,
-          });
-          draw_area_kmutnb.setMap(map);
-        // END พื้นที่ พระจอมเกล้าพระนครเหนือ
-
-        // พื้นที่ หมู่บ้านซิเมนต์ไทย
-        const thai_cement_a = { lat: 13.833294, lng: 100.539767 };
-        const thai_cement_b = { lat: 13.830202, lng: 100.541761 };
-        const thai_cement_c = { lat: 13.830106, lng: 100.549475 };
-        const thai_cement_d = { lat: 13.833513, lng: 100.551297 };
-        const thai_cement_e = { lat: 13.831744, lng: 100.546931 };
-        const thai_cement_f = { lat: 13.834021, lng: 100.545848 };
-        const thai_cement_g = { lat: 13.833294, lng: 100.544432 };
-        const thai_cement_h = { lat: 13.832725, lng: 100.544441 };
-        const thai_cement_i = { lat: 13.832541, lng: 100.543981 };
-        const thai_cement_j = { lat: 13.834477, lng: 100.542781 };
-        const thai_cement_k = { lat: 13.836728, lng: 100.540570 };
-
-        const area_thai_cement = [thai_cement_a,thai_cement_b,thai_cement_c,thai_cement_d,thai_cement_e,thai_cement_f,thai_cement_g,
-            thai_cement_h,thai_cement_i,thai_cement_j,thai_cement_k,];
-
-        // Construct the polygon.
-        const draw_area_thai_cement = new google.maps.Polygon({
-            paths: area_thai_cement,
-            strokeColor: "#008450",
-            strokeOpacity: 0.8,
-            strokeWeight: 1,
-            fillColor: "#008450",
-            fillOpacity: 0.25,
-          });
-          draw_area_thai_cement.setMap(map);
-        // END พื้นที่ หมู่บ้านซิเมนต์ไทย
     }
 
 </script>
