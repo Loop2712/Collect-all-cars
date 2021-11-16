4286186669427940fc0fc0d691f21b306b153a04
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-11-16 13:27:51 +0700

    Update facebook_messenger_api.php

--- app/Http/Controllers/API/facebook_messenger_api.php
+++ app/Http/Controllers/API/facebook_messenger_api.php
@@ -6,18 +6,19 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\DB;
+use App\Models\Mylog_fb;
 
 class facebook_messenger_api extends Controller
 {
     public function store(Request $request)
 	{
-        // //SAVE LOG
-        // $requestData = $request->all();
-        // $data = [
-        //     "title" => "Line",
-        //     "content" => json_encode($requestData, JSON_UNESCAPED_UNICODE),
-        // ];
-        // MyLog::create($data);  
+        //SAVE LOG
+        $requestData = $request->all();
+        $data = [
+            "title" => "Line",
+            "content" => json_encode($requestData, JSON_UNESCAPED_UNICODE),
+        ];
+        Mylog_fb::create($data);  
 
 	}
 
