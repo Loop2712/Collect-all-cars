21f22b1061dd7029ae6cb0a534c1f779d96ce217
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-11-19 14:44:09 +0700

    Update facebook_messenger_api.php

--- app/Http/Controllers/API/facebook_messenger_api.php
+++ app/Http/Controllers/API/facebook_messenger_api.php
@@ -19,12 +19,24 @@ class facebook_messenger_api extends Controller
         //     "content" => json_encode($requestData, JSON_UNESCAPED_UNICODE),
         // ];
         // Mylog_fb::create($data);  
+        
 
         $verify_token = env('FACEBOOK_MESSENGER_WEBHOOK_TOKEN');
         $access_token = env('PAGE_ACCESS_TOKEN');
+        
+        $hub_verify_token = null;
+        if(isset($_REQUEST['hub_challenge'])) {
+         $challenge = $_REQUEST['hub_challenge'];
+         $hub_verify_token = $_REQUEST['hub_verify_token'];
+        }
+        if ($hub_verify_token === $verify_token) {
+         echo $challenge;
+        }
+
+        
 
         // $input = json_decode(file_get_contents('php://input'), true);
-        
+
         // $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
         // $message = $input['entry'][0]['messaging'][0]['message']['text'];
         // $message_to_reply = 'Hello' . $sender;
