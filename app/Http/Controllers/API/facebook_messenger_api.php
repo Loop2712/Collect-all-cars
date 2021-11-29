779396c33defda7d2a85a04c37dfbf51b282e652
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-11-29 11:25:31 +0700

    Update facebook_messenger_api.php

--- app/Http/Controllers/API/facebook_messenger_api.php
+++ app/Http/Controllers/API/facebook_messenger_api.php
@@ -74,11 +74,14 @@ class facebook_messenger_api extends Controller
 
     public function whatsapp(Request $request)
     {
+
+        $input = json_decode(file_get_contents('php://input'), true);
+
         //SAVE LOG
         $requestData = $request->all();
         $data = [
             "title" => "whatsapp",
-            "content" => 'hello',
+            "content" => $input,
         ];
         Mylog_fb::create($data);  
         
@@ -86,12 +89,15 @@ class facebook_messenger_api extends Controller
         $access_token = env('PAGE_ACCESS_TOKEN');
 
         $hub_verify_token = null;
+
         if(isset($_REQUEST['hub_challenge'])) {
-         $challenge = $_REQUEST['hub_challenge'];
-         $hub_verify_token = $_REQUEST['hub_verify_token'];
+            $challenge = $_REQUEST['hub_challenge'];
+            $hub_verify_token = $_REQUEST['hub_verify_token'];
         }
         if ($hub_verify_token === $verify_token) {
-         echo $challenge;
+            // echo $challenge;
+
+            return $challenge ;
         }
 
     }
