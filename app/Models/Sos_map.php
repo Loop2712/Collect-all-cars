ce4172ff59042ffe4208b44004df8642705121bd
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-12-06 12:46:43 +0700

    update

--- app/Models/Sos_map.php
+++ app/Models/Sos_map.php
@@ -25,7 +25,7 @@ class Sos_map extends Model
      *
      * @var array
      */
-    protected $fillable = ['content', 'name', 'phone', 'lat', 'lng', 'area', 'user_id','photo','CountryCode'];
+    protected $fillable = ['content', 'name', 'phone', 'lat', 'lng', 'area', 'user_id','photo','CountryCode','helper','helper_id'];
 
     
 }
