098061d80ba299b9ce370cd1de85f2d56a9e2684
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-12-07 12:31:41 +0700

    Update SosmapController.php

--- app/Http/Controllers/API/SosmapController.php
+++ app/Http/Controllers/API/SosmapController.php
@@ -56,6 +56,8 @@ class SosmapController extends Controller
             // มีแล้ว
             $data_helper_old = DB::table('users')->where('id' , $helper_id_old)->get();
             $this->_send_notempty_helper($area , $data_helper_old , $name_partner_helper);
+            
+            return view('close_browser');
         }else {
             // ยังไม่มี
             if(Auth::check()){
