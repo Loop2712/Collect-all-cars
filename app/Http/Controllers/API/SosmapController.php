9198b6daf8fc9c1a02f34a912278ecfc3e7e1381
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-12-07 12:28:49 +0700

    Update SosmapController.php

--- app/Http/Controllers/API/SosmapController.php
+++ app/Http/Controllers/API/SosmapController.php
@@ -75,7 +75,7 @@ class SosmapController extends Controller
                 return view('close_browser');
 
             }else{
-                return redirect('/login/line?redirectTo=/sos_map/helper_after_login' . '/' . $id_sos_map);
+                return redirect('/login/line?redirectTo=/sos_map/helper_after_login' . '/' . $id_sos_map . '/' . $id_organization_helper);
             }
         }
         
