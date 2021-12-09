0daf90203a43d58f98b613d2f4545ba4690cb2c0
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-12-09 10:31:54 +0700

    Update LineApiController.php

--- app/Http/Controllers/API/LineApiController.php
+++ app/Http/Controllers/API/LineApiController.php
@@ -575,25 +575,10 @@ class LineApiController extends Controller
             //helper
             $string_json = str_replace("เจ้าหน้าที่",$data_topic[3],$string_json);
             $string_json = str_replace("จาก",$data_topic[4],$string_json);
-
-            foreach ($data_helpers as $data_helper) {
-
-                if (!empty($data_helper->photo)) {
-                    $photo_helper = "https://www.viicheck.com/storage/".$data_helper->photo ;
-                }
-                if (empty($data_helper->photo)) {
-                    $photo_helper = $data_helper->avatar ;
-                }
-
-                $string_json = str_replace("name_helper",$data_helper->name,$string_json);
-                $string_json = str_replace("https://scdn.line-apps.com/n/channel_devcenter/img/flexsnapshot/clip/clip13.jpg",$photo_helper,$string_json);
-
-                if (!empty($data_helper->organization)) {
-                    $string_json = str_replace("..",$data_helper->organization,$string_json);
-                }
-
-            }
-
+            $string_json = str_replace("name_helper",$name_helper,$string_json);
+            $string_json = str_replace("https://scdn.line-apps.com/n/channel_devcenter/img/flexsnapshot/clip/clip13.jpg",$photo_helper,$string_json);
+            $string_json = str_replace("..",$organization_helper,$string_json);
+            
             $messages = [ json_decode($string_json, true) ];
 
             $body = [
