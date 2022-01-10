6aba4902cffc1f62b72b64ee8e2c6406ba2eabb7
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-01-10 14:36:40 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -110,7 +110,6 @@
         <main class="py-5">
             <div class="container">
                 <div class="row">
-                    <!-- @include ('festival')  -->
                     @yield('content')
                     
                 </div>
