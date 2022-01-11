a5219f6aa2cfb9dd0d8f474b72963166f85d33f5
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-01-11 09:34:27 +0700

    Update LineMessagingAPI.php

--- app/Models/LineMessagingAPI.php
+++ app/Models/LineMessagingAPI.php
@@ -959,43 +959,40 @@ class LineMessagingAPI extends Model
                     ->where('provider_id' , $provider_id)
                     ->get();
 
-                // foreach($user as $item){
-                //     if ( !empty($item->driver_license) && !empty($item->driver_license2) ) {
-                //         $template_path = storage_path('../public/json/flex-driver_license.json');   
-                //         $string_json = file_get_contents($template_path);
-                //         $string_json = str_replace("รถยนต์",$data_topic[0],$string_json);
-                //         $string_json = str_replace("จักรยานยนต์",$data_topic[1],$string_json);
-                //         $string_json = str_replace("เพิ่มใบอนุญาตขับรถ",$data_topic[3],$string_json);
-                //         $string_json = str_replace("ccaarr",$item->driver_license,$string_json);
-                //         $string_json = str_replace("mmotorcycle",$item->driver_license2,$string_json);
-                //     }
-                //     if ( !empty($item->driver_license) && empty($item->driver_license2) ) {
-                //         $template_path = storage_path('../public/json/flex-driver_car_license.json');   
-                //         $string_json = file_get_contents($template_path);
-                //         $string_json = str_replace("รถยนต์",$data_topic[0],$string_json);
-                //         $string_json = str_replace("จักรยานยนต์",$data_topic[1],$string_json);
-                //         $string_json = str_replace("เพิ่มใบอนุญาตขับรถ",$data_topic[3],$string_json);
-                //         $string_json = str_replace("ccaarr",$item->driver_license,$string_json);
-                //     }
-                //     if ( empty($item->driver_license) && !empty($item->driver_license2) ) {
-                //         $template_path = storage_path('../public/json/flex-driver_moto_license.json');   
-                //         $string_json = file_get_contents($template_path);
-                //         $string_json = str_replace("รถยนต์",$data_topic[0],$string_json);
-                //         $string_json = str_replace("จักรยานยนต์",$data_topic[1],$string_json);
-                //         $string_json = str_replace("เพิ่มใบอนุญาตขับรถ",$data_topic[3],$string_json);
-                //         $string_json = str_replace("mmotorcycle",$item->driver_license2,$string_json);
-                //     }
-                //     if ( empty($item->driver_license) && empty($item->driver_license2) ) {
-                //         $template_path = storage_path('../public/json/flex-driver_not_license.json');
-                //         $string_json = file_get_contents($template_path);
-                //         $string_json = str_replace("รถยนต์",$data_topic[0],$string_json);
-                //         $string_json = str_replace("จักรยานยนต์",$data_topic[1],$string_json);
-                //         $string_json = str_replace("เพิ่มใบอนุญาตขับรถ",$data_topic[3],$string_json);
-                //     }
-                // }
-
-                $template_path = storage_path('../public/json/flex-driver_license.json');   
-                $string_json = file_get_contents($template_path);
+                foreach($user as $item){
+                    if ( !empty($item->driver_license) && !empty($item->driver_license2) ) {
+                        $template_path = storage_path('../public/json/flex-driver_license.json');   
+                        $string_json = file_get_contents($template_path);
+                        // $string_json = str_replace("รถยนต์",$data_topic[0],$string_json);
+                        // $string_json = str_replace("จักรยานยนต์",$data_topic[1],$string_json);
+                        // $string_json = str_replace("เพิ่มใบอนุญาตขับรถ",$data_topic[3],$string_json);
+                        $string_json = str_replace("ccaarr",$item->driver_license,$string_json);
+                        $string_json = str_replace("mmotorcycle",$item->driver_license2,$string_json);
+                    }
+                    if ( !empty($item->driver_license) && empty($item->driver_license2) ) {
+                        $template_path = storage_path('../public/json/flex-driver_car_license.json');   
+                        $string_json = file_get_contents($template_path);
+                        $string_json = str_replace("รถยนต์",$data_topic[0],$string_json);
+                        $string_json = str_replace("จักรยานยนต์",$data_topic[1],$string_json);
+                        $string_json = str_replace("เพิ่มใบอนุญาตขับรถ",$data_topic[3],$string_json);
+                        $string_json = str_replace("ccaarr",$item->driver_license,$string_json);
+                    }
+                    if ( empty($item->driver_license) && !empty($item->driver_license2) ) {
+                        $template_path = storage_path('../public/json/flex-driver_moto_license.json');   
+                        $string_json = file_get_contents($template_path);
+                        $string_json = str_replace("รถยนต์",$data_topic[0],$string_json);
+                        $string_json = str_replace("จักรยานยนต์",$data_topic[1],$string_json);
+                        $string_json = str_replace("เพิ่มใบอนุญาตขับรถ",$data_topic[3],$string_json);
+                        $string_json = str_replace("mmotorcycle",$item->driver_license2,$string_json);
+                    }
+                    if ( empty($item->driver_license) && empty($item->driver_license2) ) {
+                        $template_path = storage_path('../public/json/flex-driver_not_license.json');
+                        $string_json = file_get_contents($template_path);
+                        $string_json = str_replace("รถยนต์",$data_topic[0],$string_json);
+                        $string_json = str_replace("จักรยานยนต์",$data_topic[1],$string_json);
+                        $string_json = str_replace("เพิ่มใบอนุญาตขับรถ",$data_topic[3],$string_json);
+                    }
+                }
 
                 $messages = [ json_decode($string_json, true) ]; 
                 break;
