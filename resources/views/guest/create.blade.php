5ec67a3b02bd66d4802ad1e00bbee95f92b3b51b
luxlnw171041 <54930672+luxlnw171041@users.noreply.github.com>
2022-01-12 21:53:49 +0700

    Update create.blade.php

--- resources/views/guest/create.blade.php
+++ resources/views/guest/create.blade.php
@@ -15,7 +15,74 @@
                 </div>
                 
                 <div class="card">
-                    <div class="card-header">ข้อมูลรถที่ต้องการติดต่อ</div>
+                    <div class="card-header">ข้อมูลรถที่ต้องการติดต่อ
+                        
+                        <span class="float-right">
+                            <a id="car_TH" href="#" class="d-none"  style="padding:0px">
+                                <img width="40px" src="{{ asset('/img/national-flag/th1.png') }}" style= "border-radius: 5px;"> 
+                            </a>
+                        </span>
+                        <span>
+                            <a id="car_ID" href="#" class="d-none"  style="padding:0px">
+                                <img width="40px" src="{{ asset('/img/national-flag/ID.png') }}" style= "border-radius: 5px; border: 1px solid; color:#8C8C8C;"> 
+                            </a>
+                        </span>
+                        <span>
+                            <a id="car_LA" href="#" class="d-none"  style="padding:0px">
+                                <img width="40px" src="{{ asset('/img/national-flag/la1.png') }}" style= "border-radius: 5px;"> 
+                            </a>
+                        </span>
+                        <span>
+                            <a id="car_PH" href="#" class="d-none"  style="padding:0px">
+                                <img width="40px" src="{{ asset('/img/national-flag/ph1.png') }}" style= "border-radius: 5px;"> 
+                            </a>
+                        </span>
+                        <span>
+                            <a id="car_MM" href="#" class="d-none"  style="padding:0px">
+                                <img width="40px" src="{{ asset('/img/national-flag/my1.png') }}" style= "border-radius: 5px;"> 
+                            </a>
+                        </span>
+                        <span>
+                            <a id="car_SG" href="#" class="d-none"  style="padding:0px">
+                                <img width="40px" src="{{ asset('/img/national-flag/sg1.png') }}" style= "border-radius: 5px;border: 1px solid; color:#8C8C8C;"> 
+                            </a>
+                        </span>
+                        <span>
+                            <a id="car_KR" href="#" class="d-none"  style="padding:0px">
+                                <img width="40px" src="{{ asset('/img/national-flag/kr1.png') }}" style= "border-radius: 5px;"> 
+                            </a>
+                        </span>
+                        <span>
+                            <a id="car_BN" href="#" class="d-none"  style="padding:0px">
+                                <img width="40px" src="{{ asset('/img/national-flag/bn1.png') }}" style= "border-radius: 5px;"> 
+                            </a>
+                        </span>
+                        <span>
+                            <a id="car_VN" href="#" class="d-none"  style="padding:0px">
+                                <img width="40px" src="{{ asset('/img/national-flag/vn1.png') }}" style= "border-radius: 5px;"> 
+                            </a>
+                        </span>
+                        <span>
+                            <a id="car_MY" href="#" class="d-none"  style="padding:0px">
+                                <img width="40px" src="{{ asset('/img/national-flag/ml1.jpg') }}" style= "border-radius: 5px;"> 
+                            </a>
+                        </span>
+                        <span>
+                            <a id="car_JP" href="#" class="d-none"  style="padding:0px">
+                                <img width="40px" src="{{ asset('/img/national-flag/jp.png') }}" style= "border-radius: 5px; border: 1px solid; color:#8C8C8C;"> 
+                            </a>
+                        </span>
+                        <span>
+                            <a id="car_KO" href="#" class="d-none"  style="padding:0px">
+                                <img width="40px" src="{{ asset('/img/national-flag/ko1.png') }}" style= "border-radius: 5px;border: 1px solid; color:#8C8C8C;"> 
+                            </a>
+                        </span>
+                        <span>
+                            <a id="car_CN" href="#" class="d-none"  style="padding:0px">
+                                <img width="40px" src="{{ asset('/img/national-flag/cn.png') }}" style= "border-radius: 5px;"> 
+                            </a>
+                        </span>
+                    </div>
                     <div class="card-body">
                         <a href="{{ url('/guest') }}" title="Back"><button class="d-none btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
 
