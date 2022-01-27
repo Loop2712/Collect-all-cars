5e66ec53514c84e36873d96a797237505ba34f50
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-01-27 13:41:45 +0700

    Update check_in_finish.blade.php

--- resources/views/check_in/check_in_finish.blade.php
+++ resources/views/check_in/check_in_finish.blade.php
@@ -34,9 +34,9 @@
         </div>
     </div>
 
-    <h3 class="btn btn-success" onclick="check_add_line();">
+    <a style="height: 300px;" class="btn btn-success" onclick="check_add_line();">
       เสร็จสิ้น
-    </h3>
+    </a>
   </div>
 </center>
 <input class="d-none" type="text" name="uesr_add_line" id="uesr_add_line" value="{{ Auth::user()->add_line }}">
