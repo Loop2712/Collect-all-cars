f61553249761da9324fb3eaaaad8e1e651798701
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-01-27 15:42:49 +0700

    Update partner_add_area.blade.php

--- resources/views/partner/service_area/partner_add_area.blade.php
+++ resources/views/partner/service_area/partner_add_area.blade.php
@@ -171,7 +171,7 @@
                 </div>
 				<div class="ms-auto">
 					@if(Auth::check())
-						@if(Auth::user()->id == 21 or Auth::user()->id == 2 or Auth::user()->id == 1)
+						@if(Auth::user()->id == "admin-partner" or Auth::user()->id == 2 or Auth::user()->id == 1)
 							<a id="btn_add_area" class="btn text-white float-right" style="background-color: #008450;" data-toggle="collapse" data-target="#div_name_partner" aria-expanded="false" aria-controls="div_name_partner">
 								<i class="fadeIn animated bx bx-add-to-queue"></i>เพิ่มพื้นที่บริการใหม่
 							</a>
