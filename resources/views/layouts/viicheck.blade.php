3aafbc4cf4791443f5a94344a088c45d56d0a849
luxlnw171041 <54930672+luxlnw171041@users.noreply.github.com>
2022-02-10 09:56:53 +0700

    Update viicheck.blade.php

--- resources/views/layouts/viicheck.blade.php
+++ resources/views/layouts/viicheck.blade.php
@@ -13,7 +13,7 @@
   <!-- Favicons -->
  <!-- icon -->
  <link href="https://kit-pro.fontawesome.com/releases/v5.15.1/css/pro.min.css" rel="stylesheet">
-
+ <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
