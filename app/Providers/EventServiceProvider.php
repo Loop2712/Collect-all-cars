1365a4509e75f2137ce49693863d0b56136a738c
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-03-03 12:27:13 +0700

    Update EventServiceProvider.php

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -20,7 +20,7 @@ class EventServiceProvider extends ServiceProvider
         ],
         \SocialiteProviders\Manager\SocialiteWasCalled::class => [
         // ... other providers
-        'SocialiteProviders\\Line\\LineExtendSocialite@handle',
+        \SocialiteProviders\Line\LineExtendSocialite::class.'@handle',
     ],
     ];
 
