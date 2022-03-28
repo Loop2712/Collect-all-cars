2b53f9526d4103890fda727e25c98cbb0d0b9d05
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-03-28 15:20:44 +0700

    update

--- resources/views/name_-university/form.blade.php
+++ resources/views/name_-university/form.blade.php
@@ -15,7 +15,7 @@
 </div>
 <div class="form-group {{ $errors->has('initials_en') ? 'has-error' : ''}}">
     <label for="initials_en" class="control-label">{{ 'Initials En' }}</label>
-    <input class="form-control" name="initials_en" type="number" id="initials_en" value="{{ isset($name_university->initials_en) ? $name_university->initials_en : ''}}" >
+    <input class="form-control" name="initials_en" type="text" id="initials_en" value="{{ isset($name_university->initials_en) ? $name_university->initials_en : ''}}" >
     {!! $errors->first('initials_en', '<p class="help-block">:message</p>') !!}
 </div>
 
