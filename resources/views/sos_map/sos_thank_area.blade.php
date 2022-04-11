6200179433e5ba3b488573416e32228ae32c7a2a
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-04-11 11:25:03 +0700

    Revert "Update sos_thank_area.blade.php"
    This reverts commit 0834041a8716315d4cfae60d5a41f77203b18723.

--- resources/views/sos_map/sos_thank_area.blade.php
+++ resources/views/sos_map/sos_thank_area.blade.php
@@ -94,7 +94,6 @@
               </div>
             </div>
         </center>
-        <br>
       </div>
       <div class="modal-footer">
         <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">ปิด</button> -->
