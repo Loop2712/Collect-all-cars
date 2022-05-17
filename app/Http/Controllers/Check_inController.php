4cbd8d6f3d48cdc96efe7bae57c3d0a8c5dafc17
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-05-17 11:47:50 +0700

    Update Check_inController.php

--- app/Http/Controllers/Check_inController.php
+++ app/Http/Controllers/Check_inController.php
@@ -116,6 +116,8 @@ class Check_inController extends Controller
             ]);
         }
 
+        $requestData['check_in_at'] = str_replace("_"," ",$requestData['check_in_at']);
+
         // ค้นหา name && name_area 
         $data_partner_name_area = Partner::where('name' , $requestData['check_in_at'])
             ->where('name_area' , $requestData['name_area'])
