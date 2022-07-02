40f2ba02ef6e74e10799ebf2b77bbaba57fb9778
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-07-02 22:56:42 +0700

    Update Condo_LineMessagingAPI.php

--- app/Models/Condo_LineMessagingAPI.php
+++ app/Models/Condo_LineMessagingAPI.php
@@ -203,16 +203,16 @@ class Condo_LineMessagingAPI extends Model
         $string_json = str_replace("หมวดหมู่",$data_notify_repair->category,$string_json);
         $string_json = str_replace("photo_notify_repair.png",$data_notify_repair->photo,$string_json);
         $string_json = str_replace("content",$data_notify_repair->content,$string_json);
-        $string_json = str_replace("datetime",$data_notify_repair->appointment_date . "เวลา" . $data_notify_repair->appointment_time . ":00",$string_json);
+        // $string_json = str_replace("datetime",$data_notify_repair->appointment_date . "เวลา" . $data_notify_repair->appointment_time . ":00",$string_json);
 
-        if (!empty($data_notify_repair->user_condo_id)) {
-            $name_user_condo = "อาคาร " . $data_notify_repair->user_condo->building . " ห้อง " . $data_notify_repair->user_condo->room_number ;
-            $string_json = str_replace("name_user",$name_user_condo,$string_json);
-        }else{
-            $string_json = str_replace("name_user","นิติ",$string_json);
-        }
+        // if (!empty($data_notify_repair->user_condo_id)) {
+        //     $name_user_condo = "อาคาร " . $data_notify_repair->user_condo->building . " ห้อง " . $data_notify_repair->user_condo->room_number ;
+        //     $string_json = str_replace("name_user",$name_user_condo,$string_json);
+        // }else{
+        //     $string_json = str_replace("name_user","นิติ",$string_json);
+        // }
 
-        $string_json = str_replace("VA_notify_repair",$data_notify_repair->id,$string_json);
+        // $string_json = str_replace("VA_notify_repair",$data_notify_repair->id,$string_json);
 
         $messages = [ json_decode($string_json, true) ];
 
