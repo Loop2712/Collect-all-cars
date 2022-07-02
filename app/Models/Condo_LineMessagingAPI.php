bc3bb3cdd050cc582c479803ce9627bbfd62b7c7
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-07-02 22:54:35 +0700

    Update Condo_LineMessagingAPI.php

--- app/Models/Condo_LineMessagingAPI.php
+++ app/Models/Condo_LineMessagingAPI.php
@@ -195,24 +195,24 @@ class Condo_LineMessagingAPI extends Model
         $photo_condo = $data_condos->partner->logo ;
         $data_groupline = Group_line::where('system','notify_repair')->where('condo_id',$data_condos->id)->first();
 
-        $template_path = storage_path('../public/json/hello_group_line_condo.json');   
+        $template_path = storage_path('../public/json/send_TO_notify_repair.json');   
         $string_json = file_get_contents($template_path);
         $string_json = str_replace("ตัวอย่าง","การแจ้งซ่อมบำรุง",$string_json);
-        // $string_json = str_replace("VA_photo_condo",$photo_condo,$string_json);
-        // $string_json = str_replace("หัวเรื่อง",$data_notify_repair->title,$string_json);
-        // $string_json = str_replace("หมวดหมู่",$data_notify_repair->category,$string_json);
-        // $string_json = str_replace("photo_notify_repair.png",$data_notify_repair->photo,$string_json);
-        // $string_json = str_replace("content",$data_notify_repair->content,$string_json);
-        // $string_json = str_replace("datetime",$data_notify_repair->appointment_date . "เวลา" . $data_notify_repair->appointment_time . ":00",$string_json);
+        $string_json = str_replace("VA_photo_condo",$photo_condo,$string_json);
+        $string_json = str_replace("หัวเรื่อง",$data_notify_repair->title,$string_json);
+        $string_json = str_replace("หมวดหมู่",$data_notify_repair->category,$string_json);
+        $string_json = str_replace("photo_notify_repair.png",$data_notify_repair->photo,$string_json);
+        $string_json = str_replace("content",$data_notify_repair->content,$string_json);
+        $string_json = str_replace("datetime",$data_notify_repair->appointment_date . "เวลา" . $data_notify_repair->appointment_time . ":00",$string_json);
 
-        // if (!empty($data_notify_repair->user_condo_id)) {
-        //     $name_user_condo = "อาคาร " . $data_notify_repair->user_condo->building . " ห้อง " . $data_notify_repair->user_condo->room_number ;
-        //     $string_json = str_replace("name_user",$name_user_condo,$string_json);
-        // }else{
-        //     $string_json = str_replace("name_user","นิติ",$string_json);
-        // }
+        if (!empty($data_notify_repair->user_condo_id)) {
+            $name_user_condo = "อาคาร " . $data_notify_repair->user_condo->building . " ห้อง " . $data_notify_repair->user_condo->room_number ;
+            $string_json = str_replace("name_user",$name_user_condo,$string_json);
+        }else{
+            $string_json = str_replace("name_user","นิติ",$string_json);
+        }
 
-        // $string_json = str_replace("VA_notify_repair",$data_notify_repair->id,$string_json);
+        $string_json = str_replace("VA_notify_repair",$data_notify_repair->id,$string_json);
 
         $messages = [ json_decode($string_json, true) ];
 
