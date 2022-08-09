e011d075ede0897fd01851295941c8d3a15d4549
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-08-09 11:42:05 +0700

    Update partner_sos.blade.php

--- resources/views/partner/partner_sos.blade.php
+++ resources/views/partner/partner_sos.blade.php
@@ -296,7 +296,7 @@
                         @if(!empty($item->remark))
                         <div class="col-12">
                             <b>หมายเหตุจากเจ้าหน้าที่ : </b> {{ $item->remark }}
-                            <br>
+                            <br><br>
                         </div>
                         @endif
                         <hr>
