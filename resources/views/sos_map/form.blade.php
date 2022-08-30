744d72ceb1c74ba9d86122df3cc0b85ff3ca454c
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-08-30 15:01:59 +0700

    Update form.blade.php

--- resources/views/sos_map/form.blade.php
+++ resources/views/sos_map/form.blade.php
@@ -89,13 +89,13 @@
                             <i class="fas fa-bullhorn"></i> <b>Ask for HELP</b>
                         </a>
                     </div> 
-                    <!-- @if(Auth::user()->id == "1")
+                    @if(Auth::user()->id == "1")
                     <div class="col-12 mt-3 d-none" id="btn_emergency_js100">
                         <button class="shadow btn btn-md btn-block"  style="font-family: 'Kanit', sans-serif;border-radius:10px;color:white;background-color:#08361d;" onclick="call_sos_of_js100();">
                             <b><i class="fa-regular fa-light-emergency-on"></i> &nbsp;Call Emergency  JS 100</b>
                         </button>
                     </div>
-                    @endif -->
+                    @endif
                 </div>
             </div>
 
@@ -445,33 +445,33 @@
         
     }
 
-    function call_sos_of_js100() {
-        let text_phone = document.querySelector("#text_phone");
-        let lat_text = document.querySelector("#lat");
-        let lng_text = document.querySelector("#lng");
+    // function call_sos_of_js100() {
+    //     let text_phone = document.querySelector("#text_phone");
+    //     let lat_text = document.querySelector("#lat");
+    //     let lng_text = document.querySelector("#lng");
 
-        let content = document.querySelector("#content");
-        content.value = 'emergency_js100';
+    //     let content = document.querySelector("#content");
+    //     content.value = 'emergency_js100';
 
-        let btn_tel = document.querySelector('#btn_tel');
+    //     let btn_tel = document.querySelector('#btn_tel');
 
-        let tag_a = document.createElement("a");
+    //     let tag_a = document.createElement("a");
 
-        let tag_a_href = document.createAttribute("href");
-        tag_a_href.value = 'tel:1137';
-        tag_a.setAttributeNode(tag_a_href);
+    //     let tag_a_href = document.createAttribute("href");
+    //     tag_a_href.value = 'tel:1137';
+    //     tag_a.setAttributeNode(tag_a_href);
 
-        let tag_a_id = document.createAttribute("id");
-        tag_a_id.value = 'btn_js_1137';
-        tag_a.setAttributeNode(tag_a_id);
+    //     let tag_a_id = document.createAttribute("id");
+    //     tag_a_id.value = 'btn_js_1137';
+    //     tag_a.setAttributeNode(tag_a_id);
 
-        btn_tel.appendChild(tag_a);
+    //     btn_tel.appendChild(tag_a);
 
-        document.querySelector("#btn_js_1137").click();
-        document.querySelector("#btn_submit").click();
+    //     document.querySelector("#btn_js_1137").click();
+    //     document.querySelector("#btn_submit").click();
 
 
-    }
+    // }
 
     function check_input_pls_input_phone(){
 
