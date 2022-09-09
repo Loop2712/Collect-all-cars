44406483fcfe6b4de2fc48946e0628267e81e15b
luxlnw171041 <54930672+luxlnw171041@users.noreply.github.com>
2022-09-09 15:31:07 +0700

    Update main.js

--- public/carousel-12/js/main.js
+++ public/carousel-12/js/main.js
@@ -148,7 +148,68 @@ $(function () {
       }
     });
   }
-
+  if ($('.owl-4').length > 0) {
+    $('.owl-4').owlCarousel({
+      loop:true,
+      margin:10,
+      smartSpeed: 1000,
+      autoplay: true,
+      responsiveClass:true,
+      responsive:{
+          0:{
+              items:6,
+              nav:true
+          },
+          600:{
+              items:6,
+              nav:false
+          },
+          1000:{
+              items:1,
+              nav:true
+          }
+      }
+    });
+  }
+  
+  if ($('.owl-5').length > 0) {
+    $('.owl-5').owlCarousel({
+      center: false,
+      items: 1,
+      loop: true,
+      stagePadding: 0,
+      margin: 20,
+      smartSpeed: 1000,
+      autoplay: true,
+      nav: true,
+      dots: true,
+      pauseOnHover: false,
+      responsive: {
+        0: {
+          margin: 20,
+          nav: true,
+          items: 5
+        },
+        700: {
+          margin: 20,
+          stagePadding: 0,
+          nav: true,
+          items: 7
+        },
+        600: {
+          margin: 20,
+          nav: true,
+          items: 10
+        },
+        1000: {
+          margin: 0,
+          stagePadding: 0,
+          nav: true,
+          items: 11
+        }
+      }
+    });
+  }
   
 
 })
