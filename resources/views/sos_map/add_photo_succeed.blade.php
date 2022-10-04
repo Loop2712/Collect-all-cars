3412d3fbeac02a8a1a80afbdcf7ad7108e38e3a9
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-10-04 11:45:45 +0700

    Update add_photo_succeed.blade.php

--- resources/views/sos_map/add_photo_succeed.blade.php
+++ resources/views/sos_map/add_photo_succeed.blade.php
@@ -93,15 +93,12 @@
                 </div>
             </div>
 
-            <button id="backbutton" style="width:100%;" class="btn btn-success main-shadow main-radius mt-3">เสร็จสิ้น</button>
+            <button id="backbutton" style="width:100%;" class="btn btn-success main-shadow main-radius mt-3" onclick="onBackKeyDown();">เสร็จสิ้น</button>
 
             <script>
-                document.addEventListener('backbutton', onBackKeyDown, false);
 
                 function onBackKeyDown(event) {
-                    // Handle the back button
-                    event.preventDefault();
-                    alert('I am a demo purpose alert thingy');
+                    window.history.forward();
                 }
             </script>
         
