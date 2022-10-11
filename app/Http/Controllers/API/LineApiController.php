9ee24b990684790bceca24aefde68303989a2a1d
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-10-11 15:10:00 +0700

    Update LineApiController.php

--- app/Http/Controllers/API/LineApiController.php
+++ app/Http/Controllers/API/LineApiController.php
@@ -683,10 +683,10 @@ class LineApiController extends Controller
 
         // datetime
         $time_zone_explode = explode(" ",$time_zone);
-        
+
         $date = $time_zone_explode[0] ;
         $time = $time_zone_explode[1] ;
-        $utc = $time_zone_explode[-1] ;
+        $utc = $time_zone_explode[3] ;
 
         $data_topic = [
                     "การขอความช่วยเหลือ",
@@ -721,18 +721,18 @@ class LineApiController extends Controller
         // user
         $string_json = str_replace("name_user",$data_sos_map->name,$string_json);
         
-        // $data_users = DB::table('users')->where('id', $data_sos_map->user_id)->first();
-        // if (!empty($data_users->photo)) {
-        //     $string_json = str_replace("TEXT_PHOTO_USER",$data_users->photo,$string_json);
+        $data_users = DB::table('users')->where('id', $data_sos_map->user_id)->first();
+        if (!empty($data_users->photo)) {
+            $string_json = str_replace("TEXT_PHOTO_USER",$data_users->photo,$string_json);
 
-        // }
+        }
         // helper
         $string_json = str_replace("name_helper",$name_helper,$string_json);
 
-        // $data_helpers = DB::table('users')->where('id', $helper_id)->first();
-        // if (!empty($data_helpers->photo)) {
-        //     $string_json = str_replace("TEXT_PHOTO_HELPER",$data_helpers->photo,$string_json);
-        // }
+        $data_helpers = DB::table('users')->where('id', $helper_id)->first();
+        if (!empty($data_helpers->photo)) {
+            $string_json = str_replace("TEXT_PHOTO_HELPER",$data_helpers->photo,$string_json);
+        }
 
         $string_json = str_replace("id_sos_map",$data_sos_map->id,$string_json);
         $string_json = str_replace("date",$date,$string_json);
