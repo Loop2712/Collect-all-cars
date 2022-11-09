0d591e7ee57277245ec51b8792f55a5524438ab1
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-11-09 17:47:10 +0700

    Update CarbrandController.php

--- app/Http/Controllers/API/CarbrandController.php
+++ app/Http/Controllers/API/CarbrandController.php
@@ -502,6 +502,7 @@ class CarbrandController extends Controller
             ->where('id', $requestData['id_content'])
             ->update([
                 'user_click' => $arr_user_click ,
+                'send_round' => $requestData['redirectTo'] ,
         ]);
 
         return redirect($requestData['redirectTo'])->with('flash_message', 'Partner updated!');
