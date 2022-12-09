9086ff32e9a52ec8f18b240443175006528222bf
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-12-09 21:01:15 +0700

    Update partner_sos.blade.php

--- resources/views/partner/partner_sos.blade.php
+++ resources/views/partner/partner_sos.blade.php
@@ -177,7 +177,7 @@
                                 <a href="#" class="btn btn-sm btn-danger radius-30" >
                                     <i class="fadeIn animated bx bx-x"></i>ยังไม่ได้ดำเนินการ
                                 </a>
-                                <a type="button" style="margin-top: 10px;" class="d-none btn btn-sm radius-30 notify_alert_gotohelp" 
+                                <a type="button" style="margin-top: 10px;" class="btn btn-sm radius-30 notify_alert_gotohelp" 
                                 onclick="go_to_help('{{ $item->id }}' , '{{ Auth::user()->id }}' )">
                                     <i class="fa-solid fa-truck-medical"></i> กำลังไปช่วยเหลือ
                                 </a>
