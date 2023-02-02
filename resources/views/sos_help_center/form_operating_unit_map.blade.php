53834f3f2e35f08e7639415d5170c2bea8ca5fab
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2023-02-02 15:28:06 +0700

    Update form_operating_unit_map.blade.php

--- resources/views/sos_help_center/form_operating_unit_map.blade.php
+++ resources/views/sos_help_center/form_operating_unit_map.blade.php
@@ -647,9 +647,11 @@ function wait_operating_unit(sos_id){
                             // ADD DATA officers
                             if (result_2['img_officer']) {
                                 let data_img_officers = document.querySelector('#data_img_officers');
-                                let img_url = "https://www.viicheck.com/storage" + "/" + result_2['img_officer'] ;
-                                data_img_officers.src = img_url  ;
+                                let data_img_officers_src = document.createAttribute("src");
+                                    data_img_officers_src.value = '{{ url("storage") }}' + '/' + result_2['img_officer'] ;
+                                    data_img_officers.setAttributeNode(data_img_officers_src);
                             }
+                            
                             let data_name_officers = document.querySelector('#data_name_officers');
                                 data_name_officers.innerHTML = result_2['name_officer'] ;
                             let data_sub_organization_officers = document.querySelector('#data_sub_organization_officers');
