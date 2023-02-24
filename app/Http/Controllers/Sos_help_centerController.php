996d6ef0076a1570ce097d2d2f0f9afdcbc1d762
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2023-02-24 11:04:11 +0700

    Update Sos_help_centerController.php

--- app/Http/Controllers/Sos_help_centerController.php
+++ app/Http/Controllers/Sos_help_centerController.php
@@ -295,8 +295,8 @@ class Sos_help_centerController extends Controller
         $data_arr["symptom_other"] = $data_form_yellow->symptom_other ;
         $data_arr["idc"] = $data_form_yellow->idc ;
         $data_arr["vehicle_type"] = $data_form_yellow->vehicle_type ;
-        $data_arr["operating_suit_type"] = $data_form_yellow->operating_suit_type ;
-        $data_arr["operation_unit_name"] = $data_form_yellow->operation_unit_name ;
+        // $data_arr["operating_suit_type"] = $data_form_yellow->operating_suit_type ;
+        // $data_arr["operation_unit_name"] = $data_form_yellow->operation_unit_name ;
         $data_arr["action_set_name"] = $data_form_yellow->action_set_name ;
         $data_arr["time_create_sos"] = $data_form_yellow->time_create_sos ;
         $data_arr["time_command"] = $data_form_yellow->time_command ;
@@ -655,7 +655,7 @@ class Sos_help_centerController extends Controller
                     'vehicle_type' => $data_officers->vehicle_type,
                     'operating_suit_type' => $data_officers->level,
                     'time_go_to_help' => $date_now,
-                    'name_user' => $data_unit->name,
+                    'operation_unit_name' => $data_unit->name,
                     'action_set_name' => $data_user->name,
                 ]);
 
