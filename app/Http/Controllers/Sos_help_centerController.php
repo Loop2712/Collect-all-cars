2e5cdc44954668420de816b3f04c915015dd9334
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2023-02-24 08:55:44 +0700

    Update Sos_help_centerController.php

--- app/Http/Controllers/Sos_help_centerController.php
+++ app/Http/Controllers/Sos_help_centerController.php
@@ -642,7 +642,7 @@ class Sos_help_centerController extends Controller
                     'operating_unit_id' => $data_unit->id,
                     'name_helper' => $data_user->name,
                     'helper_id' => $data_user->id,
-                    // 'time_go_to_help' => $date_now,
+                    'time_go_to_help' => $date_now,
                     'wait' => null,
                 ]);
 
@@ -654,7 +654,7 @@ class Sos_help_centerController extends Controller
             ->update([
                     'vehicle_type' => $data_officers->vehicle_type,
                     'operating_suit_type' => $data_officers->level,
-                    // 'time_go_to_help' => $date_now,
+                    'time_go_to_help' => $date_now,
                     'operation_unit_name' => $data_unit->name,
                     'action_set_name' => $data_user->name,
                 ]);
