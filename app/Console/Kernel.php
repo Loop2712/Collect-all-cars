9b2b57f9cfdce93ee7ed514aa371405a7db4b76e
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2023-03-09 15:38:54 +0700

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,11 +27,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     { 
-        // $schedule->command('cron:alert_act')->dailyAt('08:00')->withoutOverlapping(5);
-        $schedule->command('cron:alert_act')->everyTwoMinutes()->withoutOverlapping(5);
-        $schedule->command('cron:cancel_after_6_month')->everyMinute()->withoutOverlapping(5);
-        $schedule->command('cron:delete_check_in_after_15_day')->everyMinute()->withoutOverlapping(5);
-        $schedule->command('cron:set_null_for_gen_codea')->everyMinute()->withoutOverlapping(5);
+        $schedule->command('cron:alert_act')->dailyAt('08:00')->withoutOverlapping(5);
+        $schedule->command('cron:cancel_after_6_month')->dailyAt('08:00')->withoutOverlapping(5);
+        $schedule->command('cron:delete_check_in_after_15_day')->dailyAt('08:00')->withoutOverlapping(5);
+        $schedule->command('cron:set_null_for_gen_code')->dailyAt('00:01')->withoutOverlapping(5);
     }
 
 
