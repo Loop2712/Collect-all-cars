87d8a0bd7e1c27972e04cab5f279d7130934ff09
luxlnw171041 <54930672+luxlnw171041@users.noreply.github.com>
2023-03-10 12:35:43 +0700

    Update show_user.blade.php

--- resources/views/sos_help_center/show_user.blade.php
+++ resources/views/sos_help_center/show_user.blade.php
@@ -128,6 +128,153 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#00ffffff', e
 	bottom: 0;
 	margin: auto;
 	padding: auto;
+}.close-data{
+	animation: slide-hide 1s ease 0s 1 normal forwards;
+}
+
+@keyframes slide-hide {
+	0% {
+		opacity: 1;
+		transform: translateX(0);
+	}
+
+	100% {
+		opacity: 0;
+		transform: translateX(-100px);
+	}
+}.show-data{
+	animation: show-data 1s ease 0s 1 normal forwards;
+}
+
+@keyframes show-data {
+	0% {
+		opacity: 0;
+		transform: translateX(100px);
+	}
+
+	100% {
+		opacity: 1 !important;
+		transform: translateX(0);
+	}
+}.officer-arrive{
+	z-index: 999999;
+}
+</style>
+
+<style>
+	.pl {
+  display: block;
+  width: 5.375em;
+  height: 5.375em;
+}
+
+.pl__arrows,
+.pl__ring-rotate,
+.pl__ring-stroke,
+.pl__tick {
+  animation-duration: 2s;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+}
+
+.pl__arrows {
+  animation-name: arrows42;
+  transform: rotate(45deg);
+  transform-origin: 16px 52px;
+}
+
+.pl__ring-rotate,
+.pl__ring-stroke {
+  transform-origin: 80px 80px;
+}
+
+.pl__ring-rotate {
+  animation-name: ringRotate42;
+}
+
+.pl__ring-stroke {
+  animation-name: ringStroke42;
+  transform: rotate(-45deg);
+}
+
+.pl__tick {
+  animation-name: tick42;
+}
+
+.pl__tick:nth-child(2) {
+  animation-delay: -1.75s;
+}
+
+.pl__tick:nth-child(3) {
+  animation-delay: -1.5s;
+}
+
+.pl__tick:nth-child(4) {
+  animation-delay: -1.25s;
+}
+
+.pl__tick:nth-child(5) {
+  animation-delay: -1s;
+}
+
+.pl__tick:nth-child(6) {
+  animation-delay: -0.75s;
+}
+
+.pl__tick:nth-child(7) {
+  animation-delay: -0.5s;
+}
+
+.pl__tick:nth-child(8) {
+  animation-delay: -0.25s;
+}
+
+/* Animations */
+@keyframes arrows42 {
+  from {
+    transform: rotate(45deg);
+  }
+
+  to {
+    transform: rotate(405deg);
+  }
+}
+
+@keyframes ringRotate42 {
+  from {
+    transform: rotate(0);
+  }
+
+  to {
+    transform: rotate(720deg);
+  }
+}
+
+@keyframes ringStroke42 {
+  from,
+	to {
+    stroke-dashoffset: 452;
+    transform: rotate(-45deg);
+  }
+
+  50% {
+    stroke-dashoffset: 169.5;
+    transform: rotate(-180deg);
+  }
+}
+
+@keyframes tick42 {
+  from,
+	3%,
+	47%,
+	to {
+    stroke-dashoffset: -12;
+  }
+
+  14%,
+	36% {
+    stroke-dashoffset: 0;
+  }
 }
 </style>
 <img class="logo-viichek" src="{{ asset('/img/logo/logo-viicheck-outline.png') }}" />
@@ -135,11 +282,8 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#00ffffff', e
 	<div class="sry-open-location">
 		<img src="{{ asset('/img/more/sorry-no-text.png') }}" />
 		<center>
-			<p class="sry-open-location-text h4" style="top: 35%;">ขออภัยค่ะ</p>	
-			<p class="sry-open-location-text h5" style="top: 45%;">ดำเนินการไม่สำเร็จ กรุณาเปิดตำแหน่งที่ตั้ง และลองใหม่อีกครั้งค่ะ</p>
-			<span style="top: 60%;" class="sry-open-location-text btn btn-md btn-warning main-shadow main-radius" onclick="window.location.reload(true);">
-				<i class="fa-solid fa-arrows-rotate"></i> โหลดใหม่
-			</span>
+			<p class="sry-open-location-text h4" style="top: 45%;">ขออภัยค่ะ</p>	
+			<p class="sry-open-location-text h5" style="top: 55%;">ดำเนินการไม่สำเร็จ กรุณาเปิดตำแหน่งที่ตั้ง และลองใหม่อีกครั้งค่ะ</p>
 		</center>
 	</div>	
 </div>
@@ -177,11 +321,92 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#00ffffff', e
 		</div>
 	</div>
 
-	<div class="container bg-white open-location-pls w-100" style="bottom: -4.5%;">
+	<div class="container bg-white officer-arrive w-100 d-none" style="bottom: -4.5%;">
 		<div class="w-100 text-center">
-			<img  src="{{ asset('/img/stickerline/PNG/7.png') }}" width="120"  alt="">
+			<img  src="{{ asset('/img/stickerline/PNG/34.png') }}" width="80"  alt="">
 			<br>
-			<h5 class="font-weight-bold mb-0 notranslate">กรุณาเปิดตำแหน่งที่ตั้งด้วยค่ะ</h5>		
+			<h5 class="font-weight-bold mb-0 notranslate mt-2" style="color: #808080;text-overflow: ellipsis;overflow: hidden;white-space: nowrap;">สวัสดีคุณ {{ $data_user->name }}</h5>
+			<h6 class="mb-0 notranslate mt-1" style="color: #808080;">เจ้าหน้าที่มาถึงแล้ว</h6> 
+			<button class="btn-outline-success btn btn-block w-100 p-2 mt-3" style="border-radius: 10px;">เสร็จสิ้น</button>		
+		</div>
+	</div>
+
+	<div class="container bg-white open-location-pls w-100" style="bottom: 5%;height:25% !important;">
+		<div class="w-100 text-center" style="margin-top: -25%;">
+			<div class="d-flex justify-content-center">
+				<svg class="pl" viewBox="0 0 160 160" width="160px" height="160px" xmlns="http://www.w3.org/2000/svg">
+				<defs>
+					<linearGradient id="grad" x1="0" y1="0" x2="0" y2="1">
+						<stop offset="0%" stop-color="#000"></stop>
+						<stop offset="100%" stop-color="#fff"></stop>
+					</linearGradient>
+					<mask id="mask1">
+						<rect x="0" y="0" width="160" height="160" fill="url(#grad)"></rect>
+					</mask>
+					<mask id="mask2">
+						<rect x="28" y="28" width="104" height="104" fill="url(#grad)"></rect>
+					</mask>
+				</defs>
+				
+				<g>
+					<g class="pl__ring-rotate">
+						<circle class="pl__ring-stroke" cx="80" cy="80" r="72" fill="none" stroke="hsl(223,90%,55%)" stroke-width="16" stroke-dasharray="452.39 452.39" stroke-dashoffset="452" stroke-linecap="round" transform="rotate(-45,80,80)"></circle>
+					</g>
+				</g>
+				<g mask="url(#mask1)">
+					<g class="pl__ring-rotate">
+						<circle class="pl__ring-stroke" cx="80" cy="80" r="72" fill="none" stroke="hsl(193,90%,55%)" stroke-width="16" stroke-dasharray="452.39 452.39" stroke-dashoffset="452" stroke-linecap="round" transform="rotate(-45,80,80)"></circle>
+					</g>
+				</g>
+				
+				<g>
+					<g stroke-width="4" stroke-dasharray="12 12" stroke-dashoffset="12" stroke-linecap="round" transform="translate(80,80)">
+						<polyline class="pl__tick" stroke="hsl(223,10%,90%)" points="0,2 0,14" transform="rotate(-135,0,0) translate(0,40)"></polyline>
+						<polyline class="pl__tick" stroke="hsl(223,10%,90%)" points="0,2 0,14" transform="rotate(-90,0,0) translate(0,40)"></polyline>
+						<polyline class="pl__tick" stroke="hsl(223,10%,90%)" points="0,2 0,14" transform="rotate(-45,0,0) translate(0,40)"></polyline>
+						<polyline class="pl__tick" stroke="hsl(223,10%,90%)" points="0,2 0,14" transform="rotate(0,0,0) translate(0,40)"></polyline>
+						<polyline class="pl__tick" stroke="hsl(223,10%,90%)" points="0,2 0,14" transform="rotate(45,0,0) translate(0,40)"></polyline>
+						<polyline class="pl__tick" stroke="hsl(223,10%,90%)" points="0,2 0,14" transform="rotate(90,0,0) translate(0,40)"></polyline>
+						<polyline class="pl__tick" stroke="hsl(223,10%,90%)" points="0,2 0,14" transform="rotate(135,0,0) translate(0,40)"></polyline>
+						<polyline class="pl__tick" stroke="hsl(223,10%,90%)" points="0,2 0,14" transform="rotate(180,0,0) translate(0,40)"></polyline>
+					</g>
+				</g>
+				<g mask="url(#mask1)">
+					<g stroke-width="4" stroke-dasharray="12 12" stroke-dashoffset="12" stroke-linecap="round" transform="translate(80,80)">
+						<polyline class="pl__tick" stroke="hsl(223,90%,80%)" points="0,2 0,14" transform="rotate(-135,0,0) translate(0,40)"></polyline>
+						<polyline class="pl__tick" stroke="hsl(223,90%,80%)" points="0,2 0,14" transform="rotate(-90,0,0) translate(0,40)"></polyline>
+						<polyline class="pl__tick" stroke="hsl(223,90%,80%)" points="0,2 0,14" transform="rotate(-45,0,0) translate(0,40)"></polyline>
+						<polyline class="pl__tick" stroke="hsl(223,90%,80%)" points="0,2 0,14" transform="rotate(0,0,0) translate(0,40)"></polyline>
+						<polyline class="pl__tick" stroke="hsl(223,90%,80%)" points="0,2 0,14" transform="rotate(45,0,0) translate(0,40)"></polyline>
+						<polyline class="pl__tick" stroke="hsl(223,90%,80%)" points="0,2 0,14" transform="rotate(90,0,0) translate(0,40)"></polyline>
+						<polyline class="pl__tick" stroke="hsl(223,90%,80%)" points="0,2 0,14" transform="rotate(135,0,0) translate(0,40)"></polyline>
+						<polyline class="pl__tick" stroke="hsl(223,90%,80%)" points="0,2 0,14" transform="rotate(180,0,0) translate(0,40)"></polyline>
+					</g>
+				</g>
+				
+				<g>
+					<g transform="translate(64,28)">
+						<g class="pl__arrows" transform="rotate(45,16,52)">
+							<path fill="hsl(3,90%,55%)" d="M17.998,1.506l13.892,43.594c.455,1.426-.56,2.899-1.998,2.899H2.108c-1.437,0-2.452-1.473-1.998-2.899L14.002,1.506c.64-2.008,3.356-2.008,3.996,0Z"></path>
+							<path fill="hsl(223,10%,90%)" d="M14.009,102.499L.109,58.889c-.453-1.421,.559-2.889,1.991-2.889H29.899c1.433,0,2.444,1.468,1.991,2.889l-13.899,43.61c-.638,2.001-3.345,2.001-3.983,0Z"></path>
+						</g>
+					</g>
+				</g>
+				<g mask="url(#mask2)">
+					<g transform="translate(64,28)">
+						<g class="pl__arrows" transform="rotate(45,16,52)">
+							<path fill="hsl(333,90%,55%)" d="M17.998,1.506l13.892,43.594c.455,1.426-.56,2.899-1.998,2.899H2.108c-1.437,0-2.452-1.473-1.998-2.899L14.002,1.506c.64-2.008,3.356-2.008,3.996,0Z"></path>
+							<path fill="hsl(223,90%,80%)" d="M14.009,102.499L.109,58.889c-.453-1.421,.559-2.889,1.991-2.889H29.899c1.433,0,2.444,1.468,1.991,2.889l-13.899,43.61c-.638,2.001-3.345,2.001-3.983,0Z"></path>
+						</g>
+					</g>
+				</g>
+			</svg>
+			</div>
+			<br>
+			<h5 style="margin-top: -45%;" class="font-weight-bold mb-0 notranslate">กรุณาเปิดตำแหน่งที่ตั้งด้วยค่ะ</h5>		
+			<span style="bottom: -20%;border-radius:10px" class="sry-open-location-text btn btn-md btn-warning main-shadow main-radius p-2" onclick="window.location.reload(true);">
+				<i class="fa-solid fa-arrows-rotate"></i> โหลดใหม่
+			</span>
 		</div>
 	</div>
 	<!-- <div id="" class="col-12 ">
@@ -415,9 +640,13 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#00000000', e
 </div> -->
 
 <!-- Button modal -->
-<span id="btn_modal_officer_to_the_scene" class="btn btn-primary d-none" data-toggle="modal" data-target="#modal_sos_1669"></span>
+<!-- <span id="btn_modal_officer_to_the_scene" style="z-index: 9999; position: absolute; top:50%; d-none" class="btn btn-primary " data-toggle="modal" data-target="#modal_sos_1669">ฟหกฟก</span> -->
+
+
+
+
 <!-- Modal -->
-<div class="modal fade" id="modal_sos_1669" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="BackdropLabel_modal_sos_1669" aria-hidden="true" >
+<!-- <div class="modal fade" id="modal_sos_1669" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="BackdropLabel_modal_sos_1669" aria-hidden="true" >
     <div class="modal-dialog modal-dialog-centered ">
         <div class="modal-content">
 
@@ -442,10 +671,35 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#00000000', e
                     </a>
                 </div>
             </div>
+			<style>
+				.alert-officer-arrive{
+					display: flex;
+					align-items: center;
+					justify-content: space-between;
+				}.box-alert-officer-arrive{
+					border-radius: 10px;
+					background-color: #ffffff;
+					padding: 10px;
+				}
+			</style> 
+			
+			<div class="bg-success p-3">
 
+				<div class="box-alert-officer-arrive">
+					<div class="alert-officer-arrive">
+						<div class="p-2">
+							<span class="d-block font-weight-bold">สวัสดีคุณ {{ $data_user->name }}</span>
+							<span class="d-block">เจ้าที่หน้ามาถึงคุณแล้ว</span>
+						</div>
+						<img src="{{ asset('/img/stickerline/PNG/34.png') }}" width="70rem" class="float-end" alt="">
+					</div>
+					<a class="btn btn-sm btn-success main-shadow main-radius">asd</a>
+				</div>
+				
+			</div>
         </div>
     </div>
-</div>
+</div> -->
 	
 <!-- VIICHECK ใช้จริงใช้อันนี้ -->
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBgrxXDgk1tgXngalZF3eWtcTWI-LPdeus&language=th"></script>
@@ -634,7 +888,10 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#00000000', e
 
                 }else{
                 	Stop_loop_check_officer();
-                	document.querySelector('#btn_modal_officer_to_the_scene').click();
+                	// document.querySelector('#btn_modal_officer_to_the_scene').click();
+					document.querySelector('.box-data-helper').classList.add('close-data');
+					document.querySelector('.officer-arrive').classList.remove('d-none');
+					document.querySelector('.officer-arrive').classList.add('show-data');
                 }
         });
 
