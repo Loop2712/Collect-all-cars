8a4e173e5ecc5625a21f0558398e368eb33dd962
EBenze <52275898+ebenze31@users.noreply.github.com>
2023-03-22 07:50:45 +0700

    Update form.blade.php

--- resources/views/data_1669_operating_officer/form.blade.php
+++ resources/views/data_1669_operating_officer/form.blade.php
@@ -13,18 +13,18 @@
     <input class="form-control" name="lng" type="text" id="lng" value="{{ isset($data_1669_operating_officer->lng) ? $data_1669_operating_officer->lng : ''}}" >
     {!! $errors->first('lng', '<p class="help-block">:message</p>') !!}
 </div>
-<div class="form-group {{ $errors->has('operating_unit_id') ? 'has-error' : ''}}">
+<div class="d-none form-group {{ $errors->has('operating_unit_id') ? 'has-error' : ''}}">
     <label for="operating_unit_id" class="control-label">{{ 'Operating Unit Id' }}</label>
     <input class="form-control" name="operating_unit_id" type="text" id="operating_unit_id" value="{{ isset($data_1669_operating_officer->operating_unit_id) ? $data_1669_operating_officer->operating_unit_id : $operating_unit_id }}" readonly>
     {!! $errors->first('operating_unit_id', '<p class="help-block">:message</p>') !!}
 </div>
-<div class="form-group {{ $errors->has('user_id') ? 'has-error' : ''}}">
+<div class="d-none form-group {{ $errors->has('user_id') ? 'has-error' : ''}}">
     <label for="user_id" class="control-label">{{ 'User Id' }}</label>
     <input class="form-control" name="user_id" type="text" id="user_id" value="{{ isset($data_1669_operating_officer->user_id) ? $data_1669_operating_officer->user_id : Auth::user()->id }}" readonly>
     {!! $errors->first('user_id', '<p class="help-block">:message</p>') !!}
 </div>
 <div class="form-group {{ $errors->has('area') ? 'has-error' : ''}}">
-    <label for="area" class="control-label">{{ 'User Id' }}</label>
+    <label for="area" class="control-label">{{ 'พื้นที่' }}</label>
     <input class="form-control" name="name_area" type="text" id="area" value="{{ $name_area }}" readonly>
 </div>
 
