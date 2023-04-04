f08060083ec863d18778313139c1c701ae49bbad
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2023-04-04 18:34:10 +0700

    Update rate_case.blade.php

--- resources/views/sos_help_center/rate_case.blade.php
+++ resources/views/sos_help_center/rate_case.blade.php
@@ -66,7 +66,7 @@
             href: url,
             quote: title,
             // hashtag: '#วีเช็คช่วยเหลือคนไทยฟรี',
-            caption: '#วีเช็ค #1669 #สพฉ #วีเช็คช่วยเหลือคนไทยฟรี',
+            hashtag: '#วีเช็ค #สพฉ #วีเช็คช่วยเหลือคนไทยฟรี',
             picture: imageUrl,
             description: description
         });
