2cc8e27d20cf956b7096e38f25b65b90444da8fa
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2023-04-05 10:25:50 +0700

    Update share_1669.blade.php

--- resources/views/share_to_facebook/share_1669.blade.php
+++ resources/views/share_to_facebook/share_1669.blade.php
@@ -1,28 +1,39 @@
-@extends('layouts.viicheck')
-
-@section('content')
-<br><br><br><br><br><br>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
+	<meta charset="UTF-8">
+	<meta name="description" content="HVAC Template">
+	<meta name="keywords" content="HVAC, unica, creative, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="shortcut icon" href="{{ asset('/img/logo/logo_x-icon.png') }}" type="image/x-icon" />
+	<!-- CSRF Token -->
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+
 	<title>วีเช็ค ร่วมกับ สพฉ.(1669)</title>
 
 	<meta property="og:url"           content="https://page.line.me/702ytkls" />
 	<meta property="og:type"          content="website" />
 	<meta property="og:title"         content="วีเช็ค ร่วมกับ สพฉ.(1669)" />
 	<meta property="og:description"   content="ให้การขอความช่วยเหลือเป็นเรื่องง่าย เพียงกดปุ่ม SOS.." />
-	<meta property="og:image"         content="https://www.viicheck.com/img/poster/1669_share_facebook.png" />
+	<meta property="og:image"         content="https://www.viicheck.com/img/poster/test_facebook.png" />
 </head>
 
-<a class="d-none" id="btn_addline" href="https://lin.ee/xnFKMfc">
-    <button type="button" class="btn btn-success"></button>
-</a>
-<img class="d-none" width="100%" src="https://www.viicheck.com/img/poster/1669_share_facebook.png">
-<script>
-    document.addEventListener('DOMContentLoaded', (event) => {
-        // console.log("START"); 
-        document.getElementById("btn_addline").click();
+<body>
+	<a class="d-none" id="btn_addline" href="https://lin.ee/xnFKMfc">
+	    <button type="button" class="btn btn-success"></button>
+	</a>
+    <img class="d-none" width="100%" src="https://www.viicheck.com/img/poster/test_facebook.png">
+	<script>
+	    document.addEventListener('DOMContentLoaded', (event) => {
+	        // console.log("START"); 
+	        document.getElementById("btn_addline").click();
 
-    });
-</script>
+	    });
+	</script>
+</body>
 
-@endsection
+</html>
+
+
