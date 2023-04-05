b2c9367c9084df5297cc0ea3595c8d397fd7dd5d
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2023-04-05 10:38:20 +0700

    Update rate_case.blade.php

--- resources/views/sos_help_center/rate_case.blade.php
+++ resources/views/sos_help_center/rate_case.blade.php
@@ -52,8 +52,8 @@
 
     function shareOnFacebook() {
 
-        // let url = 'https://www.viicheck.com/share_1669';
-        let url = 'https://www.mithcare.com';
+        let url = 'https://www.viicheck.com/share_1669';
+        // let url = 'https://www.mithcare.com';
         let imageUrl = 'https://www.viicheck.com/img/poster/test_facebook.jpg';
         let title = 'วีเช็ค ร่วมกับ สพฉ.(1669)';
         let description = 'ให้การขอความช่วยเหลือเป็นเรื่องง่าย เพียงกดปุ่ม SOS..' ;
