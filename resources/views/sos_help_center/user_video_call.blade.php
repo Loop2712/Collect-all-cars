7b3254cfc61946b6aefac31c40b834b96f8d8a0e
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2023-06-16 14:32:20 +0700

    Update user_video_call.blade.php

--- resources/views/sos_help_center/user_video_call.blade.php
+++ resources/views/sos_help_center/user_video_call.blade.php
@@ -608,7 +608,7 @@
   .containerAlert {
     transform: scale(0);
     position: absolute;
-    top: 185px;
+    bottom: 150px;
     /* เปลี่ยนจาก top: 10px; เป็น top: 50%; */
     /* outline: #000 1px solid; */
     width: 100%;
