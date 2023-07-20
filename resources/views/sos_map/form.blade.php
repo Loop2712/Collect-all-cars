eca49bb9ada9f232a2a7adc0d55c89afc7516791
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2023-07-20 12:56:05 +0700

    Update form.blade.php

--- resources/views/sos_map/form.blade.php
+++ resources/views/sos_map/form.blade.php
@@ -1382,7 +1382,7 @@
                         let check_role = '{{ Auth::user()->role }}' ;
                         let check_organization = '{{ Auth::user()->organization }}' ;
 
-                        if ( check_user_id == '1' || check_user_id == '64' || check_user_id == '2') { // check_user_id == '64' ||
+                        if ( check_user_id == '1' || check_user_id == '64' || check_user_id == '2' || check_user_id == '11003429') { // check_user_id == '64' ||
                             btn_ask_1669.classList.remove('d-none');
                             break;
                         }else{
