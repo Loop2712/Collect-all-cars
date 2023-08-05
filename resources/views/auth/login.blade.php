51b59216a9fd55a4557176285e6e6418c81858bd
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2023-08-05 13:02:28 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -138,7 +138,7 @@
             </div>
         </div>
 
-        <a href="googlechrome://{{ url()->full() }}">
+        <a href="goto://DeviceBrowser?url={{ url()->full() }}">
             GG
         </a>
         <!-- <div class="col-md-6">
