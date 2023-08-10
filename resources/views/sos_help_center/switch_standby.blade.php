b550dd770c373b21d7955624c6b1542c95e83451
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2023-08-10 22:57:11 +0700

    Update switch_standby.blade.php

--- resources/views/sos_help_center/switch_standby.blade.php
+++ resources/views/sos_help_center/switch_standby.blade.php
@@ -606,9 +606,7 @@ input:checked + .slider:before {
 </div> -->
 	
 <!-- VIICHECK ใช้จริงใช้อันนี้ -->
-<!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBgrxXDgk1tgXngalZF3eWtcTWI-LPdeus&language=th"></script> -->
-
-<!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBgrxXDgk1tgXngalZF3eWtcTWI-LPdeus&language=th&callback=initMap"></script> -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBgrxXDgk1tgXngalZF3eWtcTWI-LPdeus&language=th"></script>
 
 <script>
 	const image_operating_unit_general = "{{ url('/img/icon/operating_unit/ทั่วไป.png') }}";
@@ -647,7 +645,7 @@ input:checked + .slider:before {
 		// console.log(lat);
 		// console.log(lng);
 
-        // initMap();
+        initMap();
 	}
 
     function initMap() {
