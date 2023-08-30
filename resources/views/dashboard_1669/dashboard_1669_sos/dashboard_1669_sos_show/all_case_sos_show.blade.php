e522ad6f4cd7797279fa0c40b746ec77e614762e
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2023-08-30 09:42:13 +0700

    Update all_case_sos_show.blade.php

--- resources/views/dashboard_1669/dashboard_1669_sos/dashboard_1669_sos_show/all_case_sos_show.blade.php
+++ resources/views/dashboard_1669/dashboard_1669_sos/dashboard_1669_sos_show/all_case_sos_show.blade.php
@@ -137,7 +137,7 @@
                             <td>{{ $item_sos->operating_code ? $item_sos->operating_code : "--" }}</td>
                             <td>{{ $item_sos->status ? $item_sos->status : "--" }}</td>
                             <td>{{ $item_sos->remark_status ? $item_sos->remark_status : "--" }}</td>
-                            <td>{{ thaidate("j F Y" , strtotime($item_sos->created_at)) ? thaidate("j F Y" , strtotime($item_sos->created_at)) : "--" }}</td>
+                            <td>{{ thaidate("j F Y / H:i" , strtotime($item_sos->created_at)) ? thaidate("j F Y / H:i" , strtotime($item_sos->created_at)) : "--" }}</td>
                             <td>{{ $item_sos->lat ? $item_sos->lat : "--" }}</td>
                             <td>{{ $item_sos->lng ? $item_sos->lng : "--" }}</td>
                             <td>{{ $item_sos->address ? $item_sos->address : "--" }}</td>
