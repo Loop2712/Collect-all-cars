81ede086db5730a9c18c3c4129064c0b6f899955
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2023-08-31 11:37:21 +0700

    Update test_test.blade.php

--- resources/views/test_test.blade.php
+++ resources/views/test_test.blade.php
@@ -4,11 +4,33 @@
 @section('content')
 	
 	@php
-		$data_sos = App\Models\Sos_help_center::where('photo_succeed', "!=" , null)->get();
+		$data_sos_photo_sos = App\Models\Sos_help_center::where('photo_sos', "!=" , null)->get();
+		$data_sos_photo_succeed = App\Models\Sos_help_center::where('photo_succeed', "!=" , null)->get();
+		$data_sos_photo_sos_by_officers = App\Models\Sos_help_center::where('photo_sos_by_officers', "!=" , null)->get();
 	@endphp
 
-	@foreach($data_sos as $eiei)
-		<img src="{{ url('storage')}}/{{ $eiei->photo_succeed }}" style="width: 30%;" class="m-3">
+	<h1> photo_sos </h1>
+	@foreach($data_sos_photo_sos as $eiei_1)
+		<img src="{{ url('storage')}}/{{ $eiei_1->photo_sos }}" style="width: 30%;" class="m-3">
 	@endforeach
+	<br>
+	<hr>
+	<br>
+
+	<h1> photo_succeed </h1>
+	@foreach($data_sos_photo_succeed as $eiei_2)
+		<img src="{{ url('storage')}}/{{ $eiei_2->photo_succeed }}" style="width: 30%;" class="m-3">
+	@endforeach
+	<br>
+	<hr>
+	<br>
+
+	<h1> photo_sos_by_officers </h1>
+	@foreach($data_sos_photo_sos_by_officers as $eiei_3)
+		<img src="{{ url('storage')}}/{{ $eiei_3->photo_sos_by_officers }}" style="width: 30%;" class="m-3">
+	@endforeach
+	<br>
+	<hr>
+	<br>
 
 @endsection
