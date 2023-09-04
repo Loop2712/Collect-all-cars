43d0021d576aa7fbac5e7ec4d1e30b5c125f74e9
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2023-09-04 15:04:14 +0700

    Update Manage_uploaded_photos.blade.php

--- resources/views/Manage_uploaded_photos.blade.php
+++ resources/views/Manage_uploaded_photos.blade.php
@@ -11,10 +11,85 @@
 		@php
 	    	$url = Storage::url($file);
 	    	$name_file = str_replace("public/uploads/" , "" , $file);
+	    	$name_db_file = 'uploads/' . $name_file;
+
+	    	$address_img = "";
+
+	    	// --- ads_contents ---
+	    	$db_ads_contents = App\Models\Ads_content::where('photo',$name_db_file)->first();
+	    	if(!empty($db_ads_contents)){
+	    		$address_img = $address_img . "/" . 'ads_contents' ;
+	    	}
+
+	    	// --- guests ---
+	    	$db_guests = App\Models\Guest::where('photo',$name_db_file)->first();
+	    	if(!empty($db_guests)){
+	    		$address_img = $address_img . "/" . 'guests' ;
+	    	}
+
+	    	// --- news ---
+	    	$db_news = App\Models\News::where('photo',$name_db_file)->first();
+	    	if(!empty($db_news)){
+	    		$address_img = $address_img . "/" . 'news' ;
+	    	}
+
+	    	// --- parcels ---
+	    	$db_parcels = App\Models\Parcel::where('photo',$name_db_file)->first();
+	    	if(!empty($db_parcels)){
+	    		$address_img = $address_img . "/" . 'parcels' ;
+	    	}
+
+	    	// --- partners ---
+	    	$db_partners = App\Models\Partner::where('logo',$name_db_file)->first();
+	    	if(!empty($db_partners)){
+	    		$address_img = $address_img . "/" . 'partners' ;
+	    	}
+
+	    	// --- problem_reports ---
+	    	$db_problem_reports = App\Models\Problem_report::where('image',$name_db_file)->first();
+	    	if(!empty($db_problem_reports)){
+	    		$address_img = $address_img . "/" . 'problem_reports' ;
+	    	}
+
+	    	// --- promotions ---
+	    	$db_promotions = App\Models\Promotion::where('photo',$name_db_file)->first();
+	    	if(!empty($db_promotions)){
+	    		$address_img = $address_img . "/" . 'promotions' ;
+	    	}
+
+	    	// --- sos_help_centers ---
+	    	$db_sos_help_centers = App\Models\Sos_help_center::where('photo_sos',$name_db_file)
+	    		->orWhere('photo_succeed',$name_db_file)
+	    		->orWhere('photo_sos_by_officers',$name_db_file)
+	    		->first();
+	    	if(!empty($db_sos_help_centers)){
+	    		$address_img = $address_img . "/" . 'sos_help_centers' ;
+	    	}
+
+	    	// --- sos_maps ---
+	    	$db_sos_maps = App\Models\Sos_map::where('photo',$name_db_file)
+	    		->orWhere('photo_succeed',$name_db_file)
+	    		->first();
+	    	if(!empty($db_sos_maps)){
+	    		$address_img = $address_img . "/" . 'sos_maps' ;
+	    	}
+
+	    	// --- users ---
+	    	$db_users = App\User::where('photo',$name_db_file)
+	    		->orWhere('driver_license',$name_db_file)
+	    		->orWhere('driver_license2',$name_db_file)
+	    		->first();
+	    	if(!empty($db_users)){
+	    		$address_img = $address_img . "/" . 'users' ;
+	    	}
+
+
 	   	@endphp
 	    <div class="col-2 card" style="padding:10px;">
 	    	<!-- <span> {{ $url }} </span> -->
 	    	<span> {{ $name_file }} </span>
+	    	<br>
+	    	<span> {{ $address_img ? $address_img : "--" }} </span>
 	    	<hr>
 	    	<center>
 	    		<span class="btn btn-sm btn-danger mb-3" style="width:80%;" onclick="delete_photo('{{ $name_file }}');">
