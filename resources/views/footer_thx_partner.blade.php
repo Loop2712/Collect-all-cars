c8f3e4e7ee2634b23dba0dba2133c62280dcf550
lucky <54930672+luxlnw171041@users.noreply.github.com>
2023-09-08 14:26:54 +0700

    Update footer_thx_partner.blade.php

--- resources/views/footer_thx_partner.blade.php
+++ resources/views/footer_thx_partner.blade.php
@@ -7,91 +7,9 @@
             <hr style="margin-top:10px;padding:19;left:10px;">
             </div>
             <div class="col-12" style="margin-bottom:-50px;">
-                <div class="row text-center">
-                    <div class="col-3" >
-                        <a href="https://www.kmutnb.ac.th/" target="bank">
-                            <img width="42px" src="{{ asset('/img/logo-partner/KMUTNB.png') }}">
-                        </a>
-                    </div>
-                    <div class="col-3" style="padding:0px;margin-left: -8px;">
-                        <a href="https://www.rmutp.ac.th/" target="bank">
-                            <img height="42 px"  src="{{ asset('/img/logo-partner/RMUTP.png') }}">
-                        </a>
-                    </div>
-                    <div class="col-3" style="margin-left: -8px;">
-                        <a href="https://tu.ac.th/" target="bank">
-                            <img width="42px" src="{{ asset('/img/logo-partner/TU.png') }}">
-                        </a>
-                    </div>
-                    <div class="col-3 " style="margin-left: -8px;">
-                        <a href="https://www.peddyhub.com/" target="bank">
-                            <img height="42px" src="{{ asset('/img/logo-partner/nstda.png') }}">
-                        </a>
-                    </div>
-                    <div class="col-12"><br></div>
-                    <div class="col-3 text-center" style="margin-left: -8px;">
-                        <a href="https://www.js100.com/" target="bank">
-                            <img height="42 px"  src="{{ asset('/img/logo-partner/js100.png') }}">
-                        </a>
-                    </div>
-                    <div class="col-3" >
-                        <a href="http://www.impact.co.th/" target="bank">
-                            <img width="42px" src="{{ asset('/img/logo-partner/IMPACT.jpg') }}">
-                        </a>
-                    </div>
-                   
-                    
-                    <div class="col-3 " >
-                        <a href="#" target="bank">
-                            <img height="42px" src="{{ asset('/img/logo-partner/ตลาดคลองเตย.png') }}">
-                        </a>
-                    </div>
-                    <div class="col-3" >
-                        <a href="https://www.facebook.com/SouthernCoffeeClub/" target="bank">
-                            <img width="42px" src="{{ asset('/img/logo-partner/southerncoffee-1.png') }}">
-                        </a>
-                    </div> 
-                    
-                    <div class="col-12"><br></div>
-                    <div class="col-6 " >
-                        <img height="42px" src="{{ asset('/img/logo-partner/GreenLogo.png') }}">
-                    </div>
-                    <div class="col-6 " >
-                        <a href="https://www.peddyhub.com/" target="bank">
-                            <img height="42px" src="{{ asset('/img/logo-partner/logo-ph.png') }}">
-                        </a>
-                    </div>
-                </div>
+                <br>
+                @include ('layouts.partner_2_row')
             </div>
-
-
-            <!-- <div class="row text-center" style="margin-bottom:-80px;">
-                <div class="col-3">
-                    <img style="width: 55%;height: 70px;object-fit: contain;" src="{{ asset('/img/logo-partner/logo-ph.png') }}">
-                </div>
-                <div class="col-3">
-                    <img style="width: 100%;height: 90px;object-fit: contain;margin-top:-10px;" src="{{ asset('/img/logo-partner/js100.png') }}">
-                </div>
-                <div class="col-3">
-                    <img style="width: 80%;height: 70px;object-fit: contain;" src="{{ asset('/img/logo-partner/IMPACT.jpg') }}">
-                </div>
-                <div class="col-3">
-                    <img style="width: 80%;height: 70px;object-fit: contain;" src="{{ asset('/img/logo-partner/KMUTNB.png') }}">
-                </div>
-                <div class="col-3">
-                    <img style="width: 75%;height: 70px;object-fit: contain;" src="{{ asset('/img/logo-partner/RMUTP.png') }}">
-                </div>
-                <div class="col-3">
-                    <img style="width: 55%;height: 70px;object-fit: contain;" src="{{ asset('/img/logo-partner/TU.png') }}">
-                </div>
-                <div class="col-3">
-                    <img style="width: 70%;height: 70px;object-fit: contain;margin-top:-10px;" src="{{ asset('/img/logo-partner/ตลาดคลองเตย.png') }}">
-                </div>
-                <div class="col-3">
-                    <img style="width: 55%;height: 70px;object-fit: contain;" src="{{ asset('/img/logo-partner/southerncoffee-1.png') }}">
-                </div>
-            </div> -->
-            
             <br>
         </div>
       </div>
