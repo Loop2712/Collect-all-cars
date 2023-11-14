4d840918b4f4a03a23db7043bd9d78c41f90a5ed
Lomtale <43232280+deer2018@users.noreply.github.com>
2023-11-14 09:33:11 +0700

    f

--- resources/views/video_call_4/mobile_video_call_4.blade.php
+++ resources/views/video_call_4/mobile_video_call_4.blade.php
@@ -3676,7 +3676,7 @@
 
                     // วันที่และเวลาที่กำหนด
                     // let targetDate = new Date(response['data']);
-                    let targetDate = new Date('2023-11-13T10:00:00');
+                    let targetDate = new Date('2023-11-14T9:30:00');
                     let targetTime = targetDate.getTime();
 
                     // คำนวณเวลาที่ผ่านไปในมิลลิวินาที
