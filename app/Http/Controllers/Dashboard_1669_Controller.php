45cc275ffbfa7d886348167d55406e2efa8d2be5
lucky <54930672+luxlnw171041@users.noreply.github.com>
2024-02-29 09:44:35 +0700

    Update Dashboard_1669_Controller.php

--- app/Http/Controllers/Dashboard_1669_Controller.php
+++ app/Http/Controllers/Dashboard_1669_Controller.php
@@ -40,7 +40,7 @@ class Dashboard_1669_Controller extends Controller
         ->leftjoin('users as user_data', 'data_1669_officer_commands.user_id', '=', 'user_data.id')
         ->leftjoin('users as creator_data', 'data_1669_officer_commands.creator', '=', 'creator_data.id')
         ->select('data_1669_officer_commands.*', 'user_data.name as user_name' ,'user_data.sex as user_gender' ,'user_data.avatar as user_avatar' ,'user_data.photo as user_photo' ,'creator_data.name as name_creator')
-        ->orderBy('data_1669_officer_commands.created_at', 'asc')
+        ->orderBy('data_1669_officer_commands.created_at', 'DESC')
         ->get();
 
         return response()->json($data_status_officer);
