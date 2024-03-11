598d795e34b16108a61c5f65299b177509cc5220
lucky <54930672+luxlnw171041@users.noreply.github.com>
2024-03-11 10:20:09 +0700

    Update modal_hospital_offices.blade.php

--- resources/views/sos_help_center/modal_hospital_offices.blade.php
+++ resources/views/sos_help_center/modal_hospital_offices.blade.php
@@ -4,7 +4,153 @@
   #map_show_hospital {
       height: calc(86vh);
     }
+    .switcher-data-sos {
+        position: absolute;
+        z-index: 999;
+        right: 1%;
+        margin-top: 25%;
+        opacity: 1;
 
+    }
+    .switcher-btn-sos {
+        opacity: 1 !important;
+        border-radius: 10px 0 0 10px;
+        color: #000;
+        background-color: #fff;
+        margin-left: -56px;
+        height: 110px;
+        background-color: #fff;
+        position: absolute;
+    } 
+    .hospital-open {
+      animation: slide-hospital-open 1s ease 0s 1 normal forwards;
+    }
+    @keyframes slide-hospital-open {
+        0% {
+            transform: translateX(100%);
+        }
+
+        100% {
+            transform: translateX(0px);
+        }
+    }
+
+    .hospital-close {
+        animation: slide-hospital-close 1s ease 0s 1 normal forwards;
+    }
+
+    @keyframes slide-hospital-close {
+        0% {
+            transform: translateX(0px);
+        }
+
+        100% {
+            transform: translateX(111%);
+        }
+    }
+    #content_name_hospital .div_health_type:hover{
+      background-color: #f7f8f8;
+      
+    }.btn-hospital{
+      padding: 5px 5px;
+      border: 1px solid #8833ff!important;
+      color: #8833ff;
+      transition: all .15s ease-in-out;
+    }.btn-hospital:hover{
+      background-color: #8833ff;
+      color: #fff;
+    }.loading-container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .loading-spinner {
+        border: 4px solid rgba(0, 0, 0, 0.1);
+        border-left-color: #000;
+        animation: spin 1s linear infinite;
+        border-radius: 50%;
+        width: 40px;
+        height: 40px;
+        margin-right: 20px;
+        margin-top: 50px;
+        margin-bottom: 50px;
+    }
+
+
+    @keyframes spin {
+        0% {
+            transform: rotate(0deg);
+        }
+
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+
+    @keyframes drawCheck {
+        0% {
+            transform: scale(0);
+        }
+
+        100% {
+            transform: scale(1);
+        }
+    }
+
+    .checkmark__circle_modal {
+				    stroke-dasharray: 166;
+				    stroke-dashoffset: 166;
+				    stroke-width: 2;
+				    stroke-miterlimit: 10;
+				    stroke: #29cc39;
+				    fill: none;
+				    animation: stroke 0.6s cubic-bezier(0.65, 0, 0.45, 1) forwards
+				}
+
+				.checkmark_modal {
+				    width: 80px;
+				    height: 80px;
+				    border-radius: 50%;
+				    display: block;
+				    stroke-width: 2;
+				    stroke: #29cc39;
+				    stroke-miterlimit: 10;
+				    margin: 10% auto;
+				    box-shadow: inset 0px 0px 0px #29cc39;
+				    animation: fill .4s ease-in-out .4s forwards, scale .3s ease-in-out .9s both
+				}
+
+				.checkmark__check_modal {
+				    transform-origin: 50% 50%;
+				    stroke-dasharray: 48;
+				    stroke-dashoffset: 48;
+				    animation: stroke 0.3s cubic-bezier(0.65, 0, 0.45, 1) 0.8s forwards
+				}
+
+    @keyframes stroke {
+        100% {
+            stroke-dashoffset: 0
+        }
+    }
+
+    @keyframes scale {
+
+        0%,
+        100% {
+            transform: none
+        }
+
+        50% {
+            transform: scale3d(1.1, 1.1, 1)
+        }
+    }
+
+    @keyframes fill {
+        100% {
+            box-shadow: inset 0px 0px 0px 60px #fff
+        }
+    }
 </style>
 
 <!-- Button trigger modal -->
@@ -13,12 +159,28 @@
 <!-- Modal -->
 <div class="modal fade" id="modal_cf_select_hospital" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true" style="z-index:999999!important;">
   <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="Label_cf_select_hospital">โปรดยืนยัน</h5>
-      </div>
-      <div id="content_modal_cf_hospital" class="modal-body">
+    <div class="modal-content" style="border-radius: 15px;">
+        <!-- <div class="modal-header">
+          <h5 class="modal-title" id="Label_cf_select_hospital">โปรดยืนยัน</h5>
+        </div> -->
+      <div id="content_modal_cf_hospital" class="modal-body"> 
         <!--  -->
+      </div>
+
+      <div id="success_modal_cf_hospital" class="modal-body d-none"> 
+        <div class="loading-container">
+            <div class="loading-spinner"></div>
+
+            <div class="contrainerCheckmark d-none my-5">
+                <svg class="checkmark_modal" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+                    <circle class="checkmark__circle_modal" cx="26" cy="26" r="25" fill="none" />
+                    <path class="checkmark__check_modal" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" />
+                </svg>
+                <center>
+                    <h5 class="mt-5 mb-5">เสร็จสิ้น</h5>
+                </center>
+            </div>
+        </div>
       </div>
     </div>
   </div>
@@ -27,13 +189,17 @@
 <!-- Modal -->
 <div class="modal fade" id="show_hospital" tabindex="-1" aria-labelledby="show_hospitalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered modal-fullscreen p-5" style="position: relative;">
-    <div class="modal-content">
-      <div class="modal-body">
+    <div class="modal-content" >
+      <div class="modal-body"style="overflow: hidden;">
 
           <div class="col-12 row">
               <div class="col-9"></div>
-              <div class="col-3 card border-top border-0 border-4 border-primary" style="position: absolute;top: 1%;right: 2%;z-index: 999999!important;">
-                <div class="card-body p-3">
+              <div id="testt" class="card border-top border-0 border-4 border-primary hospital-open" style="width: 380px;position: absolute;top: 1%;right: 2%;z-index: 999999!important;height: calc(86vh);">
+                <button class="btn switcher-btn-sos mt-2 ml-2" onclick="hide_data_hospital()"> 
+                  <i class="fa-solid fa-chevron-right hide-data-sos"></i>
+                </button>
+                
+                <div class="card-body p-3" style="overflow: auto;">
                   <div class="card-title d-flex align-items-center">
                     <div>
                       <i class="fa-solid fa-hospital me-1 font-22 text-primary"></i>
@@ -57,12 +223,25 @@
                         <option value="โรงพยาบาลทั่วไป">โรงพยาบาลทั่วไป</option>
                         <option value="โรงพยาบาลเอกชน">โรงพยาบาลเอกชน</option>
                       </select>
+                      <!-- <input class="form-control" name="select_hospital" id="select_hospital"  list="datalist_select_hospital" placeholder="กรอกประเภท" onchange="change_select_hospital();">
+                      <datalist id="datalist_select_hospital">
+                        <option selected value="all">เลือกประเภท</option>
+                        <option value="คลินิกเอกชน">คลินิกเอกชน</option>
+                        <option value="ศูนย์บริการสาธารณสุข">ศูนย์บริการสาธารณสุข</option>
+                        <option value="ศูนย์บริการสาธารณสุข อปท.">ศูนย์บริการสาธารณสุข อปท.</option>
+                        <option value="ศูนย์สุขภาพชุมชน ของ รพ. / เมือง (ศสม.)">ศูนย์สุขภาพชุมชน ของ รพ. / เมือง (ศสม.)</option>
+                        <option value="สำนักงานสาธารณสุขจังหวัด">สำนักงานสาธารณสุขจังหวัด</option>
+                        <option value="สำนักงานสาธารณสุขอำเภอ">สำนักงานสาธารณสุขอำเภอ</option>
+                        <option value="โรงพยาบาลชุมชน">โรงพยาบาลชุมชน</option>
+                        <option value="โรงพยาบาลทั่วไป">โรงพยาบาลทั่วไป</option>
+                        <option value="โรงพยาบาลเอกชน">โรงพยาบาลเอกชน</option>
+                      </datalist> -->
                     </div>
-
+                    
                     <hr>
 
                     <!-- content name hospital -->
-                    <div id="content_name_hospital" class="col-12">
+                    <div id="content_name_hospital" class="col-12 px-0">
                       <!-- <div class="d-flex align-items-top">
                         <div class="ps-2">
                           <h6 class="mb-1 font-weight-bold">Jordan Ntolo
@@ -76,7 +255,8 @@
                     </div>
 
                   </div>
-
+                  <div>
+                  </div>
                   <div id="have_to_hospital_id" class="row g-3 d-none">
                     <div class="col-12">
                         <h5><b>คุณส่งต่อข้อมูลแล้ว</b></h5>
@@ -97,7 +277,13 @@
     </div>
   </div>
 </div>
-
+<script>
+      function hide_data_hospital() {
+          document.querySelector('#testt').classList.toggle('hospital-close');
+          document.querySelector('.hide-data-sos').classList.toggle('rotate');
+      }
+      
+  </script>
 <script>
   
   function open_map_show_hospital(){
@@ -221,30 +407,28 @@
 
                 html = `
                     <div health_type="`+result[i].health_type+`" class="div_health_type">
-                    <div class="d-flex align-items-top mb-2">
+                    <div class="d-flex align-items-top py-2">
                       <div class="ps-2">
-                          <h5 class="mb-1 font-weight-bold">
+                          <h6 class="mb-1 font-weight-bold">
                             `+result[i].name+`
-                          </h5>
+                          </h6>
                           <p class="font-14">
                             <span class="text-primary"><b>`+result[i].health_type+`</b></span>
+                            <span>·</span>
+                           
+                            <span>`+result[i].address+`</span>
                             <br>
-                            <span class="mb-2">
-                              <b>อำเภอ</b> : `+result[i].district+` <b>ตำบล</b> : `+result[i].sub_district+`
-                            </span>
-                            <br>
-                            <span><b>ที่อยู่</b> : `+result[i].address+`</span>
-                            <br>
-                          <div class="col-12 row mt-2">
-                            <span class="col-6 text-primary float-start font-13">.. กม.</span>
-                            <span class="col-6 float-end btn btn-sm btn-info" onclick="click_select_hospital('`+result[i].id+`','`+result[i].name+`');">
+                            <div class="d-flex justify-content-between align-items-center  mt-2">
+                            <span class="col-6 text-primary" style="font-size:18px">56 กม.</span>
+
+                            <span class="col-6 float-end btn btn-hospital" onclick="click_select_hospital('`+result[i].id+`','`+result[i].name+`');">
                               เลือก
                             </span>
                           </div>
                         </p>
                       </div>
                     </div>
-                    <hr>
+                    <hr class="m-0">
                     </div>
                 `;
 
@@ -299,11 +483,10 @@
         content_modal_cf_hospital.innerHTML = '';
 
     let html = `
-      <div class="text-center">
+      <div class="text-center p-3">
         <center>
         <img src="http://localhost/Collect-all-cars/public/img/stickerline/PNG/7.png" width="150">
         </center>
-        <br>
         <br>
         <h5 class="text-danger">ยืนยันการเลือก</h5>
         <h3 class="mt-2 mb-2"><b>`+hospital_name+`</b></h3>
@@ -313,7 +496,7 @@
         <button type="button" class="btn btn-primary" onclick="cf_select_hospital('`+hospital_id+`')">ยืนยัน</button>
       </div>
     `;
-
+    
     content_modal_cf_hospital.insertAdjacentHTML('beforeend', html); // แทรกล่างสุด
     
     document.querySelector('#btn_open_modal_cf_select_hospital').click();
@@ -324,10 +507,19 @@
 
     let sos_1669_id = "{{ $sos_help_center->id }}";
 
+    document.querySelector('#content_modal_cf_hospital').classList.toggle('d-none');
+    document.querySelector('#success_modal_cf_hospital').classList.toggle('d-none');
+
     fetch("{{ url('/') }}/api/create_1669_to_hospitals/" + hospital_id + "/" + sos_1669_id + "/{{ Auth::user()->id }}")
         .then(response => response.text())
         .then(result => {
             console.log(result);
+
+            setTimeout(() => {
+              
+              document.querySelector('.loading-spinner').classList.toggle('d-none'); 
+              document.querySelector('.contrainerCheckmark').classList.toggle('d-none'); 
+            }, 1500);
 
     });
 
