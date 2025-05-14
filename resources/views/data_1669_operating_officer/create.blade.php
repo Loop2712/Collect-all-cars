933e827aabc615a43da0a10660472119b4a3cda5
EBenze <52275898+ebenze31@users.noreply.github.com>
2025-05-14 10:26:16 +0700

    Update create.blade.php

--- resources/views/data_1669_operating_officer/create.blade.php
+++ resources/views/data_1669_operating_officer/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <br><br><br><br><br><br><br>
-    <div class="container">
+    <div class="container" style="margin-bottom: 150px;">
         <div class="row">
             <div class="col-md-12">
                 <div class="card">
